4647f372bb936c50e9b1d45667db401ba165fb12
김진호 <rlawlsgh1227@gmail.com>
2024-07-16 01:00:36 +0900

    feat: 메인 페이지 퍼블리싱
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ client/src/pages/Main/Main.tsx
@@ -0,0 +1,15 @@
+import {useNavigate} from 'react-router-dom';
+import {ROUTER_URLS} from '../../constants/routerUrls';
+
+const Main = () => {
+  const navigate = useNavigate();
+
+  return (
+    <section>
+      <h1>행동대장</h1>
+      <button onClick={() => navigate(ROUTER_URLS.createEvent)}>행사 생성하기</button>
+    </section>
+  );
+};
+
+export default Main;
