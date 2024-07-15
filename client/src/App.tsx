288718bf0be19924442dcf22069b542ef0d18444
pakxe <pigkill40@naver.com>
2024-07-15 21:03:29 +0900

    chore: 여러 명이 co-authored-by로 들어갈 수 있는지 테스트 커밋
    Co-authored-by: pakxe <pigkill40@naver.com>
    Co-authored-by: soi-ha <soy2302ten@gmail.com>
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>
    Co-authored-by: Todari <rhymint@gmail.com>
    
    Co-authored-by: pakxe <pigkill40@naver.com>
    Co-authored-by: soi-ha <soy2302ten@gmail.com>
    Co-authored-by: jinhokim98 <rlawlsgh1227@gmail.com>
    Co-authored-by: Todari <rhymint@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import {css} from '@emotion/react';
+// co-authored-by test
 
 // 테스트 css props 사용 예제입니다. 추후 삭제 예정
 const style = css`
-	color: red;
+  color: red;
 `;
 
 const App: React.FC = () => {
-	return <div css={style}>Hello, React with Webpack</div>;
+  return <div css={style}>Hello, React with Webpack</div>;
 };
 
 export default App;
