67415493118448e4c3dc1db5b685f3c8e9bb015d
김진호 <rlawlsgh1227@gmail.com>
2024-07-16 03:29:32 +0900

    feat: 스위치 컴포넌트 생성
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ client/src/components/Switch/Switch.tsx
@@ -0,0 +1,27 @@
+import {buttonStyle, switchStyle} from './Switch.style';
+import React from 'react';
+
+interface SwitchProps {
+  buttonList: string[];
+  initialButton?: string;
+  curSwitch: string;
+  setSwitch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Switch = ({buttonList, initialButton = buttonList[0], setSwitch, curSwitch}: SwitchProps) => {
+  const handleButtonClick = (type: string) => {
+    setSwitch(type);
+  };
+
+  return (
+    <div css={switchStyle}>
+      {buttonList.map(buttonName => (
+        <button css={buttonStyle(curSwitch === buttonName)} onClick={() => handleButtonClick(buttonName)}>
+          {buttonName}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Switch;
