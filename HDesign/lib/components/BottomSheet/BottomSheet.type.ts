4ecc274eb97398feb9ba8aa31ce0cd4b4fdd8d2a
이태훈 <rhymint@gmail.com>
2024-07-16 07:11:31 +0900

    feat: BottomSheet component 구현
    Co-authored-by: soi-ha <soy2302ten@gmail.com>

---
+++ HDesign/lib/components/BottomSheet/BottomSheet.type.ts
@@ -0,0 +1,19 @@
+import {Theme} from 'lib/theme/theme.type';
+import {ComponentPropsWithoutRef} from 'react';
+import {ButtonVariants} from '../Button/Button.type';
+
+export interface BottomSheetStyleProps {
+  theme?: Theme;
+}
+
+export interface BottomSheetCustomProps {
+  isOpened: boolean;
+  onChangeOpen?: () => void;
+  onChangeClose?: () => void;
+  fixedButtonVariants?: ButtonVariants;
+  fixedButtonText: string;
+}
+
+export type BottomSheetOptionProps = BottomSheetStyleProps & BottomSheetCustomProps;
+
+export type BottomSheetProps = ComponentPropsWithoutRef<'div'> & BottomSheetOptionProps;
