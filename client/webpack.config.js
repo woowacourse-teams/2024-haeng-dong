ee599c7ecdc3d519e3e17675c50f539afe4cb57c
pakxe <pigkill40@naver.com>
2024-07-15 20:39:13 +0900

    chore: css props 사용 위해 모든 파일 최상단에 주석을 자동으로 작성해주도록 플러그인 세팅
    Co-authored-by: pakxe <pigkill40@naver.com>, soi-ha <soy2302ten@gmail.com>, jinhokim98 <rlawlsgh1227@gmail.com>, Todari <rhymint@gmail.com>

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,38 +1,47 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
-import { fileURLToPath } from "url";
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import {ModifySourcePlugin, ConcatOperation} from 'modify-source-webpack-plugin';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: "development",
-  entry: "./src/index.tsx",
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.min.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "ts-loader",
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-    new ForkTsCheckerWebpackPlugin(),
-  ],
-  devServer: {
-    port: 3000,
-    hot: true,
-  },
+	mode: 'development',
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.min.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './index.html',
+		}),
+		new ForkTsCheckerWebpackPlugin(),
+		new ModifySourcePlugin({
+			rules: [
+				{
+					test: /\.tsx$/i,
+					operations: [new ConcatOperation('start', '/** @jsxImportSource @emotion/react */\n\n')],
+				},
+			],
+		}),
+	],
+	devServer: {
+		port: 3001,
+		hot: true,
+	},
 };
