e102d59d34482f5e07e38b311078e4a14e2b293c
김진호 <rlawlsgh1227@gmail.com>
2024-07-16 01:03:49 +0900

    feat: 라우트 경로 설정
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ client/src/constants/routerUrls.ts
@@ -0,0 +1,7 @@
+export const ROUTER_URLS = {
+  main: '',
+  createEvent: '/create-event',
+  completeCreateEvent: '/create-event/complete',
+  event: '/event',
+  eventManage: '/event/:eventId',
+};
