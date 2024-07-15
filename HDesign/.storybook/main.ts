9cf6fd53a6c2036cbbd54b625b2631c786d4b0c4
Soyeon Choe <soy2302ten@gmail.com>
2024-07-16 01:59:39 +0900

    chore: storybook 설정
    Co-Authored-By: TaehunLee <85233397+Todari@users.noreply.github.com>

--- HDesign/.storybook/main.ts
+++ HDesign/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type {StorybookConfig} from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../lib/**/*.mdx', '../lib/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-onboarding',
