24aea932df54ebc5fb3a1ac66ae1c5126055e93a
김진호 <rlawlsgh1227@gmail.com>
2024-07-16 01:03:03 +0900

    feat: 앱의 진입점 설정
    Co-authored-by: pakxe <pigkill40@naver.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,7 @@
-import {css} from '@emotion/react';
-// co-authored-by test
-
-// 테스트 css props 사용 예제입니다. 추후 삭제 예정
-const style = css`
-  color: red;
-`;
+import {Outlet} from 'react-router-dom';
 
 const App: React.FC = () => {
-  return <div css={style}>Hello, React with Webpack</div>;
+  return <Outlet />;
 };
 
 export default App;
