748656c951bbc8162343f6aa9707bded3f186ab0
Soyeon Choe <soy2302ten@gmail.com>
2024-07-15 22:41:03 +0900

    chore: 기본 webpack 설정
    Co-Authored-By: TaehunLee <85233397+Todari@users.noreply.github.com>

---
+++ HDesign/eslint.config.mjs
@@ -0,0 +1,127 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+import {fixupConfigRules, fixupPluginRules} from '@eslint/compat';
+import react from 'eslint-plugin-react';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import _import from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...fixupConfigRules(
+    compat.extends(
+      'airbnb',
+      'airbnb/hooks',
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:import/typescript',
+      'plugin:import/recommended',
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'prettier',
+      'plugin:prettier/recommended',
+    ),
+  ),
+  {
+    plugins: {
+      react: fixupPluginRules(react),
+      '@typescript-eslint': fixupPluginRules(typescriptEslint),
+      import: fixupPluginRules(_import),
+      prettier: fixupPluginRules(prettier),
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+
+        typescript: {
+          directory: './lib',
+        },
+      },
+
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/jsx-uses-vars': 'error',
+      '@typescript-eslint/no-use-before-define': ['error'],
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+
+      'import/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+
+          groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'unknown'],
+
+          pathGroups: [
+            {
+              pattern: 'react*',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@hooks/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@apis/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@pages/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@components/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@assets/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@utils/*',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@constants/*',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
---
+++ HDesign/webpack.config.js
@@ -0,0 +1,48 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import {ModifySourcePlugin, ConcatOperation} from 'modify-source-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: 'development',
+  entry: './lib/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ModifySourcePlugin({
+      rules: [
+        {
+          test: /\.tsx$/i,
+          operations: [new ConcatOperation('start', '/** @jsxImportSource @emotion/react */\n\n')],
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+};
