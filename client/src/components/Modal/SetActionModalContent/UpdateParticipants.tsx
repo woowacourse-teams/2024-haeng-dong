077c88b89fb0494dd84db6d8a1e43e8525acd0df
김진호 <rlawlsgh1227@gmail.com>
2024-07-16 03:35:04 +0900

    feat: 인원 관리 기능구현
    Co-authored-by: pakxe <pigkill40@naver.com>

---
+++ client/src/components/Modal/SetActionModalContent/UpdateParticipants.tsx
@@ -0,0 +1,42 @@
+import {useState} from 'react';
+
+interface UpdateParticipantsProps {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  participantType: string;
+  participants: string[];
+  setParticipants: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const UpdateParticipants = ({setOpen, participantType, participants, setParticipants}: UpdateParticipantsProps) => {
+  const [name, setName] = useState('');
+
+  const updateParticipant = () => {
+    if (participantType === '늦참') {
+      if (participants.includes(name)) {
+        alert('이미 있는 사용자입니다.');
+        return;
+      }
+
+      setParticipants([...participants, name]);
+    } else if (participantType === '탈주') {
+      if (!participants.includes(name)) {
+        alert('그런 사용자는 없습니다. 올바른 이름을 입력해주세요.');
+        return;
+      }
+
+      setParticipants(prev => prev.filter(participant => participant !== name));
+    }
+
+    setName('');
+    setOpen(false);
+  };
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column'}}>
+      <input onChange={event => setName(event.target.value)} value={name} />
+      <button onClick={updateParticipant}>{participantType} 인원 확정</button>
+    </div>
+  );
+};
+
+export default UpdateParticipants;
