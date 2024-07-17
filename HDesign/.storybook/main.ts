eb6bb822bd433cd82ab28fd64dad96e889a89ae7
이태훈 <rhymint@gmail.com>
2024-07-17 21:29:30 +0900

    chore: storybook svg 사용을 위한 main.ts 설정

--- HDesign/.storybook/main.ts
+++ HDesign/.storybook/main.ts
@@ -1,4 +1,6 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
 import type {StorybookConfig} from '@storybook/react-webpack5';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,6 +15,32 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  webpackFinal: async config => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src'),
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@assets': path.resolve(__dirname, '../src/assets'),
+        '@token': path.resolve(__dirname, '../src/token'),
+        '@theme': path.resolve(__dirname, '../src/theme'),
+      };
+    }
+
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+
+    const imageRule = config.module.rules.find(rule => rule?.['test']?.test('.svg'));
+    if (imageRule) {
+      imageRule['exclude'] = /\.svg$/;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 };
 export default config;
