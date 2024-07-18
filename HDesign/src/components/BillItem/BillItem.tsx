37844fbfe73dd567573ac3df47f7445b491c1b8b
pakxe <pigkill40@naver.com>
2024-07-19 01:28:47 +0900

    feat: BillItem 컴포넌트가 DragHandleItem, Flex 컴포넌트를 사용하도록 수정

--- HDesign/src/components/BillItem/BillItem.tsx
+++ HDesign/src/components/BillItem/BillItem.tsx
@@ -1,12 +1,11 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-
 import {useTheme} from '@theme/HDesignProvider';
-
 import {BillItemProps} from './BillItem.type';
-import {billItemStyle, prefixStyle, textStyle} from './BillItem.style';
+import {textStyle} from './BillItem.style';
 import Text from '@components/Text/Text';
-import IconButton from '../IconButton/IconButton';
+import DragHandleItem from '../DragHandleItem/DragHandleItem';
+import Flex from '../Flex/Flex';
 
 export const BillItem: React.FC<BillItemProps> = ({
   name = '',
@@ -16,17 +15,16 @@ export const BillItem: React.FC<BillItemProps> = ({
 }: BillItemProps) => {
   const {theme} = useTheme();
   return (
-    <div css={billItemStyle(theme, hasDragHandle)} {...htmlProps}>
-      <div css={prefixStyle}>
-        {hasDragHandle && <IconButton iconType="buljusa" />}
+    <DragHandleItem {...htmlProps} hasDragHandle={hasDragHandle} backgroundColor="lightGrayContainer">
+      <Flex justifyContent="spaceBetween" width="100%">
         <Text css={textStyle(theme)} size="bodyBold">
           {name}
         </Text>
-      </div>
-      <Text css={textStyle(theme)} size="body">
-        {price.toLocaleString('ko-kr')} 원
-      </Text>
-    </div>
+        <Text css={textStyle(theme)} size="body">
+          {price.toLocaleString('ko-kr')} 원
+        </Text>
+      </Flex>
+    </DragHandleItem>
   );
 };
 export default BillItem;
