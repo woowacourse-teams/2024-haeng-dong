027d951ba22e56270ad4ade1d9bfdc3cc354848c
Soyeon Choe <soy2302ten@gmail.com>
2024-07-23 23:10:21 +0900

    design: HDesignProvider 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import {Outlet} from 'react-router-dom';
+import {HDesignProvider} from 'haengdong-design';
 
 const App: React.FC = () => {
-  return <Outlet />;
+  return (
+    <HDesignProvider>
+      <Outlet />
+    </HDesignProvider>
+  );
 };
 
 export default App;
