ae377d6bdda817f3f28477a4636db3a6cd47ae7f
이태훈 <rhymint@gmail.com>
2024-07-23 22:09:59 +0900

    feat: Flex component backgroundColor 받을 수 있도록 변경

---
+++ HDesign/src/components/Flex/Flex.style.ts
@@ -0,0 +1,34 @@
+import {changeCamelCaseToKebabCase} from '@/utils/ return str.replace(/([a-z])([A-Z])/changeCamelCaseToKebabCase';
+import {FlexDirectionStrictType, FlexProps} from './Flex.type';
+import {css} from '@emotion/react';
+
+export const flexStyle = ({
+  justifyContent = 'flexStart',
+  alignItems = 'stretch',
+  flexDirection = 'row',
+  gap = '0',
+  padding = '0',
+  paddingInline = '0',
+  margin = '0',
+  width = 'auto',
+  height = 'auto',
+  backgroundColor = 'white',
+  theme,
+  ...rest
+}: FlexProps) =>
+  css({
+    display: 'flex',
+    justifyContent: changeCamelCaseToKebabCase(justifyContent),
+    alignItems: changeCamelCaseToKebabCase(alignItems),
+    // TODO: (@weadie) as를 사용하지 않으면 방법이 없음. css의 flexDirection속성은 string을 받지 않고 명확한 속성명(ex row-reverse)를 받고 싶어함. 다만 as를 사용해도 된다고 생각한 근거는 케밥함수가 FlexDirectionType에 명시된 모든 타입을 정확하게 변환하며, 받는 문자열이 FlexDirectionType에 제한되기 때문.
+    flexDirection: changeCamelCaseToKebabCase(flexDirection) as FlexDirectionStrictType,
+    gap,
+    padding,
+    paddingInline,
+    margin,
+    width,
+    height,
+    ...rest,
+
+    backgroundColor: backgroundColor === 'white' ? theme?.colors.white : theme?.colors.grayContainer,
+  });
--- HDesign/src/components/Flex/Flex.tsx
+++ HDesign/src/components/Flex/Flex.tsx
@@ -1,39 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import {StrictPropsWithChildren} from '@/types/strictPropsWithChildren';
-import {changeCamelCaseToKebabCase} from '@/utils/ return str.replace(/([a-z])([A-Z])/changeCamelCaseToKebabCase';
-import {css} from '@emotion/react';
-import {FlexDirectionStrictType, FlexProps} from './Flex.type';
-
-const flexStyle = ({
-  justifyContent = 'flexStart',
-  alignItems = 'stretch',
-  flexDirection = 'row',
-  gap = '0',
-  padding = '0',
-  paddingInline = '0',
-  margin = '0',
-  width = 'auto',
-  height = 'auto',
-  ...rest
-}: FlexProps) =>
-  css({
-    display: 'flex',
-    justifyContent: changeCamelCaseToKebabCase(justifyContent),
-    alignItems: changeCamelCaseToKebabCase(alignItems),
-    // TODO: (@weadie) as를 사용하지 않으면 방법이 없음. css의 flexDirection속성은 string을 받지 않고 명확한 속성명(ex row-reverse)를 받고 싶어함. 다만 as를 사용해도 된다고 생각한 근거는 케밥함수가 FlexDirectionType에 명시된 모든 타입을 정확하게 변환하며, 받는 문자열이 FlexDirectionType에 제한되기 때문.
-    flexDirection: changeCamelCaseToKebabCase(flexDirection) as FlexDirectionStrictType,
-    gap,
-    padding,
-    paddingInline,
-    margin,
-    width,
-    height,
-    ...rest,
-  });
+import {FlexProps} from './Flex.type';
+import {useTheme} from '@/theme/HDesignProvider';
+import {flexStyle} from './Flex.style';
 
 // TODO: (@weadie) 지정된 프롭 말고 다른 프롭도 가져올 수 있게 하자.
 const Flex = ({children, ...props}: StrictPropsWithChildren<FlexProps>) => {
-  return <div css={flexStyle(props)}>{children}</div>;
+  const {theme} = useTheme();
+  return <div css={flexStyle({theme, ...props})}>{children}</div>;
 };
 
 export default Flex;
