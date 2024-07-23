da1a8f2e9737ae2eb2d00c7b6c456db482e0a0c4
이태훈 <rhymint@gmail.com>
2024-07-23 22:24:44 +0900

    feat: MainLayout backgroundColor prop 추가

--- HDesign/src/layouts/MainLayout.tsx
+++ HDesign/src/layouts/MainLayout.tsx
@@ -1,11 +1,24 @@
 import {PropsWithChildren} from 'react';
 import {Flex} from '..';
+import {useTheme} from '@/theme/HDesignProvider';
+type MainLayoutBackground = 'white' | 'gray';
 
-interface MainLayoutInterface extends PropsWithChildren {}
+interface MainLayoutProps extends PropsWithChildren {
+  backgroundColor?: MainLayoutBackground;
+}
 
-export const MainLayout = ({children}: MainLayoutInterface) => {
+export const MainLayout = ({backgroundColor, children}: MainLayoutProps) => {
+  const {theme} = useTheme();
   return (
-    <Flex justifyContent="flexStart" flexDirection="column" margin="1rem 0 0 0" gap="1rem" width="100%" height="100%">
+    <Flex
+      backgroundColor={backgroundColor}
+      justifyContent="flexStart"
+      flexDirection="column"
+      margin="1rem 0 0 0"
+      gap="1rem"
+      width="100%"
+      height="100%"
+    >
       {children}
     </Flex>
   );
