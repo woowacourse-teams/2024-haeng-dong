50e11279936c36366a4ed9542d2c718b573fb324
Soyeon Choe <soy2302ten@gmail.com>
2024-07-23 15:59:43 +0900

    chore: SetInitialParticipatns & SetActionModalContent를 Modal의 index.ts에서 export 추가

--- client/src/components/Modal/index.ts
+++ client/src/components/Modal/index.ts
@@ -1,1 +1,3 @@
 export {default as Modal} from './Modal';
+export {default as SetInitialParticipants} from './SetInitialParticipants/SetInitialParticipants';
+export {default as SetActionModalContent} from './SetActionModalContent/SetActionModalContent';
