3a5768a1b9a51386528043cb4e80bab75d4b94ac
Soyeon Choe <soy2302ten@gmail.com>
2024-07-24 00:44:40 +0900

    design: 지출내역 생성 Modal 내부 퍼블리싱

---
+++ client/src/components/Modal/SetActionModalContent/SetPurchase.style.ts
@@ -0,0 +1,15 @@
+import {css} from '@emotion/react';
+
+export const setPurchaseStyle = () =>
+  css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1.5rem',
+  });
+
+export const setPurchaseInputStyle = () =>
+  css({
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '0.5rem',
+  });
--- client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
+++ client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
@@ -1,6 +1,10 @@
 import {useState} from 'react';
+import {Input, Switch} from 'haengdong-design';
+import {css} from '@emotion/react';
 
 import {PurchaseInformation} from '@pages/Event/Event';
+
+import {setPurchaseInputStyle, setPurchaseStyle} from './SetPurchase.style';
 
 interface SetPurchaseProps {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -28,15 +32,22 @@ const SetPurchase = ({setOpen, setPurchaseInformation}: SetPurchaseProps) => {
   };
 
   return (
-    <>
-      <div style={{display: 'flex', flexDirection: 'column'}}>
-        <input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
-        <input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
+    <div css={setPurchaseStyle}>
+      {/* <Switch/> */}
+      {/* TODO: (@soha) Switch는 고정, Input을 묶은 div는 Scroll */}
+      <div css={setPurchaseStyle}>
+        {/* TODO: (@soha) Input만을 담은 것을 또 묶어준 이유는 Switch 부분은 scroll되지 않고 고정되게 하기 위함임 */}
+        <div css={setPurchaseInputStyle}>
+          <Input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
+          <Input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
+        </div>
+
+        <div css={setPurchaseInputStyle}>
+          <Input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
+          <Input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
+        </div>
       </div>
-      <button style={{backgroundColor: 'lightGreen'}} onClick={addPurchaseInformation}>
-        지출 내역 작성 완료
-      </button>
-    </>
+    </div>
   );
 };
 
