c4418fbcfd54541f517bfe488be27f177dc223ca
pakxe <pigkill40@naver.com>
2024-07-24 02:48:15 +0900

    feat: 홈 페이지 레이아웃 구현

---
+++ client/src/pages/Home/HomeLayout.tsx
@@ -0,0 +1,21 @@
+import {Flex, MainLayout} from 'haengdong-design';
+import {StepListProvider} from '@hooks/useStepList/useStepList';
+import {useEffect} from 'react';
+import HomeContent from './HomeContent';
+
+const HomeLayout = () => {
+  useEffect(() => {}, []);
+
+  return (
+    <StepListProvider>
+      <MainLayout backgroundColor="gray">
+        {/* <TopNav navType="home" /> */}
+        <Flex flexDirection="column" gap="1rem">
+          <HomeContent />
+        </Flex>
+      </MainLayout>
+    </StepListProvider>
+  );
+};
+
+export default HomeLayout;
