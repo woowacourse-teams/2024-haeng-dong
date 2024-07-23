c0d85c3fd6e49bc35772c712dd2d66b91beed6e9
이태훈 <rhymint@gmail.com>
2024-07-24 02:50:15 +0900

    fix: TopNav navigate currentPath를 이용하도록 수정

--- HDesign/src/components/TopNav/TopNav.tsx
+++ HDesign/src/components/TopNav/TopNav.tsx
@@ -1,24 +1,21 @@
 /** @jsxImportSource @emotion/react */
-import {useNavigate} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import TextButton from '../TextButton/TextButton';
 import {topNavNonStyle, topNavStyle} from './TopNav.style';
 import {TopNavProps} from './TopNav.type';
 import Switch from '../Switch/Switch';
-import {useEffect, useState} from 'react';
 
 // TODO: (@todari) navigation으로 인해 storybook 동작하지 않는 오류 해결해야함
 // + 페이지 정하는 것에 따라, navigate 경로 수정해 줘야 함
 const TopNav = ({navType}: TopNavProps) => {
-  const [homeValue, setHomeValue] = useState('홈');
   const navigate = useNavigate();
+  const currentPath = useLocation().pathname;
+
   const PATH_TABLE: Record<string, string> = {
-    홈: '/home',
-    관리: '/admin',
+    홈: `${currentPath}/`,
+    관리: `${currentPath}/admin`,
   };
-  const handleChangeNav = (homeValue: string) => {
-    setHomeValue(homeValue);
-    navigate(PATH_TABLE.homeValue);
-  };
+
   const TopNavByType = () => {
     switch (navType) {
       case 'back':
@@ -33,15 +30,12 @@ const TopNav = ({navType}: TopNavProps) => {
       case 'home':
         return (
           <div css={topNavStyle}>
-            <Switch value={homeValue} values={['홈', '관리']} onChange={value => handleChangeNav(value)}></Switch>{' '}
+            <Switch value="홈" values={['홈', '관리']} onChange={value => navigate(PATH_TABLE[value])}></Switch>{' '}
           </div>
         );
 
-      case 'none':
-        return <div css={topNavNonStyle} />;
-
       default:
-        return null;
+        return <div css={topNavNonStyle} />;
     }
   };
 
