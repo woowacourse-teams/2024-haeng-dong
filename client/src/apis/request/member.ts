043e5ab70f1f25b8dbc9abb389d5cfffe0a7635f
pakxe <pigkill40@naver.com>
2024-07-24 02:44:16 +0900

    feat: 참여자 목록 갱신 api 구현

---
+++ client/src/apis/request/member.ts
@@ -0,0 +1,19 @@
+import {BASE_URL} from '@apis/baseUrl';
+import {TEMP_PREFIX} from '@apis/tempPrefix';
+import {requestPost} from '@apis/fetcher';
+import {WithEventId} from '@apis/withEventId.type';
+import {Member} from 'types/stepList';
+
+type RequestUpdateMemberList = {
+  MemberList: Member[];
+};
+
+export const requestUpdateMemberList = async ({eventId, MemberList}: WithEventId<RequestUpdateMemberList>) => {
+  await requestPost({
+    baseUrl: BASE_URL.HD,
+    endpoint: `${TEMP_PREFIX}/${eventId}/actions/members`,
+    body: {
+      actions: MemberList,
+    },
+  });
+};
