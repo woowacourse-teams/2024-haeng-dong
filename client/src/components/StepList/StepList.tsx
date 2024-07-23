77ad9f88db2dd4938ae2592706ff69fb1fea3f3d
pakxe <pigkill40@naver.com>
2024-07-24 02:45:24 +0900

    feat: 전체 지출 내역 컴포넌트 구현

---
+++ client/src/components/StepList/StepList.tsx
@@ -0,0 +1,24 @@
+import {useStepList} from '@hooks/useStepList/useStepList';
+import {Flex, InOutItem, StepItem} from 'haengdong-design';
+import {MemberType} from 'types/stepList';
+
+const StepList = () => {
+  const {stepList} = useStepList();
+
+  // TODO: (@weadie) if else 구문이 지저분하므로 리펙터링이 필요합니다.
+  return (
+    <Flex flexDirection="column" gap="0.5rem" paddingInline="0.5rem" padding="1rem 0 0 0">
+      {stepList.map(step => {
+        if (step.type === 'BILL') {
+          return <StepItem name={step.stepName} bills={step.actions} personCount={0} />;
+        } else if (step.type === 'IN' || step.type === 'OUT') {
+          return <InOutItem inOutType={step.type === 'IN' ? 'in' : 'out'} names={step.actions.map(({name}) => name)} />;
+        } else {
+          return <></>;
+        }
+      })}
+    </Flex>
+  );
+};
+
+export default StepList;
