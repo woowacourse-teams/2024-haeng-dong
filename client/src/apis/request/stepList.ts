908f9b4189db07b9b3ea0c7fe086d4e69ffb11e2
pakxe <pigkill40@naver.com>
2024-07-24 02:44:52 +0900

    feat: 행사 이력 조회 api 구현

---
+++ client/src/apis/request/stepList.ts
@@ -0,0 +1,14 @@
+import {BASE_URL} from '@apis/baseUrl';
+import {TEMP_PREFIX} from '@apis/tempPrefix';
+import {requestGet, requestPost} from '@apis/fetcher';
+import {WithEventId} from '@apis/withEventId.type';
+import {Bill, Member, StepList} from 'types/stepList';
+
+// TODO: (@weadie) 현재 토큰을 어떻게 관리할지.. 계속 사용되는데
+export const requestStepList = async ({eventId}: WithEventId) => {
+  // TODO: (@weadie) response가 어떻게 오는지 안나와서 data로만 써뒀어요.
+  return await requestGet<StepList>({
+    baseUrl: BASE_URL.HD,
+    endpoint: `${TEMP_PREFIX}/${eventId}/actions`,
+  });
+};
