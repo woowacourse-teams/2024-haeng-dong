2a9a9f597e65faae8c12a63e5585099a61806bb0
pakxe <pigkill40@naver.com>
2024-07-24 13:37:00 +0900

    chore: dotenv 세팅 수정

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ import Dotenv from 'dotenv-webpack';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({path: path.join(__dirname, '.env')});
+// dotenv.config({path: path.join(__dirname, '.env')});
 
 export default {
   mode: 'development',
