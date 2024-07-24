2d00932d1090d63821d4918fa4320d2cf99ef8ce
kunsanglee <gosmdochee2@gmail.com>
2024-07-24 21:37:41 +0900

    refactor: 행사 생성 요청 객체 검증 애너테이션 @Size 제거

--- server/src/main/java/server/haengdong/presentation/request/EventSaveRequest.java
+++ server/src/main/java/server/haengdong/presentation/request/EventSaveRequest.java
@@ -1,13 +1,11 @@
 package server.haengdong.presentation.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import server.haengdong.application.request.EventAppRequest;
 
 public record EventSaveRequest(
 
         @NotBlank
-        @Size(min = 2, max = 20)
         String eventName
 ) {
 
