08e84f906382dbfb6964c9ecff09398ca8d87ed1
Soyeon Choe <soy2302ten@gmail.com>
2024-07-24 14:32:29 +0900

    remove: 사용하지 않는 파일 제거

--- client/src/components/Modal/index.ts
+++ client/src/components/Modal/index.ts
@@ -1,3 +1,2 @@
-export {default as Modal} from './Modal';
 export {default as SetInitialParticipants} from './SetInitialParticipants/SetInitialParticipants';
 export {default as SetActionModalContent} from './SetActionModalContent/SetActionModalContent';
