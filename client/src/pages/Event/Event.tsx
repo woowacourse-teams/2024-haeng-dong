ee7820be813b0965d56e33d079a4c5df84a336c2
Soyeon Choe <soy2302ten@gmail.com>
2024-07-24 14:42:06 +0900

    chore: TopNav 수정된 디자인 시스템 설치 및 StepList 수정 주석 추가

--- client/src/pages/Event/Event.tsx
+++ client/src/pages/Event/Event.tsx
@@ -1,10 +1,9 @@
 import {useState} from 'react';
-import {css} from '@emotion/react';
 import {TopNav, Title, FixedButton, StepItem, InOutItem, MainLayout} from 'haengdong-design';
 
 import {SetActionModalContent, SetInitialParticipants} from '@components/Modal';
 
-import {EventStyle, ReceiptStyle} from './Event.style';
+import {ReceiptStyle} from './Event.style';
 
 export type PurchaseInformation = {
   name: string;
@@ -82,19 +81,28 @@ const Event = () => {
   return (
     <MainLayout backgroundColor="gray">
       <TopNav navType={'home'} />
-      <Title title="행동대장 야유회" description="“초기인원 설정하기” 버튼을 눌러서 행사 초기 인원을 설정해 주세요." />
+      <Title
+        title="행동대장 야유회"
+        description="“초기인원 설정하기” 버튼을 눌러서 행사 초기 인원을 설정해 주세요."
+        // TODO: (@soha) price 생성시 총 가격 생기기
+        price={20000}
+      />
       <section css={ReceiptStyle}>
-        <StepItem
-          name={`${order}차`}
-          personCount={participants.length}
-          bills={[
-            {name: 'QWER', price: 12000, hasDragHandle: true},
-            {name: '배고파요', price: 12000, hasDragHandle: true},
-          ]}
-        />
-        <InOutItem names={['감자', '고구마']} inOutType="out" hasDragHandle={true} />
-        {/* TODO: (@soha) 퍼블리싱을 위해 일단 주석처리 */}
-        {/* {order > 0 && <></>} */}
+        {order > 0 && (
+          // TODO: (@soha) StepList로 변경하기
+          <>
+            <StepItem
+              name={`${order}차`}
+              personCount={participants.length}
+              bills={[
+                {name: 'QWER', price: 12000, hasDragHandle: true},
+                {name: '배고파요', price: 12000, hasDragHandle: true},
+              ]}
+            />
+            <InOutItem names={['감자', '고구마']} inOutType={'OUT'} hasDragHandle={true} />
+          </>
+        )}
+        {/* TODO: (@soha) 추후 버튼 width 화면에 맞게 수정 */}
         <FixedButton
           children={participants.length === 0 ? '초기인원 설정하기' : '행동 추가하기'}
           onClick={() => setOpenBottomSheet(prev => !prev)}
