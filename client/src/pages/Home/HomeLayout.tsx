0a1c12bcfd57c53163d70554a6983dd6074a7b32
pakxe <pigkill40@naver.com>
2024-07-24 13:17:53 +0900

    feat: TopNav 추가

--- client/src/pages/Home/HomeLayout.tsx
+++ client/src/pages/Home/HomeLayout.tsx
@@ -1,16 +1,13 @@
-import {Flex, MainLayout} from 'haengdong-design';
+import {Flex, MainLayout, TopNav} from 'haengdong-design';
 import {StepListProvider} from '@hooks/useStepList/useStepList';
-import {useEffect} from 'react';
 
 import HomeContent from './HomeContent';
 
 const HomeLayout = () => {
-  useEffect(() => {}, []);
-
   return (
     <StepListProvider>
       <MainLayout backgroundColor="gray">
-        {/* <TopNav navType="home" /> */}
+        <TopNav navType="home" />
         <Flex flexDirection="column" gap="1rem">
           <HomeContent />
         </Flex>
