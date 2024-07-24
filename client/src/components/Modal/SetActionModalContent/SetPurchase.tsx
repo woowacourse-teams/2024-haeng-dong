db2d01e0b1799747143b7cbe1df5a4300e34771b
Soyeon Choe <soy2302ten@gmail.com>
2024-07-24 11:38:05 +0900

    design: 지출 내역 생성 Modal 내부 퍼블리싱

--- client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
+++ client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
@@ -1,10 +1,10 @@
 import {useState} from 'react';
-import {Input, Switch} from 'haengdong-design';
+import {Input, FixedButton} from 'haengdong-design';
 import {css} from '@emotion/react';
 
 import {PurchaseInformation} from '@pages/Event/Event';
 
-import {setPurchaseInputStyle, setPurchaseStyle} from './SetPurchase.style';
+import {setPurchaseInputStyle, setPurchaseStyle, setPurchaseInputContainerStyle} from './SetPurchase.style';
 
 interface SetPurchaseProps {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -33,20 +33,13 @@ const SetPurchase = ({setOpen, setPurchaseInformation}: SetPurchaseProps) => {
 
   return (
     <div css={setPurchaseStyle}>
-      {/* <Switch/> */}
-      {/* TODO: (@soha) Switch는 고정, Input을 묶은 div는 Scroll */}
-      <div css={setPurchaseStyle}>
-        {/* TODO: (@soha) Input만을 담은 것을 또 묶어준 이유는 Switch 부분은 scroll되지 않고 고정되게 하기 위함임 */}
-        <div css={setPurchaseInputStyle}>
-          <Input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
-          <Input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
-        </div>
-
+      <div css={setPurchaseInputContainerStyle}>
         <div css={setPurchaseInputStyle}>
           <Input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
           <Input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
         </div>
       </div>
+      <FixedButton variants={'primary'} children={'추가하기'} />
     </div>
   );
 };
