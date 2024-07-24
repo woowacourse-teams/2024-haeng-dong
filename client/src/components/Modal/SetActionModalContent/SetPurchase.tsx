3c1ca04518e9b8a67338f8f53dc04735e1426629
Soyeon Choe <soy2302ten@gmail.com>
2024-07-25 01:21:25 +0900

    feat: 지출 내역에 대한 기능 구현 (useDynamicInputPairs 훅 생성)

--- client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
+++ client/src/components/Modal/SetActionModalContent/SetPurchase.tsx
@@ -1,44 +1,50 @@
-import {useState} from 'react';
 import {Input, FixedButton} from 'haengdong-design';
 
-import {PurchaseInformation} from '@pages/Event/Event';
+import useDynamicInputPairs from '@hooks/useDynamicInputPairs';
 
 import {setPurchaseInputStyle, setPurchaseStyle, setPurchaseInputContainerStyle} from './SetPurchase.style';
 
 interface SetPurchaseProps {
-  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  setPurchaseInformation: any;
-  purchaseInformation: any;
+  setOpenBottomSheet: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SetPurchase = ({setOpen, setPurchaseInformation}: SetPurchaseProps) => {
-  const [newPurchaseInformation, setNewPurchaseInformation] = useState<PurchaseInformation>({
-    name: '',
-    price: 0,
-  });
+const SetPurchase = ({setOpenBottomSheet}: SetPurchaseProps) => {
+  const {inputPairs, inputRefs, handleInputChange, handleInputBlur} = useDynamicInputPairs();
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewPurchaseInformation({...newPurchaseInformation, name: e.target.value});
-  };
-
-  const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNewPurchaseInformation({...newPurchaseInformation, price: parseFloat(e.target.value)});
-  };
-
-  const addPurchaseInformation = () => {
-    setPurchaseInformation((prev: PurchaseInformation[]) => [...prev, newPurchaseInformation]);
-    setOpen(false);
+  const handleSetPurchaseSubmit = () => {
+    // TODO: (@soha) api 요청시 inputPairs를 보내면 됨
+    setOpenBottomSheet(false);
   };
 
   return (
     <div css={setPurchaseStyle}>
       <div css={setPurchaseInputContainerStyle}>
-        <div css={setPurchaseInputStyle}>
-          <Input type="text" value={newPurchaseInformation.name} onChange={handleNameChange} placeholder="지출 내역" />
-          <Input type="number" value={newPurchaseInformation.price} onChange={handlePriceChange} placeholder="금액" />
-        </div>
+        {inputPairs.map((pair, index) => (
+          <div key={index} css={setPurchaseInputStyle}>
+            <Input
+              type="text"
+              value={pair.name}
+              onChange={e => handleInputChange(index, 'name', e.target.value)}
+              onBlur={() => handleInputBlur(index)}
+              placeholder="지출 내역"
+              ref={el => (inputRefs.current[index * 2] = el)}
+            />
+            <Input
+              type="number"
+              value={pair.price}
+              onChange={e => handleInputChange(index, 'price', e.target.value)}
+              onBlur={() => handleInputBlur(index)}
+              placeholder="금액"
+              ref={el => (inputRefs.current[index * 2 + 1] = el)}
+            />
+          </div>
+        ))}
       </div>
-      <FixedButton variants={'primary'} children={'추가하기'} />
+      <FixedButton
+        variants={inputPairs.length - 1 ? 'primary' : 'tertiary'}
+        children={'추가하기'}
+        onClick={handleSetPurchaseSubmit}
+      />
     </div>
   );
 };
