f98c97a2c6a99e8c59042d157c7d6e8647913aba
juha <khabh@naver.com>
2024-07-24 11:20:25 +0900

    refactor: CORS 설정 분리

--- server/src/main/java/server/haengdong/config/WebConfig.java
+++ server/src/main/java/server/haengdong/config/WebConfig.java
@@ -1,20 +1,27 @@
 package server.haengdong.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@RequiredArgsConstructor
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private static final long MAX_AGE_SECS = 3600;
+    @Value("${cors.max-age}")
+    private Long maxAge;
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Override
-    public void addCorsMappings(final CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://haengdong.pro", "https://dev.haengdong.pro")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowedHeaders("*")
-                .maxAge(MAX_AGE_SECS);
+                .maxAge(maxAge);
     }
 }
