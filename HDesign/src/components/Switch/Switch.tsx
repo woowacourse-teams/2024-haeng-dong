4b5a416270c7e30fe2d94c01b4fa6ef3fb5546a0
이태훈 <rhymint@gmail.com>
2024-07-24 14:54:38 +0900

    fix: Switch 내부의 불필요한 로직 제거

--- HDesign/src/components/Switch/Switch.tsx
+++ HDesign/src/components/Switch/Switch.tsx
@@ -2,19 +2,16 @@
 import TextButton from '../TextButton/TextButton';
 import {switchContainerStyle} from './Switch.style';
 import {SwitchProps} from './Switch.type';
-import {useSwitch} from './useSwitch';
 
 const Switch = ({value, values, onChange}: SwitchProps) => {
-  const {selectedValue, handleClick} = useSwitch({value, values, onChange});
-
   return (
     <div css={switchContainerStyle}>
       {values.map((item, index) => (
         <TextButton
           key={`${index}_${item}`}
-          textColor={selectedValue === item ? 'black' : 'gray'}
+          textColor={value === item ? 'black' : 'gray'}
           textSize="bodyBold"
-          onClick={() => handleClick(index)}
+          onClick={() => onChange(values[index])}
         >
           {item}
         </TextButton>
