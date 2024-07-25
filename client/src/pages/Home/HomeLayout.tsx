84d2cc7703d1bbb3e1768d7be492bf97ed00b486
pakxe <pigkill40@naver.com>
2024-07-25 16:15:13 +0900

    feat: 디자인시스템 수정에 다른 컴포넌트 호출 형태 변경

--- client/src/pages/Home/HomeLayout.tsx
+++ client/src/pages/Home/HomeLayout.tsx
@@ -1,14 +1,31 @@
-import {Flex, MainLayout, TopNav} from 'haengdong-design';
+import {Flex, MainLayout, TopNav, NavSwitch} from 'haengdong-design';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 import {StepListProvider} from '@hooks/useStepList/useStepList';
 
 import HomeContent from './HomeContent';
 
 const HomeLayout = () => {
+  const navigate = useNavigate();
+  const basePath = location.pathname.split('/').slice(0, -1);
+
+  const PATH_TABLE: Record<string, string> = {
+    홈: 'home',
+    관리: 'admin',
+  };
+
   return (
     <StepListProvider>
       <MainLayout backgroundColor="gray">
-        <TopNav navType="home" />
+        <TopNav>
+          <NavSwitch
+            paths={['홈', '관리']}
+            defaultPath={'홈'}
+            onChange={(value: string) => {
+              navigate(PATH_TABLE[value]);
+            }}
+          />
+        </TopNav>
         <Flex flexDirection="column" gap="1rem">
           <HomeContent />
         </Flex>
