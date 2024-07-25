07e5e492469d4072f8f1a3800124472e45831e5d
pakxe <pigkill40@naver.com>
2024-07-25 16:11:03 +0900

    feat: 지출 내역, 인원 조정 api연결 후 provider로 전파

--- client/src/hooks/useStepList/useStepList.tsx
+++ client/src/hooks/useStepList/useStepList.tsx
@@ -1,25 +1,49 @@
 import {PropsWithChildren, createContext, useEffect, useState} from 'react';
 
-import {BillAction, MemberType, StepList} from 'types/stepList';
+import {Bill, BillAction, MemberType, StepList} from 'types/stepList';
+import {requestStepList} from '@apis/request/stepList';
+import useEventId from '@hooks/useEventId/useEventId';
+import {requestAddBillList} from '@apis/request/bill';
+import {requestUpdateMemberList} from '@apis/request/member';
 
 import stepListJsonData from '@mocks/stepList.json';
+
+interface StepListContext {
+  stepList: StepList;
+  getTotalPrice: () => number;
+  addBill: (billList: Bill[]) => Promise<void>;
+  updateMemberList: ({type, memberNameList}: {type: MemberType; memberNameList: string[]}) => Promise<void>;
+}
 
 const stepListMockData = stepListJsonData as StepList;
 
 const useStepList = () => {
   const [stepList, setStepList] = useState<StepList>(stepListMockData);
+  const {eventId} = useEventId();
 
   useEffect(() => {
+    if (eventId === '') return;
+
+    const getStepList = async () => {
+      // TODO: (@weadie) 아직 구현 안된 api입니다.
+      // const stepList = await requestStepList({eventId});
+      console.log(stepList);
+
+      setStepList(stepList);
+    };
+
+    getStepList();
     // TODO: (@weadie) useEffect를 꼭 써야하는가?
     // 초기 리스트 불러서 setActionList
-  }, []);
+  }, [eventId]);
 
-  const updateMemberList = (type: MemberType, memberList: string[]) => {
-    // api를 호출하고 set한다
+  const updateMemberList = async ({type, memberNameList}: {type: MemberType; memberNameList: string[]}) => {
+    await requestUpdateMemberList({eventId, type, memberNameList});
   };
 
-  const addBill = (title: string, price: number) => {
-    // api를 호출하고 set한다.
+  const addBill = async (billList: Bill[]) => {
+    // TODO: (@weadie) 에러 처리
+    await requestAddBillList({eventId, billList});
   };
 
   const calculateBillSum = (actions: BillAction[]) => {
@@ -35,15 +59,16 @@ const useStepList = () => {
     }, 0);
   };
 
-  return {stepList, getTotalPrice};
+  return {stepList, getTotalPrice, addBill, updateMemberList};
 };
 
-const StepListContext = createContext<StepList>([]); // TODO: (@weadie) 인자를 어떻게 줘야 하는지 고민하기.
+// TODO: (@soha) any 수정
+export const StepListContext = createContext<StepListContext | undefined>([]); // TODO: (@weadie) 인자를 어떻게 줘야 하는지 고민하기.
 
 const StepListProvider = ({children}: PropsWithChildren) => {
-  const {stepList} = useStepList();
+  const stepListProps = useStepList();
 
-  return <StepListContext.Provider value={stepList}>{children}</StepListContext.Provider>;
+  return <StepListContext.Provider value={stepListProps}>{children}</StepListContext.Provider>;
 };
 
 export {useStepList, StepListProvider};
