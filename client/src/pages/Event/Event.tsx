1d7bbfaf8c81cb01b3a7e0e14a96fd4147144b18
pakxe <pigkill40@naver.com>
2024-07-25 16:14:20 +0900

    feat: useContext를 사용해 총 가격을 불러오도록 연결

--- client/src/pages/Event/Event.tsx
+++ client/src/pages/Event/Event.tsx
@@ -1,16 +1,18 @@
-import {useState} from 'react';
-import {TopNav, Title, FixedButton, StepItem, InOutItem, MainLayout} from 'haengdong-design';
+import {useContext, useEffect, useState} from 'react';
+import {TopNav, Title, FixedButton, MainLayout, NavSwitch} from 'haengdong-design';
+import {useNavigate} from 'react-router-dom';
 
 import StepList from '@components/StepList/StepList';
-import {useStepList} from '@hooks/useStepList/useStepList';
-import {StepListProvider} from '@hooks/useStepList/useStepList';
+import {StepListContext} from '@hooks/useStepList/useStepList';
+import {requestGetEventName} from '@apis/request/event';
+import useEventId from '@hooks/useEventId/useEventId';
 
 import {SetActionModalContent, SetInitialParticipants} from '@components/Modal';
 
 import {ReceiptStyle} from './Event.style';
 
 export type PurchaseInformation = {
-  name: string;
+  title: string;
   price: number;
 };
 
@@ -64,36 +66,69 @@ const Event = () => {
   const [participants, setParticipants] = useState<string[]>([]);
   const [order, setOrder] = useState<number>(0);
 
-  const {getTotalPrice} = useStepList();
+  // TODO: (@weadie) eventName이 새로고침시 공간이 없다가 생겨나 레이아웃이 움직이는 문제
+  const [eventName, setEventName] = useState(' ');
+  const navigate = useNavigate();
+
+  const {getTotalPrice} = useContext(StepListContext);
+
+  const {eventId} = useEventId();
+
+  // TODO: (@weadie) 아래 로직을 훅으로 분리합니다.
+  useEffect(() => {
+    if (eventId === '') return;
+
+    const getEventName = async () => {
+      const {eventName} = await requestGetEventName({eventId: eventId ?? ''});
+
+      setEventName(eventName);
+    };
+
+    getEventName();
+  }, [eventId]);
+
+  const basePath = location.pathname.split('/').slice(0, -1).join('/');
+
+  const PATH_TABLE: Record<string, string> = {
+    홈: 'home',
+    관리: 'admin',
+  };
 
   return (
     <MainLayout backgroundColor="gray">
-      <TopNav navType={'home'} />
+      <TopNav>
+        <NavSwitch
+          paths={['홈', '관리']}
+          defaultPath={'홈'}
+          onChange={(value: string) => {
+            navigate(`${basePath}/${PATH_TABLE[value]}`);
+          }}
+        />
+      </TopNav>
       <Title
-        title="행동대장 야유회"
+        title={eventName}
         description="“초기인원 설정하기” 버튼을 눌러서 행사 초기 인원을 설정해 주세요."
         price={getTotalPrice()}
       />
       <section css={ReceiptStyle}>
         {order > 0 && (
           //  TODO: (@soha) order가 0일때 기본 Step 뜨기
-          <StepListProvider>
-            <StepList />
-          </StepListProvider>
+          <StepList />
         )}
         {/* TODO: (@soha) 추후 버튼 width 화면에 맞게 수정 */}
         <FixedButton
           children={participants.length === 0 ? '초기인원 설정하기' : '행동 추가하기'}
           onClick={() => setOpenBottomSheet(prev => !prev)}
         />
-        {openBottomSheet &&
-          ModalRendering({
-            participants,
-            setOrder,
-            setParticipants,
-            setOpenBottomSheet,
-            openBottomSheet,
-          })}
+        {openBottomSheet && (
+          <ModalRendering
+            participants={participants}
+            setOrder={setOrder}
+            setParticipants={setParticipants}
+            setOpenBottomSheet={setOpenBottomSheet}
+            openBottomSheet={openBottomSheet}
+          />
+        )}
       </section>
     </MainLayout>
   );
