b7b7d6f6f08aa580078c1b508f5c125eeec1df5e
pakxe <pigkill40@naver.com>
2024-07-25 19:04:28 +0900

    feat: steps를 꺼내서 return하도록 수정

--- client/src/apis/request/stepList.ts
+++ client/src/apis/request/stepList.ts
@@ -8,8 +8,10 @@ import {WithEventId} from '@apis/withEventId.type';
 // TODO: (@weadie) 현재 토큰을 어떻게 관리할지.. 계속 사용되는데
 export const requestStepList = async ({eventId}: WithEventId) => {
   // TODO: (@weadie) response가 어떻게 오는지 안나와서 data로만 써뒀어요.
-  return await requestGet<StepList>({
+  const {steps} = await requestGet<StepList>({
     baseUrl: BASE_URL.HD,
     endpoint: `${TEMP_PREFIX}/${eventId}/actions`,
   });
+
+  return steps;
 };
