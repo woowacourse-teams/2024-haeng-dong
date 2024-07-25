6dc8d8ba4623dc93e3e512fef1677d5819b5f40b
pakxe <pigkill40@naver.com>
2024-07-25 17:52:04 +0900

    feat: TopNav 추가

--- client/src/pages/Event/EventLayout.tsx
+++ client/src/pages/Event/EventLayout.tsx
@@ -1,13 +1,19 @@
-import {PropsWithChildren} from 'react';
-
-import {StepListProvider} from '@hooks/useStepList/useStepList';
-
-import Event from './Event';
+import {MainLayout, TopNav, Switch} from 'haengdong-design';
+import StepListProvider from '@hooks/useStepList/useStepList';
+import {Outlet} from 'react-router-dom';
+import useNavSwitch from '@hooks/useNavSwitch';
 
 const EventLayout = () => {
+  const {nav, paths, onChange} = useNavSwitch();
+
   return (
     <StepListProvider>
-      <Event />
+      <MainLayout backgroundColor="gray">
+        <TopNav>
+          <Switch value={nav} values={paths} onChange={onChange} />
+        </TopNav>
+        <Outlet />
+      </MainLayout>
     </StepListProvider>
   );
 };
