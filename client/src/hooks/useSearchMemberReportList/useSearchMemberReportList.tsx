28bb18f81db1e14191201edb9ee430b3714a034f
pakxe <pigkill40@naver.com>
2024-07-26 13:01:08 +0900

    fix: eventId, 전체 검색 결과에 따라 검색 결과가 보여지도록 수정

--- client/src/hooks/useSearchMemberReportList/useSearchMemberReportList.tsx
+++ client/src/hooks/useSearchMemberReportList/useSearchMemberReportList.tsx
@@ -6,6 +6,7 @@ import {requestMemberReportList} from '@apis/request/report';
 import {WithEventId} from '@apis/withEventId.type';
 
 import memberReportSearchJsonList from '@mocks/memberReportSearchList.json';
+import useEventId from '@hooks/useEventId/useEventId';
 
 const memberReportSearchMockList = memberReportSearchJsonList as MemberReport[];
 
@@ -13,29 +14,31 @@ type UseSearchMemberReportListParams = {
   name: string;
 };
 
-const useSearchMemberReportList = ({name, eventId}: WithEventId<UseSearchMemberReportListParams>) => {
-  const [memberReportList, setMemberReportList] = useState<MemberReport[]>(memberReportSearchMockList);
+const useSearchMemberReportList = ({name}: UseSearchMemberReportListParams) => {
+  const [memberReportList, setMemberReportList] = useState<MemberReport[]>([]);
   const [memberReportSearchList, setMemberReportSearchList] = useState<MemberReport[]>([]);
+  const {eventId} = useEventId();
 
   useEffect(() => {
     const fetchMemberReportList = async () => {
       // TODO: (@weadie) cors 고쳐지면 주석 풀게요.
-      // const memberReportListData = await requestMemberReportList({eventId});
+      // TODO: (@weadie) eventId에 의존하는 두 개의 훅에 대한 리펙토링 필요
+      if (eventId === '') return;
 
-      const memberReportListData = memberReportSearchMockList;
+      const memberReportListData = await requestMemberReportList({eventId});
 
       setMemberReportList(memberReportListData);
     };
 
     fetchMemberReportList();
-  }, []);
+  }, [eventId]);
 
   // TODO: (@weadie) 글자가 완성될 때마다 아래 로직이 실행되어야 합니다.
   useEffect(() => {
     if (name === '') setMemberReportSearchList(memberReportList);
 
     setMemberReportSearchList(memberReportList.filter(memberReport => memberReport.name.includes(name)));
-  }, [name]);
+  }, [name, memberReportList]);
 
   return {
     memberReportSearchList,
