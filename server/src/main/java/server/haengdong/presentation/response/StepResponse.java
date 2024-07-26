0bd9b88cf7e31b7cd2a1fcda7f4c522d18cfb36d
Arachneee <jhg2819@naver.com>
2024-07-27 00:09:57 +0900

    refactor: 액션 이력 조회 response 수정

--- server/src/main/java/server/haengdong/presentation/response/StepResponse.java
+++ server/src/main/java/server/haengdong/presentation/response/StepResponse.java
@@ -1,57 +1,27 @@
 package server.haengdong.presentation.response;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import server.haengdong.application.response.ActionAppResponse;
 
 public record StepResponse(
-        List<ActionsResponse> steps
+        String type,
+        String stepName,
+        List<String> members,
+        List<ActionResponse> actions
 ) {
-
-    public static StepResponse of(List<ActionAppResponse> actions) {
-        if (actions.isEmpty()) {
-            return new StepResponse(List.of());
-        }
-        List<ActionsResponse> actionsResponse = new ArrayList<>();
-        Set<String> members = new HashSet<>();
-        ActionAppResponse firstAction = getFirstAction(actions);
-        List<ActionAppResponse> group = new ArrayList<>();
-        group.add(firstAction);
-        String currentActionType = firstAction.actionTypeName();
-        members.add(firstAction.name());
-
-        for (int i = 1; i < actions.size(); i++) {
-            ActionAppResponse action = actions.get(i);
-            String typeName = action.actionTypeName();
-            if (currentActionType.equals(typeName)) {
-                if (typeName.equals("IN")) {
-                    members.add(action.name());
-                }
-                if (typeName.equals("OUT")) {
-                    members.remove(action.name());
-                }
-                group.add(action);
-                continue;
-            }
-            actionsResponse.add(ActionsResponse.of(group, members));
-            currentActionType = typeName;
-            group.clear();
-            if (typeName.equals("IN")) {
-                members.add(action.name());
-            }
-            if (typeName.equals("OUT")) {
-                members.remove(action.name());
-            }
-            group.add(action);
-        }
-        actionsResponse.add(ActionsResponse.of(group, members));
-
-        return new StepResponse(actionsResponse);
+    public static StepResponse of(List<ActionAppResponse> actions, List<String> members, String stepName) {
+        return new StepResponse(
+                actions.get(0).actionTypeName(),
+                stepName,
+                new ArrayList<>(members),
+                toActionsResponse(actions)
+        );
     }
 
-    private static ActionAppResponse getFirstAction(List<ActionAppResponse> actions) {
-        return actions.get(0);
+    private static List<ActionResponse> toActionsResponse(List<ActionAppResponse> actions) {
+        return actions.stream()
+                .map(ActionResponse::of)
+                .toList();
     }
 }
---
+++ server/src/main/java/server/haengdong/presentation/response/StepsResponse.java
@@ -0,0 +1,55 @@
+package server.haengdong.presentation.response;
+
+import java.util.ArrayList;
+import java.util.List;
+import server.haengdong.application.response.ActionAppResponse;
+import server.haengdong.application.response.ActionAppResponse.ActionType;
+
+public record StepsResponse(List<StepResponse> steps) {
+
+    public static StepsResponse of(List<ActionAppResponse> actions) {
+        List<StepResponse> steps = new ArrayList<>();
+        List<String> currentMembers = new ArrayList<>();
+        List<List<ActionAppResponse>> groups = createGroups(actions);
+
+        int billGroupCount = 0;
+        for (List<ActionAppResponse> group : groups) {
+            changeCurrentMembers(group, currentMembers);
+            if (group.get(0).actionType() == ActionType.BILL) {
+                billGroupCount++;
+            }
+            StepResponse stepResponse = StepResponse.of(group, currentMembers, billGroupCount + "차");
+            steps.add(stepResponse);
+        }
+        return new StepsResponse(steps);
+    }
+
+    private static List<List<ActionAppResponse>> createGroups(List<ActionAppResponse> actions) {
+        List<List<ActionAppResponse>> groups = new ArrayList<>();
+
+        for (ActionAppResponse action : actions) {
+            if (groups.isEmpty() || isActionTypeChange(action, groups)) {
+                groups.add(new ArrayList<>());
+            }
+            groups.get(groups.size() - 1).add(action);
+        }
+        return groups;
+    }
+
+    private static boolean isActionTypeChange(ActionAppResponse action, List<List<ActionAppResponse>> groups) {
+        List<ActionAppResponse> currentGroup = groups.get(groups.size() - 1);
+        return currentGroup.get(0).actionType() != action.actionType();
+    }
+
+    private static void changeCurrentMembers(List<ActionAppResponse> group, List<String> currentMembers) {
+        for (ActionAppResponse action : group) {
+            if (action.actionType() == ActionType.IN) {
+                currentMembers.add(action.name());
+                continue;
+            }
+            if (action.actionType() == ActionType.OUT) {
+                currentMembers.remove(action.name());
+            }
+        }
+    }
+}
