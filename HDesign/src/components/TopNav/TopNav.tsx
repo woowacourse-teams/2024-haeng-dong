8dd3ecdda920ed2f0988de0421e427f104ac719d
jinho_kim98 <rlawlsgh1227@gmail.com>
2024-07-27 23:27:49 +0900

    fix: children이 필수가 아니도록 변경

--- HDesign/src/components/TopNav/TopNav.tsx
+++ HDesign/src/components/TopNav/TopNav.tsx
@@ -2,19 +2,15 @@
 import React from 'react';
 import {topNavNonStyle, topNavStyle} from './TopNav.style';
 import Back from './Back';
-import NavSwitch from './NavSwitch';
 import Switch from '@components/Switch/Switch';
 
 const TopNav: React.FC<React.PropsWithChildren> = ({children}) => {
   const hasBack = React.Children.toArray(children).some(child => React.isValidElement(child) && child.type === Back);
-  const hasNavSwitch = React.Children.toArray(children).some(
-    child => React.isValidElement(child) && child.type === NavSwitch,
-  );
   const hasSwitch = React.Children.toArray(children).some(
     child => React.isValidElement(child) && child.type === Switch,
   );
 
-  const isExistNav = hasBack || hasNavSwitch || hasSwitch;
+  const isExistNav = hasBack || hasSwitch;
 
   return <div css={isExistNav ? topNavStyle : topNavNonStyle}>{children}</div>;
 };
