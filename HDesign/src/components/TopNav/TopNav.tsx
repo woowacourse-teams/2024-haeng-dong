a7583169670cca8a6479a77fdc8e0642f4579932
jinho_kim98 <rlawlsgh1227@gmail.com>
2024-07-27 23:22:17 +0900

    fix: TopNav가 children이 없어도 작동하도록 변경

--- HDesign/src/components/TopNav/TopNav.tsx
+++ HDesign/src/components/TopNav/TopNav.tsx
@@ -1,17 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
 import {topNavNonStyle, topNavStyle} from './TopNav.style';
-import {StrictPropsWithChildren} from '@/types/strictPropsWithChildren';
 import Back from './Back';
 import NavSwitch from './NavSwitch';
+import Switch from '@components/Switch/Switch';
 
-const TopNav: React.FC<StrictPropsWithChildren> = ({children}) => {
+const TopNav: React.FC<React.PropsWithChildren> = ({children}) => {
   const hasBack = React.Children.toArray(children).some(child => React.isValidElement(child) && child.type === Back);
   const hasNavSwitch = React.Children.toArray(children).some(
     child => React.isValidElement(child) && child.type === NavSwitch,
   );
+  const hasSwitch = React.Children.toArray(children).some(
+    child => React.isValidElement(child) && child.type === Switch,
+  );
 
-  const isExistNav = hasBack || hasNavSwitch;
+  const isExistNav = hasBack || hasNavSwitch || hasSwitch;
 
   return <div css={isExistNav ? topNavStyle : topNavNonStyle}>{children}</div>;
 };
