48b8db4a1099b7905e2232a99e6ce8d42bd1cef4
Soyeon Choe <soy2302ten@gmail.com>
2024-07-30 15:59:20 +0900

    chore: error 및 trash svg 파일 추가
    Co-Authored-By: TaehunLee <85233397+Todari@users.noreply.github.com>
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- HDesign/src/assets/index.ts
+++ HDesign/src/assets/index.ts
@@ -2,3 +2,5 @@ export {default as InputDelete} from '@assets/inputDelete.svg';
 export {default as Plus} from '@assets/plus.svg';
 export {default as Buljusa} from '@assets/buljusa.svg';
 export {default as Arrow} from '@assets/arrow.svg';
+export {default as Error} from '@assets/error.svg';
+export {default as Trash} from '@assets/trash.svg';
