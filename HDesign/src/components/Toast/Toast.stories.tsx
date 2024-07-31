5b1b69a6e48806ae2ab7ef81fd762cb8223a3daa
pakxe <pigkill40@naver.com>
2024-07-31 13:54:45 +0900

    test: 다양한 모습의 토스트를 스토리북으로 시연

--- HDesign/src/components/Toast/Toast.stories.tsx
+++ HDesign/src/components/Toast/Toast.stories.tsx
@@ -1,9 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import type {Meta, StoryObj} from '@storybook/react';
-
 import Toast from '@components/Toast/Toast';
 
-// TODO: (@cookie) 스토리북 야무지게 꾸미기
 const meta = {
   title: 'Components/Toast',
   component: Toast,
@@ -12,12 +10,12 @@ const meta = {
     // layout: 'centered',
   },
   args: {
+    type: 'confirm',
     position: 'top',
     top: '80px',
     message: `서버 오류로 인해 인원을 설정하는데 실패했어요.
 두글자면 이렇게 보여요.`,
-    showingTime: 1000,
-    alwaysShow: true,
+    onUndo: () => alert('되돌리기 버튼이 눌렸습니다. 실행할 로직을 전달해주세요'),
   },
 } satisfies Meta<typeof Toast>;
 
@@ -25,4 +23,49 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Playground: Story = {};
+export const ConfirmToast: Story = {
+  args: {
+    ...meta.args,
+    type: 'confirm',
+    top: '80px',
+    message: `님 이거 다 작성했는데, 혹시 되돌림?
+    되돌릴 수도 있음 ㅇㅇ 굿`,
+  },
+};
+
+export const ConfirmToastWithoutUndo: Story = {
+  args: {
+    ...meta.args,
+    onUndo: undefined,
+    top: '160px',
+  },
+};
+
+export const ErrorToast: Story = {
+  args: {
+    ...meta.args,
+    top: '240px',
+    type: 'error',
+    message: `님 이거 다 작성했는데, 혹시 되돌림?
+    되돌릴 수도 있음 ㅇㅇ 굿`,
+  },
+};
+
+export const ErrorToastWithoutUndo: Story = {
+  args: {
+    ...meta.args,
+    top: '320px',
+    onUndo: undefined,
+    type: 'error',
+  },
+};
+
+export const NoneToast: Story = {
+  args: {
+    ...meta.args,
+    top: '400px',
+    onUndo: undefined,
+    type: 'none',
+    message: '웨디는 커비의 먹잇감인가요? 그치만 감자는 웨디한테 먹힘 쿠스쿠스 ㅋ',
+  },
+};
