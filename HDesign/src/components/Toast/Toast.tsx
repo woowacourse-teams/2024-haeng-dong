9ee5e30b0a324937dbb8ee2b9f899e27ea489f52
pakxe <pigkill40@naver.com>
2024-07-31 13:44:24 +0900

    feat: 되돌리기 버튼과 클릭했을 시 닫히도록 하는 기능 추가
    Co-authored-by: JinHo Kim <jinhokim98@users.noreply.github.com>

--- HDesign/src/components/Toast/Toast.tsx
+++ HDesign/src/components/Toast/Toast.tsx
@@ -2,32 +2,66 @@
 import {createPortal} from 'react-dom';
 
 import Text from '@components/Text/Text';
-
 import ErrorIcon from '@assets/error.svg';
 import ConfirmIcon from '@assets/confirm.svg';
 
 import {useTheme} from '@theme/HDesignProvider';
-
 import {toastStyle, textStyle, toastMarginStyle} from './Toast.style';
-import {ToastProps} from './Toast.type';
-import useToast from './useToast';
+import {ToastProps, ToastType} from './Toast.type';
 import Flex from '@components/Flex/Flex';
+import Button from '../Button/Button';
 
-const Toast = ({position, top, bottom, message, showingTime, alwaysShow, ...htmlProps}: ToastProps) => {
+const renderIcon = (type: ToastType) => {
+  switch (type) {
+    case 'error':
+      return <ErrorIcon />;
+
+    case 'confirm':
+      return <ConfirmIcon />;
+
+    case 'none':
+      return null;
+
+    default:
+      return null;
+  }
+};
+
+const Toast = ({
+  type = 'confirm',
+  top = '0px',
+  bottom = '0px',
+  isClickToClose = true,
+  position,
+  message,
+  onUndo,
+  onClose,
+  ...htmlProps
+}: ToastProps) => {
   const {theme} = useTheme();
-  const {isShow} = useToast({message, showingTime, alwaysShow});
   const styleProps = {position, top, bottom};
 
-  // TODO: (@cookie) toast를 위한 시멘틱 태그 알아보기
+  const handleClickToClose = () => {
+    if (!isClickToClose || !onClose) return;
+
+    onClose();
+  };
+
   return createPortal(
-    <div css={toastMarginStyle({...styleProps, isShow})} {...htmlProps}>
+    <div css={toastMarginStyle({...styleProps})} {...htmlProps} onClick={handleClickToClose}>
       <div css={toastStyle(theme)}>
-        <Flex alignItems="center" gap="0.5rem">
-          <ConfirmIcon />
-          {/* <ErrorIcon /> */}
-          <Text size="smallBodyBold" css={textStyle(theme)}>
-            {message}
-          </Text>
+        <Flex justifyContent="spaceBetween" alignItems="center">
+          <Flex alignItems="center" gap="0.5rem">
+            {renderIcon(type)}
+            <Text size="smallBodyBold" css={textStyle(theme)}>
+              {message}
+            </Text>
+          </Flex>
+          {onUndo && (
+            <Button variants="tertiary" size="small" onClick={onUndo}>
+              되돌리기
+            </Button>
+          )}
         </Flex>
       </div>
     </div>,
