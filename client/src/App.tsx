89fd48a9150df4aebc296ffc83d3aea87eed0d05
김진호 <rlawlsgh1227@gmail.com>
2024-07-31 17:55:20 +0900

    refactor: global style app과 index에 적용
    Co-authored-by: Pakxe <pakxe@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
-import {HDesignProvider} from 'haengdong-design';
 import {Outlet} from 'react-router-dom';
+import {HDesignProvider} from 'haengdong-design';
+import {Global} from '@emotion/react';
+
+import {GlobalStyle} from './GlobalStyle';
 
 const App: React.FC = () => {
   return (
     <HDesignProvider>
+      <Global styles={GlobalStyle} />
       <Outlet />
     </HDesignProvider>
   );
