4b29905ca5cd87b742a4dcc7bc56c8a0c7849e98
pakxe <pigkill40@naver.com>
2024-08-01 21:44:54 +0900

    feat: StepList에 사용되는 타입 파일 작성

---
+++ client/src/types/stepList.ts
@@ -0,0 +1,54 @@
+export type MemberType = 'IN' | 'OUT';
+
+export type InOutType = '늦참' | '탈주';
+
+export type MemberReport = {
+  name: string;
+  price: number;
+};
+
+export type Bill = {
+  title: string;
+  price: number;
+};
+
+type StepBase = {
+  members: string[];
+};
+
+export type MemberStep = StepBase & {
+  type: MemberType;
+  stepName: null;
+  actions: MemberAction[];
+};
+
+export type BillStep = StepBase & {
+  type: 'BILL';
+  stepName: string;
+  actions: BillAction[];
+};
+
+// TODO: (@weadie) 준 데이터 형식에서 steps를 빼내 flat하게 사용중. 일관성있게 하는게 좋긴 하나 사용시 번거로움이 있을 거라고 판단.
+export type StepList = {
+  steps: (MemberStep | BillStep)[];
+};
+
+export type Action = {
+  actionId: number;
+  name: string;
+  price: number | null;
+  sequence: number;
+};
+
+export type BillAction = Omit<Action, 'price'> & {
+  price: number;
+};
+
+export type MemberAction = Omit<Action, 'price'> & {
+  price: null;
+};
+
+export type Member = {
+  name: string;
+  status: MemberType;
+};
