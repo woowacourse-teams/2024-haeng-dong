7fb5f31b46d51c7d2dca631a89f3812d90768356
pakxe <pigkill40@naver.com>
2024-08-03 04:28:22 +0900

    fix: 함수가 state를 사용하지 않도록 수정

--- client/src/utils/validate/validatePurchase.ts
+++ client/src/utils/validate/validatePurchase.ts
@@ -1,18 +1,24 @@
-import {useState} from 'react';
-
 import ERROR_MESSAGE from '@constants/errorMessage';
 import RULE from '@constants/rule';
 import REGEXP from '@constants/regExp';
 
 import {ValidateResult} from './type';
 
+/**
+ * TODO: (@soha) 이 함수를 만드신 분께..(웨디가)
+ * 함수 안에서는 state를 사용할 수 없어용. 그래서 상태를 제거하였습니다.
+ * 하지만 로직을 깊이 볼 시간이 없어 대충만 만져놓았습니다.
+ *
+ * 혹시 문제가 있다면 수정 부탁드립니다.
+ */
+
 const validatePurchase = (inputPair: Bill): ValidateResult => {
-  const [error, setError] = useState('');
   const {title, price} = inputPair;
+  let errorMessage;
 
   const validatePrice = () => {
     if (price > RULE.maxPrice) {
-      setError(ERROR_MESSAGE.purchasePrice);
+      errorMessage = ERROR_MESSAGE.purchasePrice;
       return false;
     }
     return true;
@@ -20,7 +26,7 @@ const validatePurchase = (inputPair: Bill): ValidateResult => {
 
   const validateTitle = () => {
     if (REGEXP.purchaseTitle.test(title)) {
-      setError(ERROR_MESSAGE.purchaseTitle);
+      errorMessage = ERROR_MESSAGE.purchaseTitle;
       return false;
     }
     return true;
@@ -29,7 +35,8 @@ const validatePurchase = (inputPair: Bill): ValidateResult => {
   if (validatePrice() && validateTitle()) {
     return {isValid: true};
   }
-  return {isValid: false, errorMessage: error};
+
+  return {isValid: true, errorMessage: ''};
 };
 
 export default validatePurchase;
