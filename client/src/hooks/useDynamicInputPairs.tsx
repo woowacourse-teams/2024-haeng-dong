a4942e87dde9903d00c4a9b1c6fc61565d00afd2
pakxe <pigkill40@naver.com>
2024-08-03 04:19:18 +0900

    fix: 인덱스틀 사용해 인풋 쌍을 관리하도록 수정. 인덱스를 사용함에 따라 모든 함수도 인덱스를 사용하도록 수정

--- client/src/hooks/useDynamicInputPairs.tsx
+++ client/src/hooks/useDynamicInputPairs.tsx
@@ -2,31 +2,98 @@ import {useEffect, useRef, useState} from 'react';
 
 import {ValidateResult} from '@utils/validate/type';
 
-const useDynamicInputPairs = (validateFunc: (inputPair: Bill) => ValidateResult) => {
-  const [inputPairs, setInputPairs] = useState<Bill[]>([{title: '', price: 0}]);
-  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+type InputPair = Omit<Bill, 'price'> & {
+  price: string;
+  index: number;
+};
+
+type BillInputType = 'title' | 'price';
+
+// TODO: (@weadie) 지나치게 도메인에 묶여있는 인풋. 절대 다른 페어인풋으로 재사용할 수 없다.
+const useDynamicBillActionInput = (validateFunc: (inputPair: Bill) => ValidateResult) => {
+  const [inputPairList, setInputPairList] = useState<InputPair[]>([{title: '', price: '', index: 0}]);
+  const inputRefList = useRef<(HTMLInputElement | null)[]>([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [canSubmit, setCanSubmit] = useState(false);
 
-  const handleInputChange = (index: number, field: 'title' | 'price', event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    const currentPair = inputPairs[index];
-    const validation = validateFunc(currentPair);
+  useEffect(() => {
+    if (inputRefList.current.length > 0) {
+      const lastInputPair = inputRefList.current.slice(-2);
+      lastInputPair.forEach(ref => ref?.scrollIntoView({behavior: 'smooth', block: 'center'}));
+    }
+  }, [inputPairList]);
 
-    if (validation.isValid) {
-      const newInputPairs = [...inputPairs];
-      newInputPairs[index] = {
-        ...newInputPairs[index],
-        [field]: field === 'price' ? parseFloat(value) : value,
-      };
-      setInputPairs(newInputPairs);
+  const handleInputChange = (index: number, field: BillInputType, event: React.ChangeEvent<HTMLInputElement>) => {
+    const {value} = event.target;
+    const targetInputPair = findInputPairByIndex(index);
+    const {isValid: isValidInput, errorMessage: validationResultMessage} = validateFunc({
+      ...targetInputPair,
+      price: 0, // price가 input에서 0을 초기값으로 갖지않도록 타입을 수정했기 때문에 0을 명시적으로 넘겨줍니다.
+      [field]: value,
+    });
+
+    const {title, price} = targetInputPair;
+
+    if (isLastInputPairFilled({index, field, value})) {
       setErrorMessage('');
+      setInputPairList(prevInputPairList => {
+        const updatedInputPairList = [...prevInputPairList];
+        const targetInputPair = findInputPairByIndex(index, updatedInputPairList);
+
+        targetInputPair[field] = value;
+
+        // 새로운 인덱스를 inputs 배열 길이를 기준으로 설정
+        const newIndex = updatedInputPairList[updatedInputPairList.length - 1].index + 1;
+        const finalInputs = [...updatedInputPairList, {index: newIndex, title: '', price: ''}];
+
+        return finalInputs;
+      });
+    } else if (isValidInput || value.length === 0) {
+      setErrorMessage('');
+      setInputPairList(prevInputPairList => {
+        const updatedInputPairList = [...prevInputPairList];
+        const targetInputPair = findInputPairByIndex(index, updatedInputPairList);
+
+        targetInputPair[field] = value;
+
+        return updatedInputPairList;
+      });
     } else {
-      event.target.value = inputPairs[index][field].toString();
-      setErrorMessage(validation.errorMessage ?? '');
+      const targetInput = findInputPairByIndex(index);
+
+      event.target.value = targetInput[field];
+
+      setErrorMessage(validationResultMessage ?? '');
     }
 
-    if (inputPairs[index].price !== 0 && inputPairs[index].title.trim().length > 0 && inputPairs.length > 0) {
+    handleCanSubmit();
+  };
+
+  const deleteEmptyInputPairElementOnBlur = () => {
+    // 이름, 금액 2개중 최소 하나 이상 값을 가지고 있는 inputPair 배열
+    const filledMinInputPairList = inputPairList.filter(({title, price}) => title !== '' || price !== '');
+
+    // 0쌍, 1쌍 input이 값이 있는 상태에서 두 쌍의 값을 모두 x버튼으로 제거해도 입력 쌍이 2개 남아있는 문제를 위해 조건문을 추가했습니다.
+    if (filledMinInputPairList.length === 0 && inputPairList.length > 1) {
+      setInputPairList([{index: 0, title: '', price: ''}]);
+      return;
+    }
+
+    if (filledMinInputPairList.length === 0) return;
+
+    if (filledMinInputPairList.length !== inputPairList.length) {
+      // 이름, 금액 2개중 하나라도 값이 있다면 지우지 않습니다.
+      setInputPairList(prevInputPairList => {
+        const filledInputPairList = prevInputPairList.filter(({title, price}) => title !== '' || price !== '');
+
+        const newIndex = filledInputPairList[filledInputPairList.length - 1].index + 1;
+        return [...filledInputPairList, {index: newIndex, title: '', price: ''}];
+      });
+    }
+  };
+
+  const handleCanSubmit = () => {
+    if (inputPairList.length > 0 && getFilledInputPairList().length > 0) {
       setCanSubmit(true);
     } else {
       setCanSubmit(false);
@@ -45,25 +112,33 @@ const useDynamicInputPairs = (validateFunc: (inputPair: Bill) => ValidateResult)
     }
   };
 
-  const getNonEmptyInputPairs = () => {
-    return inputPairs.filter(pair => pair.title.trim() !== '' && pair.price !== 0);
+  // 아래부터는 이 훅에서 재사용되는 함수입니다.
+
+  // list 인자를 넘겨주면 그 인자로 찾고, 없다면 InputPairList state를 사용합니다.
+  const findInputPairByIndex = (index: number, list?: InputPair[]) => {
+    return (list ?? inputPairList).filter(input => input.index === index)[0];
   };
 
-  useEffect(() => {
-    if (inputRefs.current.length > 0) {
-      const lastInputPair = inputRefs.current.slice(-2);
-      lastInputPair.forEach(ref => ref?.scrollIntoView({behavior: 'smooth', block: 'center'}));
-    }
-  }, [inputPairs]);
+  // list 인자를 넘겨주면 그 인자로 찾고, 없다면 InputPairList state를 사용합니다.
+  const getFilledInputPairList = (list?: InputPair[]) => {
+    return (list ?? inputPairList).filter(({title, price}) => title !== '' && price !== '');
+  };
+
+  const isLastInputPairFilled = ({index, value}: {index: number; field: BillInputType; value: string}) => {
+    const lastInputIndex = inputPairList[inputPairList.length - 1].index;
+
+    return value !== '' && index === lastInputIndex;
+  };
 
   return {
-    inputPairs,
-    getNonEmptyInputPairs,
-    inputRefs,
+    inputPairList,
+    getFilledInputPairList,
+    inputRefList,
     handleInputChange,
-    handleInputBlur,
+    deleteEmptyInputPairElementOnBlur,
     errorMessage,
     canSubmit,
+    focusNextInputOnEnter,
   };
 };
 
