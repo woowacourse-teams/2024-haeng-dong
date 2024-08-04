8dca946eddb868e1214c499f827b319590f69899
pakxe <pigkill40@naver.com>
2024-08-04 15:17:41 +0900

    design: Input에 css props 적용

--- HDesign/src/components/Input/Input.tsx
+++ HDesign/src/components/Input/Input.tsx
@@ -2,11 +2,14 @@
 import React, {forwardRef, useImperativeHandle, useRef} from 'react';
 import {useInput} from './useInput';
 import {InputProps} from './Input.type';
+import {inputBoxStyle, inputStyle} from './Input.style';
+import {useTheme} from '@/theme/HDesignProvider';
 
 export const Input: React.FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(function Input(
   {value: propsValue, onChange, onFocus, onBlur, inputType, isError, placeholder, ...htmlProps}: InputProps,
   ref,
 ) {
+  const {theme} = useTheme();
   useImperativeHandle(ref, () => inputRef.current!);
   const inputRef = useRef<HTMLInputElement>(null);
   const {value, handleChange, hasFocus, handleClickDelete, handleBlur, handleFocus, handleKeyDown} = useInput({
@@ -18,8 +21,9 @@ export const Input: React.FC<InputProps> = forwardRef<HTMLInputElement, InputPro
   });
 
   return (
-    <div>
+    <div css={inputBoxStyle(theme, inputType, hasFocus, isError)}>
       <input
+        css={inputStyle(theme)}
         ref={inputRef}
         value={value}
         onChange={handleChange}
