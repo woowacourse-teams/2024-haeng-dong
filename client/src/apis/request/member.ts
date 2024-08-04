778f17f0f54961459debe48be3b5b4160bb37356
Soyeon Choe <soy2302ten@gmail.com>
2024-08-04 17:51:53 +0900

    feat: 변경된 API endpoint로 수정
    Co-Authored-By: Pakxe <64801796+pakxe@users.noreply.github.com>
    Co-Authored-By: JinHo Kim <81083461+jinhokim98@users.noreply.github.com>

--- client/src/apis/request/member.ts
+++ client/src/apis/request/member.ts
@@ -1,24 +1,31 @@
 import {BASE_URL} from '@apis/baseUrl';
 import {TEMP_PREFIX} from '@apis/tempPrefix';
-import {requestPost} from '@apis/fetcher';
+import {requestPost, requestDelete} from '@apis/fetcher';
 import {WithEventId} from '@apis/withEventId.type';
 
-type RequestUpdateMemberList = {
+type RequestPostMemberList = {
   memberNameList: string[];
   type: MemberType;
 };
 
-export const requestUpdateMemberList = async ({
-  eventId,
-  type,
-  memberNameList,
-}: WithEventId<RequestUpdateMemberList>) => {
+export const requestPostMemberList = async ({eventId, type, memberNameList}: WithEventId<RequestPostMemberList>) => {
   await requestPost({
     baseUrl: BASE_URL.HD,
-    endpoint: `${TEMP_PREFIX}/${eventId}/actions/members`,
+    endpoint: `${TEMP_PREFIX}/${eventId}/member-actions`,
     body: {
       members: memberNameList,
       status: type,
     },
+  });
+};
+
+type RequestDeleteMemberAction = {
+  actionId: number;
+};
+
+export const requestDeleteMemberAction = async ({eventId, actionId}: WithEventId<RequestDeleteMemberAction>) => {
+  await requestDelete({
+    baseUrl: BASE_URL.HD,
+    endpoint: `${TEMP_PREFIX}/${eventId}/member-actions/${actionId}`,
   });
 };
