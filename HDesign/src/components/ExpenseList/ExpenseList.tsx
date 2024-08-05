14c9b28ac2a40f11973a6823ea4069bdfda1adfe
이태훈 <rhymint@gmail.com>
2024-08-05 23:50:01 +0900

    fix: 잘못 사용된 주석 변경

--- HDesign/src/components/ExpenseList/ExpenseList.tsx
+++ HDesign/src/components/ExpenseList/ExpenseList.tsx
@@ -1,14 +1,15 @@
 /** @jsxImportSource @emotion/react */
 
 import Text from '@components/Text/Text';
+import Icon from '@components/Icon/Icon';
 
 import {useTheme} from '@theme/HDesignProvider';
 
 import {ExpenseItemProps, ExpenseListProps} from './ExpenseList.type';
 import {expenseItemStyle, expenseListStyle, expenseItemLeftStyle, TextStyle} from './ExpenseList.style';
-import Icon from '@components/Icon/Icon';
 
 // TODO: (@soha) 따로 파일 분리할까 고민중.. 여기서만 사용할 것 같긴 한데.. 흠
+// TODO: (@todari) : 추후 클릭 시 상호작용이 생기면 iconButton으로 변경할 수 있음
 function ExpenseItem({name, price, ...buttonProps}: ExpenseItemProps) {
   const {theme} = useTheme();
   return (
@@ -18,7 +19,6 @@ function ExpenseItem({name, price, ...buttonProps}: ExpenseItemProps) {
       </Text>
       <div css={expenseItemLeftStyle}>
         <Text css={TextStyle(theme)}>{price.toLocaleString('ko-kr')}원</Text>
-        // TODO: (@todari) : 추후 클릭 시 상호작용이 생기면 iconButton으로 변경할 수 있음
         <Icon iconType="rightChevron" />
       </div>
     </button>
