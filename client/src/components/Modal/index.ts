6089b52e65e00a79e108f7d6f9adcf7275e49ca9
김진호 <rlawlsgh1227@gmail.com>
2024-08-05 15:44:29 +0900

    refactor: index.ts를 통해 import문 줄임

---
+++ client/src/components/Modal/index.ts
@@ -0,0 +1,2 @@
+export {default as SetActionListModal} from './SetActionModal/SetActionListModal';
+export {default as SetInitialMemberListModal} from './SetInitialMemberListModal/SetInitialMemberListModal';
