3304536bf32aadf4400d5683a0db9aa13cef6d8d
3juhwan <13selfesteem91@naver.com>
2024-08-05 15:27:48 +0900

    feat: 테스트 클리너 상수, 메서드 분리

--- server/src/test/java/server/haengdong/support/extension/DatabaseCleaner.java
+++ server/src/test/java/server/haengdong/support/extension/DatabaseCleaner.java
@@ -1,5 +1,6 @@
 package server.haengdong.support.extension;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import java.util.List;
@@ -9,8 +10,33 @@ import org.springframework.transaction.annotation.Transactional;
 @Component
 class DatabaseCleaner {
 
+    private static final String REFERENTIAL_FORMAT = "set referential_integrity %b;";
+    private static final String TRUNCATE_FORMAT = "truncate table %s restart identity;";
+
     @PersistenceContext
     private EntityManager em;
+    private String truncateTablesQuery;
+
+    @PostConstruct
+    public void createTruncateQuery() {
+        List<String> tableNames = getTableNames();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String tableName : tableNames) {
+            String truncateQuery = String.format(TRUNCATE_FORMAT, tableName);
+            stringBuilder.append(truncateQuery);
+        }
+        truncateTablesQuery = stringBuilder.toString();
+    }
+
+    private List<String> getTableNames() {
+        String sql = """
+                select table_name
+                from information_schema.tables
+                where table_schema = 'public'
+                """;
+        return em.createNativeQuery(sql).getResultList();
+    }
 
     @Transactional
     public void clear() {
@@ -19,18 +45,8 @@ class DatabaseCleaner {
     }
 
     private void truncate() {
-        em.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
-        getTruncateQueries().forEach(query -> em.createNativeQuery(query).executeUpdate());
-        em.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
-    }
-
-    private List<String> getTruncateQueries() {
-        String sql = """
-                SELECT Concat('TRUNCATE TABLE ', TABLE_NAME, ' RESTART IDENTITY', ';') AS q
-                FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_SCHEMA = 'PUBLIC'
-                """;
-
-        return em.createNativeQuery(sql).getResultList();
+        em.createNativeQuery(String.format(REFERENTIAL_FORMAT, false)).executeUpdate();
+        em.createNativeQuery(truncateTablesQuery).executeUpdate();
+        em.createNativeQuery(String.format(REFERENTIAL_FORMAT, true)).executeUpdate();
     }
 }
