169d928ffa76b7b48a48986ee3487fecfa387217
이태훈 <rhymint@gmail.com>
2024-08-05 22:33:59 +0900

    feat: Icon component 구현

---
+++ HDesign/src/components/Icon/Icon.style.ts
@@ -0,0 +1,24 @@
+import {css} from '@emotion/react';
+import {Theme} from '@theme/theme.type';
+import {IconColor, IconType} from './Icon.type';
+import {ColorKeys} from '@token/colors';
+
+const ICON_DEFAULT_COLOR: Record<IconType, IconColor> = {
+  inputDelete: 'gray',
+  buljusa: 'gray',
+  rightChevron: 'gray',
+  search: 'gray',
+  confirm: 'complete',
+  error: 'warn',
+  trash: 'white',
+};
+
+export const iconStyle = (iconType: IconType, theme: Theme, iconColor?: IconColor) => {
+  if (iconColor) {
+    return css({
+      color: theme.colors[iconColor as ColorKeys],
+    });
+  } else {
+    color: theme.colors[ICON_DEFAULT_COLOR[iconType]];
+  }
+};
