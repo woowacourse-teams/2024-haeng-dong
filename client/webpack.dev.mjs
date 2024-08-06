7d9e328161877cf5c1bfe2f12ea2cda67a84bb9b
이태훈 <rhymint@gmail.com>
2024-08-07 01:58:40 +0900

    chore: dev, prod 환경에 따른 webpack 설정을 위해 webpack.dev.mjs, webpack.prod.mjs 생성

---
+++ client/webpack.dev.mjs
@@ -0,0 +1,30 @@
+import path from 'path';
+import {merge} from 'webpack-merge';
+import Dotenv from 'dotenv-webpack';
+import common from './webpack.common.mjs';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/',
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+  plugins: [
+    new Dotenv({
+      path: '.env.dev',
+    }),
+  ],
+});
