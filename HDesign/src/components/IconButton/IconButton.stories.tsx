c4c6c4370333e7d86d093bf9f972994c97b89aff
이태훈 <rhymint@gmail.com>
2024-08-07 02:23:08 +0900

    test: storybook에서 Icon component가 잘못 사용되던 오류 수정

--- HDesign/src/components/IconButton/IconButton.stories.tsx
+++ HDesign/src/components/IconButton/IconButton.stories.tsx
@@ -2,7 +2,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
 import IconButton from '@components/IconButton/IconButton';
-import {Search, Buljusa, InputDelete, RightChevron, Error, Trash} from '@assets';
+import Icon from '@components/Icon/Icon';
 
 const meta = {
   title: 'Components/IconButton',
@@ -25,13 +25,22 @@ const meta = {
     children: {
       description: '',
       control: {type: 'select'},
-      options: [<InputDelete />, <Buljusa />, <RightChevron />, <Search />, <Error />, <Trash />],
+      // TODO: (@todari) : Icon의 색상을 variants에 의해 자동으로 변경해 줄 수 있는 로직 추가
+      options: [
+        <Icon iconType="inputDelete" />,
+        <Icon iconType="buljusa" />,
+        <Icon iconType="rightChevron" />,
+        <Icon iconType="search" />,
+        <Icon iconType="confirm" />,
+        <Icon iconType="error" />,
+        <Icon iconType="trash" />,
+      ],
     },
   },
   args: {
     size: 'medium',
     variants: 'destructive',
-    children: <Trash />,
+    children: <Icon iconType="trash" />,
   },
 } satisfies Meta<typeof IconButton>;
 
