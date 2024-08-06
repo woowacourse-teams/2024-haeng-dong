72968565cf2643429ea5492616f499c18458ff59
이태훈 <rhymint@gmail.com>
2024-08-07 01:58:17 +0900

    chore: dev, prod 환경의 공통 설정을 webpack.common.mjs 로 분리

---
+++ client/webpack.common.mjs
@@ -0,0 +1,56 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import {ModifySourcePlugin, ConcatOperation} from 'modify-source-webpack-plugin';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@apis': path.resolve(__dirname, 'src/apis/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@constants': path.resolve(__dirname, 'src/constants/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@mocks': path.resolve(__dirname, 'src/mocks/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ModifySourcePlugin({
+      rules: [
+        {
+          test: /\.tsx$/i,
+          operations: [new ConcatOperation('start', '/** @jsxImportSource @emotion/react */\n\n')],
+        },
+      ],
+    }),
+  ],
+};
