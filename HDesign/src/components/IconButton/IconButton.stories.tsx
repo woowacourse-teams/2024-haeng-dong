c8d8c172a71bf1b017735f4fd523cc95fba48abe
TaehunLee <85233397+Todari@users.noreply.github.com>
2024-08-07 10:53:30 +0900

    feat: 삭제 아이콘을 위한 IconButton component 수정 (#209)
    * chore: greenColor token 추가
    
    * remove: 사용하지 않는 svg 삭제
    
    * fix: 지난번 변경사항이 반영되지 않은 문제 해결
    
    * fix: 동적으로 변경되는 color를 사용하기 위해 svg 내 fill 및 stroke attribute를 currentColor로 설정
    
    * feat: Icon component 구현
    
    * feat: IconButton Component 구현
    
    * fix: DefaultColor가 제대로 적용되지 않던 오류 수정
    
    * fix: StepItem prop type이 잘못 지정된 오류 수정
    
    * style: lint 적용
    
    * fix: size prop optional로 변경
    
    * test: IconButton storybook props 변경
    
    * fix: 사용하지 않는 css svg 속성 제거
    
    * fix: Icon 위아래 정렬이 맞지 않는 오류 수정
    
    * fix: confirm, error Icon이 제대로 export 되어있지 않던 오류 수정
    
    * feat: Icon, ListButton export
    
    * refactor: Icon / IconButton 변경으로 인한 기존 코드 이에 맞게 변경
    
    * fix: 잘못 사용된 주석 변경
    
    * style: lint 적용
    
    * chore: v0.1.57 배포
    
    * test: storybook에서 Icon component가 잘못 사용되던 오류 수정
    
    * refactor: style prop이 여러개인 경우, 객체를 통해 prop 전달하도록 변경
    
    * fix: svg 파일 변경

--- HDesign/src/components/IconButton/IconButton.stories.tsx
+++ HDesign/src/components/IconButton/IconButton.stories.tsx
@@ -25,11 +25,13 @@ const meta = {
     children: {
       description: '',
       control: {type: 'select'},
+      // TODO: (@todari) : Icon의 색상을 variants에 의해 자동으로 변경해 줄 수 있는 로직 추가
       options: [
         <Icon iconType="inputDelete" />,
         <Icon iconType="buljusa" />,
         <Icon iconType="rightChevron" />,
         <Icon iconType="search" />,
+        <Icon iconType="confirm" />,
         <Icon iconType="error" />,
         <Icon iconType="trash" />,
       ],
