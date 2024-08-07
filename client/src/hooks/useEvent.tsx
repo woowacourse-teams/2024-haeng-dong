95931550b9fe395671cea9150719511618e28fc3
pakxe <pigkill40@naver.com>
2024-08-08 01:27:26 +0900

    feat: 이벤트를 새로 생성하기 위한 api콜을 담당하는 useEvent훅 구현

---
+++ client/src/hooks/useEvent.tsx
@@ -0,0 +1,14 @@
+import {ResponsePostNewEvent, requestPostNewEvent} from '@apis/request/event';
+import {useFetch} from '@apis/useFetch';
+
+const useEvent = () => {
+  const {fetch} = useFetch();
+
+  const createNewEvent = async ({eventName}: {eventName: string}) => {
+    return await fetch<ResponsePostNewEvent>(() => requestPostNewEvent({eventName}));
+  };
+
+  return {createNewEvent};
+};
+
+export default useEvent;
