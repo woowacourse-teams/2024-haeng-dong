88e628ababc718232ca74235659e46bf966ddcab
pakxe <pigkill40@naver.com>
2024-08-08 01:13:56 +0900

    feat: msw를 앱 시작 지점에 연결

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {RouterProvider} from 'react-router-dom';
 
 import router from './router';
+import {RouterProvider} from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  const {worker} = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
+});
