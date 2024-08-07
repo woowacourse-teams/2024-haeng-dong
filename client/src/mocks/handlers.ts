eded4da7e7cbc1806bc64fc8eef1868cb9634a40
pakxe <pigkill40@naver.com>
2024-08-08 02:54:44 +0900

    fix: /가 하나 더 들어가있던 부분 수정

--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -3,7 +3,10 @@ import {http, HttpResponse} from 'msw';
 import {TEMP_PREFIX} from '@apis/tempPrefix';
 
 export const handlers = [
-  http.post(`${process.env.API_BASE_URL!}/${TEMP_PREFIX}`, ({request}) => {
-    return HttpResponse.json({errorCode: 'testCode', message: 'testMessage'}, {status: 404});
+  http.post(`${process.env.API_BASE_URL!}${TEMP_PREFIX}`, ({request}) => {
+    return HttpResponse.json(
+      {errorCode: 'ACTION_NOT_FOUND', message: 'msw모킹함수의 에러메세지입니다.'},
+      {status: 404},
+    );
   }),
 ];
