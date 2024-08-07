743547f76bd8c16819fce53101d01441046368e7
pakxe <pigkill40@naver.com>
2024-08-07 17:11:46 +0900

    feat: ErrorProvider가 ToastProvider를 감싸도록 작성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,23 @@
 import {Outlet} from 'react-router-dom';
-import {HDesignProvider, ToastProvider} from 'haengdong-design';
+import {HDesignProvider} from 'haengdong-design';
 import {Global} from '@emotion/react';
 
 import {GlobalStyle} from './GlobalStyle';
+import {toastConfig} from 'react-simple-toasts';
+import {ErrorProvider} from './ErrorProvider';
+import {ToastProvider} from '@components/Toast/ToastProvider';
+
+toastConfig({theme: 'dark'});
 
 const App: React.FC = () => {
   return (
     <HDesignProvider>
       <Global styles={GlobalStyle} />
-      <ToastProvider>
-        <Outlet />
-      </ToastProvider>
+      <ErrorProvider>
+        <ToastProvider>
+          <Outlet />
+        </ToastProvider>
+      </ErrorProvider>
     </HDesignProvider>
   );
 };
