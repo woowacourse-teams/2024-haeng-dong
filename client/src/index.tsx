4130ecd04a818c149007910fb0130c64bfdd4282
pakxe <pigkill40@naver.com>
2024-08-08 02:04:05 +0900

    chore: msw를 사용하는 코드 주석 처리

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,15 +4,15 @@ import ReactDOM from 'react-dom/client';
 import router from './router';
 import {RouterProvider} from 'react-router-dom';
 
-async function enableMocking() {
-  const {worker} = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
-  return worker.start();
-}
+// async function enableMocking() {
+//   const {worker} = await import('./mocks/browser'); //Dynamic import하는 것이 눈에 띄였다.
+//   return worker.start();
+// }
 
-enableMocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
-  );
-});
+// enableMocking().then(() => {
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
+// });
