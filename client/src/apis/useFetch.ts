4ed81200f909f300b1dbfec4fbad158acaa81d4c
pakxe <pigkill40@naver.com>
2024-08-08 17:31:43 +0900

    feat: 에러 코드에 맞게 level을 설정해 sentry에 보내는 함수 구현

--- client/src/apis/useFetch.ts
+++ client/src/apis/useFetch.ts
@@ -31,3 +31,45 @@ export const useFetch = () => {
 
   return {loading, fetch};
 };
+
+const captureError = async (error: Error, navigate: NavigateFunction) => {
+  const errorBody: ServerError =
+    error instanceof FetchError
+      ? error.errorBody
+      : {message: await JSON.parse(error.message), errorCode: UNHANDLED_ERROR};
+
+  switch (errorBody?.errorCode) {
+    case 'INTERNAL_SERVER_ERROR':
+      sendLogToSentry({error, errorBody, level: 'fatal'});
+      break;
+
+    case 'TOKEN_INVALID':
+      sendLogToSentry({error, errorBody});
+      break;
+
+    case 'TOKEN_EXPIRED':
+      sendLogToSentry({error, errorBody});
+      // TODO: (@weadie) 여기에 토스트를 띄울지 말지
+      navigate('/'); // TODO: (@weadie) 루트 경로 상수화
+
+      break;
+
+    case 'TOKEN_NOT_FOUND':
+      sendLogToSentry({error, errorBody});
+      break;
+
+    // 비밀 번호를 까먹는 사람이 얼마나 많은 지 추측하기 위함
+    case 'PASSWORD_INVALID':
+      sendLogToSentry({error, errorBody, level: 'debug'});
+      break;
+
+    // 1천만원 이상 입력하는 사람이 얼마나 많은 지 추측하기 위함
+    case 'BILL_ACTION_PRICE_INVALID':
+      sendLogToSentry({error, errorBody, level: 'debug'});
+      break;
+
+    default:
+      sendLogToSentry({error, errorBody, level: 'fatal'});
+      break;
+  }
+};
