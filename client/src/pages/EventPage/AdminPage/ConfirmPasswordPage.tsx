7588c5c7c52e3d609f521358935e9c882e048159
이태훈 <rhymint@gmail.com>
2024-08-08 15:58:25 +0900

    feat: ConfirmPasswordPage 구현

---
+++ client/src/pages/EventPage/AdminPage/ConfirmPasswordPage.tsx
@@ -0,0 +1,67 @@
+import {useEffect, useState} from 'react';
+import {useLocation, useNavigate} from 'react-router-dom';
+import {FixedButton, MainLayout, LabelInput, Title, TopNav, Back} from 'haengdong-design';
+
+import validateEventPassword from '@utils/validate/validateEventPassword';
+import {requestPostNewEvent} from '@apis/request/event';
+
+import useEvent from '@hooks/useEvent';
+
+import RULE from '@constants/rule';
+import {ROUTER_URLS} from '@constants/routerUrls';
+import useEventId from '@hooks/useEventId/useEventId';
+
+const ConfirmPasswordPage = () => {
+  const [eventName, setEventName] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  const navigate = useNavigate();
+
+  const submitPassword = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const {eventId} = useEventId();
+
+    navigate(`${ROUTER_URLS.eventCreateComplete}?${new URLSearchParams({eventId})}`);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    const validation = validateEventPassword(newValue);
+
+    setCanSubmit(newValue.length === RULE.maxEventPasswordLength);
+
+    if (validation.isValid) {
+      setPassword(newValue);
+      setErrorMessage('');
+    } else {
+      event.target.value = password;
+      setErrorMessage(validation.errorMessage ?? '');
+    }
+  };
+  return (
+    <MainLayout>
+      <TopNav>
+        <Back />
+      </TopNav>
+      <Title title="행사 비밀번호 입력" description="행사 생성 시 설정한 4 자리의 숫자 비밀번호를 입력해 주세요." />
+      <form onSubmit={submitPassword} style={{padding: '0 1rem'}}>
+        <LabelInput
+          labelText="비밀번호"
+          errorText={errorMessage}
+          value={password}
+          type="secret"
+          maxLength={RULE.maxEventPasswordLength}
+          placeholder="비밀번호"
+          onChange={e => handleChange(e)}
+          isError={!!errorMessage}
+          autoFocus
+        ></LabelInput>
+        <FixedButton disabled={!canSubmit}>관리 페이지로</FixedButton>
+      </form>
+    </MainLayout>
+  );
+};
+
+export default ConfirmPasswordPage;
