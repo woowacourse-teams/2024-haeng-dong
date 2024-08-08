a8124a3e7a54253d783b118ff0523974e32d6dce
pakxe <pigkill40@naver.com>
2024-08-08 17:33:32 +0900

    feat: sentry 에 endpoint, url, errorCode, message, status, requestBody 로그를 담아 보내는 함수 구현

---
+++ client/src/utils/sendLogToSentry.ts
@@ -0,0 +1,55 @@
+import * as Sentry from '@sentry/react';
+
+import {ServerError} from 'ErrorProvider';
+
+import FetchError from '../errors/FetchError';
+/**
+ * level은 아래와 같은 용도에 맞게 지정해줍니다.
+ *
+ * fatal: 앱이 종료될 수 있는 치명적인 오류
+ * error: 특정 기능 실패로 앱 종료까지는 아닌 오류
+ * warning: 잠재적으로 문제가 될 수 있는 오류. 현재는 심각하지 않은 오류
+ * info: 시스템의 정상적인 동작을 나타냄. 중요한 이벤트나 상태 변화 기록용
+ * debug: 디버깅 목적으로 사용됨
+ * log: 일반적인 로그 메세지
+ */
+
+type SentryLevel = 'fatal' | 'error' | 'warning' | 'info' | 'debug' | 'log';
+type SendLogToSentry = {
+  level?: SentryLevel;
+  error: Error;
+  errorBody: ServerError;
+};
+
+const sendLogToSentry = ({level = 'error', error, errorBody}: SendLogToSentry) => {
+  Sentry.withScope(scope => {
+    const {errorCode, message} = errorBody;
+    scope.setLevel(level);
+
+    scope.setTag('environment', process.env.NODE_ENV);
+
+    if (error instanceof FetchError) {
+      scope.setTags({
+        endpoint: error.endpoint,
+        url: window.location.href,
+        errorCode,
+        errorMessage: error.errorBody.message,
+        status: error.status,
+        requestBody: error.requestBody,
+      });
+
+      Sentry.captureMessage(`${errorCode}`);
+    } else {
+      scope.setTags({
+        url: window.location.href,
+        errorCode,
+        message: error.message,
+        name: error.name,
+      });
+
+      Sentry.captureMessage(`${errorCode}`);
+    }
+  });
+};
+
+export default sendLogToSentry;
