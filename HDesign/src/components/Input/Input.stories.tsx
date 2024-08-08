66c1d22ec6018ba16903eec05b9b8402f94dc251
TaehunLee <85233397+Todari@users.noreply.github.com>
2024-08-09 01:55:28 +0900

    fix: 디자인시스템 Input value 동기화 오류 수정 (#266)
    * fix: useInput에 value dependency를 받도록 설정
    
    * chore: v0.1.65 publish

--- HDesign/src/components/Input/Input.stories.tsx
+++ HDesign/src/components/Input/Input.stories.tsx
@@ -3,6 +3,8 @@ import type {Meta, StoryObj} from '@storybook/react';
 import React, {useEffect, useState} from 'react';
 
 import Input from '@components/Input/Input';
+import Flex from '@components/Flex/Flex';
+import Button from '@components/Button/Button';
 
 const meta = {
   title: 'Components/Input',
@@ -26,21 +28,29 @@ type Story = StoryObj<typeof meta>;
 
 export const Playground: Story = {
   render: ({...args}) => {
+    const regex = /^[ㄱ-ㅎ가-힣]*$/;
     const [value, setValue] = useState('');
     const [isError, setIsError] = useState(false);
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.value.length < 4) {
-        setValue(event.target.value);
+      const newValue = event.target.value;
+      if (regex.test(newValue)) {
+        setValue(newValue);
         setIsError(false);
       } else {
-        event.target.value = value;
         setIsError(true);
       }
     };
-    const handleBlur = () => {
-      console.log('blur');
+
+    const changeRandomValue = () => {
+      setValue('외부에서 값 변경됨');
     };
 
-    return <Input value={value} onChange={e => handleChange(e)} isError={isError} onBlur={handleBlur} {...args} />;
+    return (
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+        <Button onClick={changeRandomValue}>input 값 변경</Button>
+        <Input value={value} onChange={handleChange} isError={isError} {...args} />
+      </div>
+    );
   },
 };
