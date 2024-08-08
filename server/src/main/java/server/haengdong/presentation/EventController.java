37eeb55dde5c5a5546d9e8c916c157f6bdbba400
Arachneee <jhg2819@naver.com>
2024-08-08 22:43:30 +0900

    fix: 어드민 권한 확인 불가 버그 수정

--- server/src/main/java/server/haengdong/presentation/EventController.java
+++ server/src/main/java/server/haengdong/presentation/EventController.java
@@ -90,7 +90,7 @@ public class EventController {
                 .build();
     }
 
-    @GetMapping("/api/events/{eventId}/auth")
+    @PostMapping("/api/events/{eventId}/auth")
     public ResponseEntity<Void> authenticate(@PathVariable("eventId") String token) {
         return ResponseEntity.ok().build();
     }
