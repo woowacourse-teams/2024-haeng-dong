ae82699689df19cc1b78ebafcf035586a61fdc4c
JUHA <84626225+khabh@users.noreply.github.com>
2024-08-08 11:04:55 +0900

    feat: REST docs를 통한 문서화 (#238)
    * feat: REST Docs 적용
    
    * feat: REST Docs prettyPrint 및 snippets 적용
    
    * feat: REST Docs에 예외 항목 추가
    
    * test: 행사 액션 이력 조회 테스트 추가
    
    * refactor: 중복된 http-request snippet 제거
    
    * refactor: 사용되지 않는 snippet 제거
    
    * refactor: 빌드 시 자동으로 문서 최신화하도록 gradle 설정 추가
    
    * fix: CookieProperties에 sameSite 옵션 추가
    
    ---------
    
    Co-authored-by: Arachne <66822642+Arachneee@users.noreply.github.com>

--- server/src/test/java/server/haengdong/support/fixture/Fixture.java
+++ server/src/test/java/server/haengdong/support/fixture/Fixture.java
@@ -1,9 +1,11 @@
 package server.haengdong.support.fixture;
 
+import jakarta.servlet.http.Cookie;
 import server.haengdong.domain.event.Event;
 
 public class Fixture {
 
     public static final Event EVENT1 = new Event("쿠키", "1234", "TOKEN1");
     public static final Event EVENT2 = new Event("웨디", "1234", "TOKEN2");
+    public static final Cookie EVENT_COOKIE = new Cookie("eventToken", "토큰토큰");
 }
