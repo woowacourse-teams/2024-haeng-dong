1c3a2882568e997ae518711aff5819bcfc7be362
김진호 <rlawlsgh1227@gmail.com>
2024-08-08 17:17:45 +0900

    feat: Search 컴포넌트 검색 기능을 빼서 연관검색어 작동만 하도록 수정

--- HDesign/src/components/Search/Search.tsx
+++ HDesign/src/components/Search/Search.tsx
@@ -3,33 +3,26 @@ import Flex from '@components/Flex/Flex';
 
 import {useTheme} from '@theme/HDesignProvider';
 
-import Input from '../Input/Input';
-import {InputProps} from '../Input/Input.type';
-
 import {searchStyle, searchTermsStyle, searchTermStyle} from './Search.style';
-import useSearch from './useSearch';
 
-export interface SearchProps extends InputProps {
+export interface SearchProps {
+  isShowTargetInput: boolean;
   searchTerms: string[];
-  setState: React.Dispatch<React.SetStateAction<string>>;
+  onTermClick: (term: string) => void;
 }
 
-const Search: React.FC<SearchProps> = ({searchTerms, setState, ...inputProps}: SearchProps) => {
+const Search = ({isShowTargetInput, searchTerms, onTermClick, children}: React.PropsWithChildren<SearchProps>) => {
   const {theme} = useTheme();
-  const {value, showSearchTerms, handleOnChange, handleOnClick, filterSearchTerms} = useSearch({
-    searchTerms,
-    setState,
-  });
 
   return (
     <fieldset css={searchStyle}>
-      <Input inputType="search" value={value} onChange={handleOnChange} {...inputProps} />
-      {showSearchTerms && (
+      {children}
+      {searchTerms.length > 0 && isShowTargetInput && (
         <ul css={searchTermsStyle(theme)}>
           <Flex flexDirection="column" gap="0.5rem">
-            {filterSearchTerms(value).map((searchTerm, index) => (
+            {searchTerms.map((searchTerm, index) => (
               <li key={`${searchTerm}-${index}`}>
-                <button type="button" css={searchTermStyle(theme)} onClick={() => handleOnClick(searchTerm)}>
+                <button type="button" css={searchTermStyle(theme)} onClick={() => onTermClick(searchTerm)}>
                   {searchTerm}
                 </button>
               </li>
