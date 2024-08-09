5daff9f8a58a4e7b989b4e00c54d227f1346b2e5
이태훈 <rhymint@gmail.com>
2024-08-09 09:44:48 +0900

    fix: 로그인 시 오류가 발생하던 문제 해결

--- client/src/apis/useFetch.ts
+++ client/src/apis/useFetch.ts
@@ -7,11 +7,14 @@ import {UNKNOWN_ERROR} from '@constants/errorMessage';
 
 import {ServerError, useError} from '../ErrorProvider';
 import FetchError from '../errors/FetchError';
+import useEventId from '@hooks/useEventId/useEventId';
+import {ROUTER_URLS} from '@constants/routerUrls';
 
 export const useFetch = () => {
   const {setError, clearError} = useError();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const {eventId} = useEventId();
 
   const fetch = async <T>(queryFunction: () => Promise<T>): Promise<T> => {
     setLoading(true);
@@ -27,12 +30,12 @@ export const useFetch = () => {
           error instanceof FetchError ? error.errorBody : {errorCode: error.name, message: error.message};
 
         setError(errorBody);
-
-        captureError(error, navigate);
+        console.log('???');
+        captureError(error, navigate, eventId);
       } else {
         setError({errorCode: UNKNOWN_ERROR, message: JSON.stringify(error)});
-
-        captureError(new Error(UNKNOWN_ERROR), navigate);
+        console.log('!!!');
+        captureError(new Error(UNKNOWN_ERROR), navigate, eventId);
 
         // 에러를 throw 해 에러 바운더리로 보냅니다. 따라서 에러 이름은 중요하지 않음
         throw new Error(UNKNOWN_ERROR);
@@ -47,33 +50,40 @@ export const useFetch = () => {
   return {loading, fetch};
 };
 
-const captureError = async (error: Error, navigate: NavigateFunction) => {
+const captureError = async (error: Error, navigate: NavigateFunction, eventId: string) => {
   const errorBody: ServerError =
     error instanceof FetchError ? error.errorBody : {message: error.message, errorCode: error.name};
 
+  console.log(errorBody);
   switch (errorBody?.errorCode) {
     case 'INTERNAL_SERVER_ERROR':
       sendLogToSentry({error, errorBody, level: 'fatal'});
       break;
 
+    case 'FORBIDDEN':
+      sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
     case 'TOKEN_INVALID':
       sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
       break;
 
     case 'TOKEN_EXPIRED':
       sendLogToSentry({error, errorBody});
-      // TODO: (@weadie) 여기에 토스트를 띄울지 말지
-      navigate('/'); // TODO: (@weadie) 루트 경로 상수화
-
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
       break;
 
     case 'TOKEN_NOT_FOUND':
       sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
       break;
 
     // 비밀 번호를 까먹는 사람이 얼마나 많은 지 추측하기 위함
     case 'PASSWORD_INVALID':
       sendLogToSentry({error, errorBody, level: 'debug'});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
       break;
 
     // 1천만원 이상 입력하는 사람이 얼마나 많은 지 추측하기 위함
