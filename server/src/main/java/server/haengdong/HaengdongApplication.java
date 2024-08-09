d2be8f61ead90a8de4376b0dd800794d5ba9a061
kunsanglee <gosmdochee2@gmail.com>
2024-08-09 22:45:32 +0900

    feat: actuator health 엔드포인트 설정 변경

--- server/src/main/java/server/haengdong/HaengdongApplication.java
+++ server/src/main/java/server/haengdong/HaengdongApplication.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HaengdongApplication {
 
     public static void main(String[] args) {
-        log.error("################### 안녕하세요. CI/CD 테스트입니다. ###################");
         SpringApplication.run(HaengdongApplication.class, args);
     }
 
--- server/src/main/java/server/haengdong/exception/GlobalExceptionHandler.java
+++ server/src/main/java/server/haengdong/exception/GlobalExceptionHandler.java
@@ -17,25 +17,35 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ErrorResponse> authenticationException(AuthenticationException e) {
+        log.warn(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(ErrorResponse.of(e.getErrorCode()));
     }
 
-    @ExceptionHandler({HttpRequestMethodNotSupportedException.class, NoResourceFoundException.class})
-    public ResponseEntity<ErrorResponse> noResourceException() {
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> noResourceException(HttpRequestMethodNotSupportedException e) {
+        log.warn(e.getMessage(), e);
+        return ResponseEntity.badRequest()
+                .body(ErrorResponse.of(HaengdongErrorCode.REQUEST_METHOD_NOT_SUPPORTED));
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> noResourceException(NoResourceFoundException e) {
+        log.warn(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of(HaengdongErrorCode.NO_RESOURCE_REQUEST));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<ErrorResponse> httpMessageNotReadableException() {
+    public ResponseEntity<ErrorResponse> httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.warn(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of(HaengdongErrorCode.MESSAGE_NOT_READABLE));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.info(e.getMessage(), e);
+        log.warn(e.getMessage(), e);
         String errorMessage = e.getFieldErrors().stream()
                 .map(error -> error.getField() + " " + error.getDefaultMessage())
                 .collect(Collectors.joining(", "));
@@ -46,7 +56,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HaengdongException.class)
     public ResponseEntity<ErrorResponse> haengdongException(HaengdongException e) {
-        log.info(e.getMessage(), e);
+        log.warn(e.getMessage(), e);
         return ResponseEntity.badRequest()
                 .body(ErrorResponse.of(e.getErrorCode()));
     }
