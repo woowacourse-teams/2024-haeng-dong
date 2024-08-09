41a6b60bcf7123bf2b468a47ebc23b99faa73b0b
김진호 <rlawlsgh1227@gmail.com>
2024-08-09 09:41:36 +0900

    fix: 홈에서 액션 수정 삭제 되지 않도록 설정

--- client/src/components/StepList/MemberStepItem.tsx
+++ client/src/components/StepList/MemberStepItem.tsx
@@ -1,6 +1,7 @@
 import type {MemberStep} from 'types/serviceType';
 
 import {DragHandleItem} from 'haengdong-design';
+import {useOutletContext} from 'react-router-dom';
 
 import {DeleteMemberActionModal} from '@components/Modal/SetActionModal/DeleteMemberActionModal';
 
@@ -11,14 +12,17 @@ interface MemberStepItemProps {
 }
 
 const MemberStepItem: React.FC<MemberStepItemProps> = ({step, isOpenBottomSheet, setIsOpenBottomSheet}) => {
+  const isAdmin = useOutletContext<boolean>();
+
   return (
     <>
       <DragHandleItem
+        hasDragHandler={isAdmin}
         backgroundColor="white"
         prefix={`${step.actions.map(({name}) => name).join(', ')} ${step.type === 'IN' ? '들어옴' : '나감'}`}
         onClick={() => setIsOpenBottomSheet(prev => !prev)}
       />
-      {isOpenBottomSheet && (
+      {isOpenBottomSheet && isAdmin && (
         <DeleteMemberActionModal
           memberActionType={step.type}
           memberActionList={step.actions}
