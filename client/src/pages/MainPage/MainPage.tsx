3e3a1168d9bea97879c77e7b6511e0fd1fbb6c45
김진호 <rlawlsgh1227@gmail.com>
2024-08-12 16:48:21 +0900

    feat: 메인페이지 설명 수정 및 로고 적용

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,6 +1,8 @@
 import {useNavigate} from 'react-router-dom';
 import {FixedButton, MainLayout, Title, TopNav} from 'haengdong-design';
 
+import {Logo} from '@components/Common/Logo';
+
 import {ROUTER_URLS} from '@constants/routerUrls';
 
 const MainPage = () => {
@@ -9,7 +11,8 @@ const MainPage = () => {
   return (
     <MainLayout>
       <TopNav children={<></>} />
-      <Title title="행동대장" description="데모데이 ~~ 시연 ~~~~~! 페이지입니다." />
+      <Title title="행동대장" description="👑행동대장들의 행복한 행사 진행을 위해... 📢행동개시!" />
+      <Logo />
       <FixedButton onClick={() => navigate(ROUTER_URLS.eventCreateName)}>행사 생성하기</FixedButton>
     </MainLayout>
   );
