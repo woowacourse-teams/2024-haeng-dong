65b797f2b5f46edec6fa8000f7b9c4dbe0dde2b5
pakxe <pigkill40@naver.com>
2024-08-13 02:11:11 +0900

    chore: jest사용을 위한 환경 세팅

---
+++ client/jest.config.ts
@@ -0,0 +1,20 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  // testEnvironment: 'node', //  Node.js 모듈(fs, path, http 등)을 사용한 서버 사이드 로직이나 파일 시스템 접근, 네트워크 요청 등을 테스트
+  testEnvironment: 'jsdom', // 브라우저 내에서의 JavaScript 동작을 모방하여, DOM 조작, 이벤트 핸들링, 브라우저 관련 API 호출
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageReporters: ['text'],
+  verbose: true,
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1', // path alias를 적용하기 위함
+  },
+};
+
+export default config;
