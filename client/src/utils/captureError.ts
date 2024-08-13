0912ee1fb86256b5c3f90bbc0d1bbb0bb3a8a9d1
pakxe <pigkill40@naver.com>
2024-08-14 02:12:09 +0900

    refactor: captureError 함수를 useFetch로부터 분리

---
+++ client/src/utils/captureError.ts
@@ -0,0 +1,52 @@
+import FetchError from '@errors/FetchError';
+import {ServerError} from 'ErrorProvider';
+import {NavigateFunction} from 'react-router-dom';
+import sendLogToSentry from './sendLogToSentry';
+import {ROUTER_URLS} from '@constants/routerUrls';
+
+// TODO: (@weadie) 함수 분리
+export const captureError = async (error: Error, navigate: NavigateFunction, eventId: string) => {
+  const errorBody: ServerError =
+    error instanceof FetchError ? error.errorBody : {message: error.message, errorCode: error.name};
+
+  switch (errorBody?.errorCode) {
+    case 'INTERNAL_SERVER_ERROR':
+      sendLogToSentry({error, errorBody, level: 'fatal'});
+      break;
+
+    case 'FORBIDDEN':
+      sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
+    case 'TOKEN_INVALID':
+      sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
+    case 'TOKEN_EXPIRED':
+      sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
+    case 'TOKEN_NOT_FOUND':
+      sendLogToSentry({error, errorBody});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
+    // 비밀 번호를 까먹는 사람이 얼마나 많은 지 추측하기 위함
+    case 'PASSWORD_INVALID':
+      sendLogToSentry({error, errorBody, level: 'debug'});
+      navigate(`${ROUTER_URLS.event}/${eventId}/login`);
+      break;
+
+    // 1천만원 이상 입력하는 사람이 얼마나 많은 지 추측하기 위함
+    case 'BILL_ACTION_PRICE_INVALID':
+      sendLogToSentry({error, errorBody, level: 'debug'});
+      break;
+
+    default:
+      sendLogToSentry({error, errorBody, level: 'fatal'});
+      break;
+  }
+};
