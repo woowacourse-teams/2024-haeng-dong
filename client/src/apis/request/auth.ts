c29ba9da1b0edf54ec45fdd11b35b4cca7c76c42
pakxe <pigkill40@naver.com>
2024-08-14 00:23:00 +0900

    refactor: withEventId 타입을 사용하도록 수정

--- client/src/apis/request/auth.ts
+++ client/src/apis/request/auth.ts
@@ -1,24 +1,20 @@
 import {BASE_URL} from '@apis/baseUrl';
 import {TEMP_PREFIX} from '@apis/tempPrefix';
 import {requestPostWithoutResponse} from '@apis/fetcher';
-
-export type RequestAuthentication = {
-  eventId: string;
-};
+import {WithEventId} from '@apis/withEventId.type';
 
 export type RequestToken = {
-  eventId: string;
   password: string;
 };
 
-export const requestPostAuthentication = async ({eventId}: RequestAuthentication) => {
+export const requestPostAuthentication = async ({eventId}: WithEventId) => {
   await requestPostWithoutResponse({
     baseUrl: BASE_URL.HD,
     endpoint: `${TEMP_PREFIX}/${eventId}/auth`,
   });
 };
 
-export const requestPostToken = async ({eventId, password}: RequestToken) => {
+export const requestPostToken = async ({eventId, password}: WithEventId<RequestToken>) => {
   await requestPostWithoutResponse({
     baseUrl: BASE_URL.HD,
     endpoint: `${TEMP_PREFIX}/${eventId}/login`,
