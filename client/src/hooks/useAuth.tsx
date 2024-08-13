235e06ef2adf4a510ba00fa980294f5c3c15e6cf
pakxe <pigkill40@naver.com>
2024-08-14 00:20:09 +0900

    feat: useAuth훅이 eventId를 스스로 호출하도록 함

--- client/src/hooks/useAuth.tsx
+++ client/src/hooks/useAuth.tsx
@@ -1,15 +1,18 @@
-import {RequestAuthentication, RequestToken, requestPostAuthentication, requestPostToken} from '@apis/request/auth';
+import {RequestToken, requestPostAuthentication, requestPostToken} from '@apis/request/auth';
 
-import {useFetch} from '@apis/useFetch';
+import {useFetch} from '@hooks/useFetch';
+import useEventId from '@hooks/useEventId/useEventId';
 
+// 이 훅의 핵심 기능: 권한을 확인한다.
 const useAuth = () => {
   const {fetch} = useFetch();
+  const {eventId} = useEventId();
 
-  const checkAuthentication = async ({eventId}: RequestAuthentication) => {
+  const checkAuthentication = async () => {
     return await fetch({queryFunction: () => requestPostAuthentication({eventId})});
   };
 
-  const loginUser = async ({eventId, password}: RequestToken) => {
+  const loginUser = async ({password}: RequestToken) => {
     return await fetch({queryFunction: () => requestPostToken({eventId, password})});
   };
 
