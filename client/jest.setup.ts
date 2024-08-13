4eb2fe58a48aca1ecb844e73bf3a0f478ba5208f
pakxe <pigkill40@naver.com>
2024-08-14 00:21:24 +0900

    test: 모든 테스트 사전에 실행되어야 하는 모킹 수행

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,5 +1,15 @@
 import {server} from './src/mocks/server';
+import * as router from 'react-router';
 
-// beforeAll(() => server.listen());
-// afterEach(() => server.resetHandlers());
-// afterAll(() => server.close());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+jest.mock('./src/hooks/useFetch', () => ({
+  ...jest.requireActual('./src/hooks/useFetch'),
+  captureError: jest.fn(),
+}));
+
+jest.mock('./src/utils/sendLogToSentry');
+
+jest.spyOn(router, 'useNavigate').mockImplementation(() => jest.fn());
