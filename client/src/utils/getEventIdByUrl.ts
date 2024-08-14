f5118a304b56bfeacaecdf6daadcc44c66ab3af6
이태훈 <rhymint@gmail.com>
2024-08-14 10:06:38 +0900

    fix: 함수 이름 수정

--- client/src/utils/getEventIdByUrl.ts
+++ client/src/utils/getEventIdByUrl.ts
@@ -1,11 +1,11 @@
-const extractIdFromUrl = (url: string) => {
+const extractEventIdFromUrl = (url: string) => {
   const regex = /\/event\/([a-zA-Z0-9-]+)\//;
   const match = url.match(regex);
   return match ? match[1] : null;
 };
 
 const getEventIdByUrl = () => {
-  return extractIdFromUrl(window.location.pathname) || '';
+  return extractEventIdFromUrl(window.location.pathname) || '';
 };
 
 export default getEventIdByUrl;
