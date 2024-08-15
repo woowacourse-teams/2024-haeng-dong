2641ccf41939a2e31cf7391f7edca29cfcb22409
pakxe <pigkill40@naver.com>
2024-08-16 02:45:52 +0900

    test: test에 사용하는 상수 선언

---
+++ client/src/mocks/serverConstants.ts
@@ -0,0 +1,6 @@
+export const VALID_PASSWORD_LENGTH_IN_SERVER = 4;
+
+export const VALID_EVENT_NAME_LENGTH_IN_SERVER = {
+  min: 2,
+  max: 30,
+};
---
+++ client/src/mocks/validValueForTest.ts
@@ -0,0 +1,2 @@
+export const VALID_PASSWORD_FOR_TEST = 1111;
+export const VALID_TOKEN_FOR_TEST = 'valid-token';
