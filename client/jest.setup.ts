ce6c85841eb59ceadffcf49ce89041ca409d189f
pakxe <pigkill40@naver.com>
2024-08-16 02:23:55 +0900

    chore: test환경에서의 location과 pathname 세팅

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,15 +1,22 @@
 import {server} from './src/mocks/server';
 import * as router from 'react-router';
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+  server.listen();
+
+  Object.defineProperty(window, 'location', {
+    writable: true,
+    value: {
+      ...window.location,
+      pathname: '/event/abc-123/', // 원하는 pathname 설정
+    },
+  });
+});
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-jest.mock('./src/hooks/useFetch', () => ({
-  ...jest.requireActual('./src/hooks/useFetch'),
-  captureError: jest.fn(),
-}));
-
+beforeAll(() => {});
+jest.mock('./src/utils/captureError');
 jest.mock('./src/utils/sendLogToSentry');
 
 jest.spyOn(router, 'useNavigate').mockImplementation(() => jest.fn());
