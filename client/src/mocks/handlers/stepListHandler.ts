838bb7c4f28586ac16e59723833adffb3c0bdec2
pakxe <pigkill40@naver.com>
2024-08-16 02:45:12 +0900

    test: stepList와 관련된 api 모킹 함수 구현

---
+++ client/src/mocks/handlers/stepListHandler.ts
@@ -0,0 +1,99 @@
+import {HttpResponse, http} from 'msw';
+
+import {Bill, MemberType, StepList} from 'types/serviceType';
+
+import {TEMP_PREFIX} from '@apis/tempPrefix';
+
+import stepListJson from '../stepList.json';
+
+type StepListResponseBody = {
+  step: StepList;
+};
+
+type PostMemberListRequestBody = {
+  members: string[];
+  status: MemberType;
+};
+
+type PostBillListRequestBody = {
+  actions: Bill[];
+};
+
+let stepListMockData = stepListJson;
+
+export const stepListHandler = [
+  http.get<any, StepListResponseBody, any, `${typeof TEMP_PREFIX}/:eventId/actions`>(
+    `${TEMP_PREFIX}/:eventId/actions`,
+    () => {
+      return HttpResponse.json({
+        steps: stepListMockData,
+      });
+    },
+  ),
+
+  http.get(`${TEMP_PREFIX}/:eventId/members/current`, () => {
+    return HttpResponse.json({
+      memberNames: stepListMockData
+        .filter(({type}) => type !== 'BILL')
+        .map(({actions}) => actions.map(({name}) => name))
+        .flat(),
+    });
+  }),
+
+  http.delete(`${TEMP_PREFIX}/:eventId/member-actions/:actionId`, () => {
+    return HttpResponse.json({
+      status: 200,
+    });
+  }),
+
+  http.post<any, PostMemberListRequestBody, any, `${typeof TEMP_PREFIX}/:eventId/member-actions`>(
+    `${TEMP_PREFIX}/:eventId/member-actions`,
+    async ({request}) => {
+      const {members, status} = await request.json();
+      stepListMockData = [
+        ...stepListJson,
+        {
+          type: status,
+          stepName: '영차영차',
+          members: status === 'IN' ? members : [],
+          actions: members.map(name => ({
+            actionId: 999,
+            name,
+            price: 0,
+            sequence: 999,
+          })),
+        },
+      ];
+
+      return HttpResponse.json({
+        status: 200,
+      });
+    },
+  ),
+
+  http.post<any, PostBillListRequestBody, any, `${typeof TEMP_PREFIX}/:eventId/bill-actions`>(
+    `${TEMP_PREFIX}/:eventId/bill-actions`,
+    async ({request}) => {
+      const {actions} = await request.json();
+
+      stepListMockData = [
+        ...stepListJson,
+        {
+          type: 'BILL',
+          stepName: '밥스카이',
+          members: [],
+          actions: actions.map(({title, price}, index) => ({
+            actionId: 999,
+            name: title,
+            price,
+            sequence: 999,
+          })),
+        },
+      ];
+
+      return HttpResponse.json({
+        status: 200,
+      });
+    },
+  ),
+];
