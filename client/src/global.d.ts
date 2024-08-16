08c60bd84677d3a6a6e5ff998cd644165c87bc01
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-08-16 16:13:47 +0900

    feat: 환경 변수 타입 선언 (#345)
    * chore: env key 타입 선언
    
    * refactor: env type 선언으로 인한 빈 문자열 대체 제거
    
    * refactor: NODE_ENV 타입 선언으로 빈 문자열 제거 및 development 환경 바로 비교
    
    * refactor: 이벤트 페이지 url 자주사용되는 로직 유틸로 분리
    
    ---------
    
    Co-authored-by: Pakxe <64801796+pakxe@users.noreply.github.com>

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -3,5 +3,9 @@ declare module '*.svg';
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
+
+    // env keys
+    readonly API_BASE_URL: string;
+    readonly AMPLITUDE_KEY: string;
   }
 }
--- client/src/pages/CreateEventPage/CompleteCreateEventPage.tsx
+++ client/src/pages/CreateEventPage/CompleteCreateEventPage.tsx
@@ -6,6 +6,8 @@ import {css} from '@emotion/react';
 
 import {useToast} from '@components/Toast/ToastProvider';
 
+import getEventPageUrlByEnvironment from '@utils/getEventPageUrlByEnvironment';
+
 import {ROUTER_URLS} from '@constants/routerUrls';
 
 const CompleteCreateEventPage = () => {
@@ -26,8 +28,7 @@ const CompleteCreateEventPage = () => {
 
   const {showToast} = useToast();
 
-  const env = process.env.NODE_ENV || '';
-  const homeUrlByEnvironment = `https://${env.includes('development') ? 'dev.' : ''}haengdong.pro${ROUTER_URLS.event}/${url}/home`;
+  const homePageUrl = getEventPageUrlByEnvironment(url, 'home');
 
   return (
     <MainLayout>
@@ -41,10 +42,10 @@ const CompleteCreateEventPage = () => {
           <Text textColor="gray">링크가 없으면 페이지에 접근할 수 없어요.</Text>
           <Text textColor="primary">관리를 위해서 행사 링크를 복사 후 보관해 주세요.</Text>
         </Flex>
-        <Input value={homeUrlByEnvironment} disabled />
+        <Input value={homePageUrl} disabled />
 
         <CopyToClipboard
-          text={homeUrlByEnvironment}
+          text={homePageUrl}
           onCopy={() =>
             showToast({
               showingTime: 3000,
