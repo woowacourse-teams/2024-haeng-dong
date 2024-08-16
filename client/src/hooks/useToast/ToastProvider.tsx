7ecd473fc9ef339bb28e991ece1e29f40054bed3
pakxe <pigkill40@naver.com>
2024-08-17 01:27:50 +0900

    chore: 파일을 hooks내부로 이동

---
+++ client/src/hooks/useToast/ToastProvider.tsx
@@ -0,0 +1,72 @@
+/** @jsxImportSource @emotion/react */
+import {createContext, useContext, useEffect, useState} from 'react';
+
+import {useError} from '@hooks/useError/useError';
+
+import {SERVER_ERROR_MESSAGES} from '@constants/errorMessage';
+
+import {ToastProps} from '../../components/Toast/Toast.type';
+import Toast from '../../components/Toast/Toast';
+
+export const ToastContext = createContext<ToastContextProps | null>(null);
+
+const DEFAULT_TIME = 3000;
+
+interface ToastContextProps {
+  showToast: (args: ShowToast) => void;
+}
+
+type ShowToast = ToastProps & {
+  showingTime?: number;
+  isAlwaysOn?: boolean;
+};
+
+export const ToastProvider = ({children}: React.PropsWithChildren) => {
+  const [currentToast, setCurrentToast] = useState<ShowToast | null>(null);
+  const {errorInfo, clearError, clientErrorMessage} = useError();
+
+  const showToast = ({showingTime = DEFAULT_TIME, isAlwaysOn = false, ...toastProps}: ShowToast) => {
+    setCurrentToast({showingTime, isAlwaysOn, ...toastProps});
+  };
+
+  const closeToast = () => {
+    setCurrentToast(null);
+  };
+
+  useEffect(() => {
+    if (errorInfo !== null) {
+      showToast({
+        message: clientErrorMessage || SERVER_ERROR_MESSAGES.UNHANDLED,
+        showingTime: DEFAULT_TIME, // TODO: (@weadie) 나중에 토스트 프로바이더를 제거한 토스트를 만들 것이기 때문에 많이 리펙터링 안함
+        isAlwaysOn: false,
+        position: 'bottom',
+        bottom: '6.25rem',
+        // TODO: (@soha&weadie) zIndex의 값 추후에 꼭!!! 수정
+        style: {zIndex: '1000'},
+      });
+
+      clearError(DEFAULT_TIME);
+    }
+  }, [errorInfo, clientErrorMessage]);
+
+  useEffect(() => {
+    if (!currentToast) return;
+
+    if (!currentToast.isAlwaysOn) {
+      const timer = setTimeout(() => {
+        setCurrentToast(null);
+      }, currentToast.showingTime);
+
+      return () => clearTimeout(timer);
+    }
+
+    return;
+  }, [currentToast]);
+
+  return (
+    <ToastContext.Provider value={{showToast}}>
+      {currentToast && <Toast onClose={closeToast} {...currentToast} />}
+      {children}
+    </ToastContext.Provider>
+  );
+};
