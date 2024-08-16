469553267fa0025b8dd23cbf578f8cd7453d63e2
JUHA <84626225+khabh@users.noreply.github.com>
2024-08-16 10:56:51 +0900

    refactor: 현재 참여 인원 목록 조회 API 반환 형식 수정 (#361)

--- server/src/main/java/server/haengdong/presentation/response/CurrentMembersResponse.java
+++ server/src/main/java/server/haengdong/presentation/response/CurrentMembersResponse.java
@@ -3,11 +3,11 @@ package server.haengdong.presentation.response;
 import java.util.List;
 import server.haengdong.application.response.CurrentMemberAppResponse;
 
-public record CurrentMembersResponse(List<CurrentMemberResponse> members) {
+public record CurrentMembersResponse(List<String> memberNames) {
 
     public static CurrentMembersResponse of(List<CurrentMemberAppResponse> currentMembers) {
-        List<CurrentMemberResponse> responses = currentMembers.stream()
-                .map(CurrentMemberResponse::of)
+        List<String> responses = currentMembers.stream()
+                .map(CurrentMemberAppResponse::name)
                 .toList();
 
         return new CurrentMembersResponse(responses);
