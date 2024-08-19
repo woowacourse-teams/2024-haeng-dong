7566ba87bc8bebd1be3ec5f9e1516f55b4b7795f
김진호 <rlawlsgh1227@gmail.com>
2024-08-19 14:28:46 +0900

    refactor: 파생상태를 활용해서 state 제거

--- client/src/hooks/useSearchMemberReportList/useSearchMemberReportList.tsx
+++ client/src/hooks/useSearchMemberReportList/useSearchMemberReportList.tsx
@@ -1,7 +1,3 @@
-import type {MemberReport} from 'types/serviceType';
-
-import {useEffect, useState} from 'react';
-
 import useRequestGetMemberReportList from '@hooks/useRequestGetMemberReportList';
 
 type UseSearchMemberReportListParams = {
@@ -11,24 +7,9 @@ type UseSearchMemberReportListParams = {
 const useSearchMemberReportList = ({name}: UseSearchMemberReportListParams) => {
   const {data} = useRequestGetMemberReportList();
   const memberReportList = data ?? [];
-  const [memberReportSearchList, setMemberReportSearchList] = useState<MemberReport[]>([]);
-
-  // TODO: (@weadie) 글자가 완성될 때마다 아래 로직이 실행되어야 합니다.
-  useEffect(() => {
-    if (name === '') setMemberReportSearchList(memberReportList);
-
-    setMemberReportSearchList(memberReportList.filter(memberReport => memberReport.name.includes(name)));
-  }, [name, memberReportList]);
-
-  // TODO: (@weadie) 글자가 완성될 때마다 아래 로직이 실행되어야 합니다.
-  useEffect(() => {
-    if (name === '') setMemberReportSearchList(memberReportList);
-
-    setMemberReportSearchList(memberReportList.filter(memberReport => memberReport.name.includes(name)));
-  }, [name, memberReportList]);
 
   return {
-    memberReportSearchList,
+    memberReportSearchList: memberReportList.filter(memberReport => memberReport.name.includes(name)),
   };
 };
 
