fbb7dd311f0cf6922a8ad5d247b29b517d597a9b
pakxe <pigkill40@naver.com>
2024-08-19 21:06:27 +0900

    test: 라벨이 있는 EditableItem, 라벨이 여러개 있는 EditableItem, 리스트 형태의 EditableItem의 스토리북 작성

--- HDesign/src/components/EditableItem/EditableItem.stories.tsx
+++ HDesign/src/components/EditableItem/EditableItem.stories.tsx
@@ -42,3 +42,67 @@ export const Playground: Story = {
     );
   },
 };
+
+export const WithLabel: Story = {
+  render: ({...args}) => {
+    return (
+      <EditableItem
+        prefixLabelText="라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+        <Flex gap="0.25rem" alignItems="center">
+          <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+          <Text size="caption">원</Text>
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
+
+export const WithLabels: Story = {
+  render: ({...args}) => {
+    return (
+      <EditableItem
+        prefixLabelText="왼쪽 라벨"
+        suffixLabelText="오른쪽 라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+        <Flex gap="0.25rem" alignItems="center">
+          <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+          <Text size="caption">원</Text>
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
+
+export const ListView: Story = {
+  render: ({...args}) => {
+    return (
+      <EditableItem
+        prefixLabelText="라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <Flex flexDirection="column" width="100%" gap="1rem">
+          {new Array(5).fill(0).map(() => (
+            <Flex justifyContent="spaceBetween">
+              <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+              <Flex gap="0.25rem" alignItems="center">
+                <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+                <Text size="caption">원</Text>
+              </Flex>
+            </Flex>
+          ))}
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
