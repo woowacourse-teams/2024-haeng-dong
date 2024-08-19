a4513fcaf5c20155cedfdf250e629f384149349e
Soyeon Choe <soy2302ten@gmail.com>
2024-08-19 16:36:39 +0900

    chore: 변경된 이름 반영

--- client/src/hooks/useRequestPostAuthentication.ts
+++ client/src/hooks/useRequestPostAuthentication.ts
@@ -1,7 +1,7 @@
 import {useMutation} from '@tanstack/react-query';
 import {useNavigate} from 'react-router-dom';
 
-import {requestAuthentication} from '@apis/request/auth';
+import {requestPostAuthentication} from '@apis/request/auth';
 
 import getEventIdByUrl from '@utils/getEventIdByUrl';
 
@@ -12,7 +12,7 @@ const useRequestPostAuthenticate = () => {
   const navigate = useNavigate();
 
   return useMutation({
-    mutationFn: () => requestAuthentication({eventId}),
+    mutationFn: () => requestPostAuthentication({eventId}),
     onError: () => {
       // 에러가 발생하면 로그인 페이지로 리다이렉트
       navigate(`${ROUTER_URLS.event}/${eventId}/login`);
