6a9aabca91e4ad55e71686ba9633eae1a262ad58
이태훈 <rhymint@gmail.com>
2024-08-21 00:53:45 +0900

    fix: 사용하지 않는 코드 주석처리

--- client/src/hooks/useError/useError.tsx
+++ client/src/hooks/useError/useError.tsx
@@ -1,12 +1,12 @@
-import {useContext} from 'react';
+// import {useContext} from 'react';
 
-import {ErrorContext, ErrorContextType} from './ErrorProvider';
+// import {ErrorContext, ErrorContextType} from './ErrorProvider';
 
-// 에러 컨텍스트를 사용하는 커스텀 훅
-export const useError = (): ErrorContextType => {
-  const context = useContext(ErrorContext);
-  if (!context) {
-    throw new Error('useError must be used within an ErrorProvider');
-  }
-  return context;
-};
+// // 에러 컨텍스트를 사용하는 커스텀 훅
+// export const useError = (): ErrorContextType => {
+//   const context = useContext(ErrorContext);
+//   if (!context) {
+//     throw new Error('useError must be used within an ErrorProvider');
+//   }
+//   return context;
+// };
