6e3bef4877a1173c6799393d1fa0d46513e121ba
pakxe <pigkill40@naver.com>
2024-08-20 19:52:29 +0900

    feat: 실제 컴포넌트를 렌더링하고 보여지는 width를 가져오는 함수 구현

--- HDesign/src/components/EditableItem/EditableItem.Input.tsx
+++ HDesign/src/components/EditableItem/EditableItem.Input.tsx
@@ -8,8 +8,26 @@ import {useTheme} from '@theme/HDesignProvider';
 import {inputStyle, inputWrapperStyle} from './EditableItem.Input.style';
 import useEditableItemInput from './useEditableItemInput';
 
-export const EditableItemInput: React.FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(function Input(
-  {textSize = 'body', hasError = false, ...htmlProps},
+// 실제 컴포넌트를 렌더링하고 그 width를 받아와 언더라인의 길이를 정확하게 표시할 수 있도록 함
+const calculateTextWidthWithVirtualElement = (text: string) => {
+  const element = document.createElement('div');
+  element.style.position = 'absolute';
+  element.style.whiteSpace = 'nowrap';
+  element.style.visibility = 'hidden';
+  element.textContent = text;
+
+  document.body.appendChild(element); // 렌더링
+
+  // 요소의 실제 너비 계산
+  const width = element.offsetWidth;
+
+  document.body.removeChild(element); // 제거
+
+  return `${width}px`;
+};
+
+export const EditableItemInput = forwardRef<HTMLInputElement, InputProps>(function Input(
+  {isFixed = false, textSize = 'body', hasError = false, value = '', readOnly = false, ...htmlProps},
   ref,
 ) {
   const {theme} = useTheme();
