89844f9e75dbe62bf84ec3047e6ba5fb554f0b78
pakxe <pigkill40@naver.com>
2024-08-20 19:54:38 +0900

    test: value를 반드시 넘겨주도록 스토리북 수정

--- HDesign/src/components/EditableItem/EditableItem.stories.tsx
+++ HDesign/src/components/EditableItem/EditableItem.stories.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import type {Meta, StoryObj} from '@storybook/react';
 
+import {useState} from 'react';
+
 import EditableItem from '@components/EditableItem/EditableItem';
 import Flex from '@components/Flex/Flex';
 import Text from '@components/Text/Text';
@@ -27,15 +29,29 @@ type Story = StoryObj<typeof meta>;
 
 export const Playground: Story = {
   render: ({...args}) => {
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+
     return (
       <EditableItem
         backgroundColor={args.backgroundColor}
         onFocus={() => console.log('focus')}
         onBlur={() => console.log('blur')}
       >
-        <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+        <EditableItem.Input
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
         <Flex gap="0.25rem" alignItems="center">
-          <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+          <EditableItem.Input
+            value={value2}
+            onChange={e => setValue2(e.target.value)}
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
           <Text size="caption">원</Text>
         </Flex>
       </EditableItem>
@@ -45,6 +61,9 @@ export const Playground: Story = {
 
 export const WithLabel: Story = {
   render: ({...args}) => {
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+
     return (
       <EditableItem
         prefixLabelText="라벨"
@@ -52,9 +71,54 @@ export const WithLabel: Story = {
         onFocus={() => console.log('focus')}
         onBlur={() => console.log('blur')}
       >
-        <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+        <EditableItem.Input
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
         <Flex gap="0.25rem" alignItems="center">
-          <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+          <EditableItem.Input
+            value={value2}
+            onChange={e => setValue2(e.target.value)}
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
+          <Text size="caption">원</Text>
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
+
+export const WithLabelAndIsFixedIcon: Story = {
+  render: ({...args}) => {
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+
+    return (
+      <EditableItem
+        prefixLabelText="라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <EditableItem.Input
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
+        <Flex gap="0.25rem" alignItems="center">
+          <EditableItem.Input
+            value={value2}
+            onChange={e => setValue2(e.target.value)}
+            isFixed={true}
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
           <Text size="caption">원</Text>
         </Flex>
       </EditableItem>
@@ -64,6 +128,9 @@ export const WithLabel: Story = {
 
 export const WithLabels: Story = {
   render: ({...args}) => {
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+
     return (
       <EditableItem
         prefixLabelText="왼쪽 라벨"
@@ -72,9 +139,83 @@ export const WithLabels: Story = {
         onFocus={() => console.log('focus')}
         onBlur={() => console.log('blur')}
       >
-        <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+        <EditableItem.Input
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
         <Flex gap="0.25rem" alignItems="center">
-          <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+          <EditableItem.Input
+            value={value2}
+            onChange={e => setValue2(e.target.value)}
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
+          <Text size="caption">원</Text>
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
+
+export const ReadOnly: Story = {
+  render: ({...args}) => {
+    return (
+      <EditableItem
+        prefixLabelText="왼쪽 라벨"
+        suffixLabelText="오른쪽 라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <EditableItem.Input
+          value={'훠궈무한리필'}
+          readOnly
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
+        <Flex gap="0.25rem" alignItems="center">
+          <EditableItem.Input
+            value={'10000'}
+            readOnly
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
+          <Text size="caption">원</Text>
+        </Flex>
+      </EditableItem>
+    );
+  },
+};
+
+export const ReadOnlyWithIsFixedIcon: Story = {
+  render: ({...args}) => {
+    return (
+      <EditableItem
+        prefixLabelText="왼쪽 라벨"
+        suffixLabelText="오른쪽 라벨"
+        backgroundColor={args.backgroundColor}
+        onFocus={() => console.log('focus')}
+        onBlur={() => console.log('blur')}
+      >
+        <EditableItem.Input
+          value={'훠궈무한리필'}
+          readOnly
+          placeholder="지출 내역"
+          textSize="bodyBold"
+        ></EditableItem.Input>
+        <Flex gap="0.25rem" alignItems="center">
+          <EditableItem.Input
+            isFixed
+            value={'10000'}
+            readOnly
+            placeholder="0"
+            type="number"
+            style={{textAlign: 'right'}}
+          ></EditableItem.Input>
           <Text size="caption">원</Text>
         </Flex>
       </EditableItem>
@@ -84,6 +225,9 @@ export const WithLabels: Story = {
 
 export const ListView: Story = {
   render: ({...args}) => {
+    const [value, setValue] = useState('');
+    const [value2, setValue2] = useState('');
+
     return (
       <EditableItem
         prefixLabelText="라벨"
@@ -94,9 +238,20 @@ export const ListView: Story = {
         <Flex flexDirection="column" width="100%" gap="1rem">
           {new Array(5).fill(0).map(() => (
             <Flex justifyContent="spaceBetween">
-              <EditableItem.Input placeholder="지출 내역" textSize="bodyBold"></EditableItem.Input>
+              <EditableItem.Input
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                placeholder="지출 내역"
+                textSize="bodyBold"
+              ></EditableItem.Input>
               <Flex gap="0.25rem" alignItems="center">
-                <EditableItem.Input placeholder="0" type="number" style={{textAlign: 'right'}}></EditableItem.Input>
+                <EditableItem.Input
+                  value={value2}
+                  onChange={e => setValue2(e.target.value)}
+                  placeholder="0"
+                  type="number"
+                  style={{textAlign: 'right'}}
+                ></EditableItem.Input>
                 <Text size="caption">원</Text>
               </Flex>
             </Flex>
