154fc8fd469cd8c342b48a370d9789367862c1dc
pakxe <pigkill40@naver.com>
2024-08-20 19:53:14 +0900

    feat: 동적으로 input의 width가 변하도록 가상 컴포넌트인 shadowRef를 추가하여 구현

--- HDesign/src/components/EditableItem/EditableItem.Input.tsx
+++ HDesign/src/components/EditableItem/EditableItem.Input.tsx
@@ -1,12 +1,21 @@
 /** @jsxImportSource @emotion/react */
-import React, {forwardRef, useEffect, useImperativeHandle, useRef, useState} from 'react';
+import {forwardRef, useEffect, useRef, useState} from 'react';
 
-import {InputProps} from '@components/EditableItem/EditableItem.Input.type';
+import Flex from '@components/Flex/Flex';
+import Text from '@components/Text/Text';
 
 import {useTheme} from '@theme/HDesignProvider';
 
-import {inputStyle, inputWrapperStyle} from './EditableItem.Input.style';
-import useEditableItemInput from './useEditableItemInput';
+import {
+  editingContainerStyle,
+  inputStyle,
+  inputWrapperStyle,
+  isFixedIconStyle,
+  notEditingContainerStyle,
+  notEditingTextStyle,
+  underlineStyle,
+} from './EditableItem.Input.style';
+import {InputProps} from './EditableItem.Input.type';
 
 // 실제 컴포넌트를 렌더링하고 그 width를 받아와 언더라인의 길이를 정확하게 표시할 수 있도록 함
 const calculateTextWidthWithVirtualElement = (text: string) => {
@@ -32,12 +41,68 @@ export const EditableItemInput = forwardRef<HTMLInputElement, InputProps>(functi
 ) {
   const {theme} = useTheme();
   const inputRef = useRef<HTMLInputElement>(null);
-  const {hasFocus} = useEditableItemInput({inputRef});
-  useImperativeHandle(ref, () => inputRef.current!);
+  const shadowRef = useRef<HTMLDivElement>(null);
+  const [hasFocus, setHasFocus] = useState(false);
+  const [width, setWidth] = useState(
+    value
+      ? calculateTextWidthWithVirtualElement(String(value))
+      : calculateTextWidthWithVirtualElement(htmlProps.placeholder || ' '),
+  );
+
+  const handleDivClick = () => {
+    if (!readOnly) {
+      setHasFocus(true);
+    }
+  };
+
+  useEffect(() => {
+    if (shadowRef.current && inputRef.current) {
+      // 보이지는 않지만 존재하며 value가 담겨있는 Shadow div의 크기를 기준으로 input의 너비를 설정
+      setWidth(`${shadowRef.current.offsetWidth}px`);
+      inputRef.current.style.width = `${shadowRef.current.offsetWidth}px`;
+    }
+  }, [value]);
 
   return (
-    <div css={inputWrapperStyle({theme, hasFocus, hasError})}>
-      <input css={inputStyle({theme, textSize})} ref={inputRef} {...htmlProps} />
+    <div css={inputWrapperStyle}>
+      {hasFocus && !readOnly ? (
+        <Flex flexDirection="row">
+          <div ref={shadowRef} css={editingContainerStyle}>
+            <Text size={textSize}>{value || htmlProps.placeholder}</Text>
+          </div>
+          {isFixed && <div css={isFixedIconStyle({theme})}>*</div>}
+          <div css={underlineStyle({theme, hasError, hasFocus})}>
+            <input
+              onFocus={() => setHasFocus(true)}
+              onBlur={() => setHasFocus(false)}
+              css={[
+                inputStyle({
+                  theme,
+                  textSize,
+                  width,
+                }),
+              ]}
+              ref={inputRef}
+              autoFocus
+              readOnly={readOnly}
+              disabled={readOnly}
+              {...htmlProps}
+              placeholder={htmlProps.placeholder}
+              value={value}
+            />
+          </div>
+        </Flex>
+      ) : (
+        <div onClick={handleDivClick} ref={inputRef} css={notEditingContainerStyle}>
+          <Flex flexDirection="row">
+            {isFixed && <div css={isFixedIconStyle({theme})}>*</div>}
+
+            <Text textColor={value === '' ? 'darkGray' : 'black'} css={notEditingTextStyle} size={textSize}>
+              {value || htmlProps.placeholder}
+            </Text>
+          </Flex>
+        </div>
+      )}
     </div>
   );
 });
