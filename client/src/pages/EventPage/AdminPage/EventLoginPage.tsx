1e37410db71a1e602c0931136e0a4b99b77e50c9
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-08-22 00:54:56 +0900

    feat: 관리자 로그인 페이지에서 스위치가 관리로 활성화되지 않는 버그 (#469)
    * fix: 관리페이지 로그인에서도 top nav 관리 탭으로 활성화 되도록 수정
    
    * refactor: useEventLogin으로 훅 분리

--- client/src/pages/EventPage/AdminPage/EventLoginPage.tsx
+++ client/src/pages/EventPage/AdminPage/EventLoginPage.tsx
@@ -1,48 +1,15 @@
-import {useState} from 'react';
-import {FixedButton, MainLayout, LabelInput, Title, TopNav, Switch} from 'haengdong-design';
+import {FixedButton, LabelInput, Title} from 'haengdong-design';
 
-import validateEventPassword from '@utils/validate/validateEventPassword';
-import useRequestPostLogin from '@hooks/queries/useRequestPostLogin';
-
-import useNavSwitch from '@hooks/useNavSwitch';
+import useEventLogin from '@hooks/useEventLogin';
 
 import RULE from '@constants/rule';
 import {PASSWORD_LENGTH} from '@constants/password';
 
 const EventLoginPage = () => {
-  const [password, setPassword] = useState('');
-  const {nav, paths, onChange} = useNavSwitch();
-  const [errorMessage, setErrorMessage] = useState('');
-  const [canSubmit, setCanSubmit] = useState(false);
-  const {mutate: postLogin} = useRequestPostLogin();
-
-  const submitPassword = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    postLogin({password}, {onError: () => setErrorMessage('비밀번호가 틀렸어요')});
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = event.target.value;
-    const validation = validateEventPassword(newValue);
-
-    setCanSubmit(newValue.length === RULE.maxEventPasswordLength);
-
-    if (validation.isValid) {
-      setPassword(newValue);
-      setErrorMessage('');
-    } else {
-      event.target.value = password;
-      setErrorMessage(validation.errorMessage ?? '');
-    }
-  };
+  const {password, errorMessage, handleChange, canSubmit, submitPassword} = useEventLogin();
 
   return (
-    <MainLayout>
-      <TopNav>
-        <Switch value={nav} values={paths} onChange={onChange} />
-        {/* <Back /> */}
-      </TopNav>
+    <>
       <Title
         title="행사 비밀번호 입력"
         description={`관리를 위해선 비밀번호가 필요해요. 행사 생성 시 설정한 ${PASSWORD_LENGTH} 자리의 숫자 비밀번호를 입력해 주세요.`}
@@ -61,7 +28,7 @@ const EventLoginPage = () => {
         ></LabelInput>
         <FixedButton disabled={!canSubmit}>관리 페이지로</FixedButton>
       </form>
-    </MainLayout>
+    </>
   );
 };
 
