baf7a6e62ee708227bec250fe49eb8b53d2a0597
김진호 <rlawlsgh1227@gmail.com>
2024-08-21 23:47:28 +0900

    fix: 관리페이지 로그인에서도 top nav 관리 탭으로 활성화 되도록 수정

--- client/src/pages/EventPage/AdminPage/EventLoginPage.tsx
+++ client/src/pages/EventPage/AdminPage/EventLoginPage.tsx
@@ -1,17 +1,14 @@
 import {useState} from 'react';
-import {FixedButton, MainLayout, LabelInput, Title, TopNav, Switch} from 'haengdong-design';
+import {FixedButton, MainLayout, LabelInput, Title} from 'haengdong-design';
 
 import validateEventPassword from '@utils/validate/validateEventPassword';
 import useRequestPostLogin from '@hooks/queries/useRequestPostLogin';
-
-import useNavSwitch from '@hooks/useNavSwitch';
 
 import RULE from '@constants/rule';
 import {PASSWORD_LENGTH} from '@constants/password';
 
 const EventLoginPage = () => {
   const [password, setPassword] = useState('');
-  const {nav, paths, onChange} = useNavSwitch();
   const [errorMessage, setErrorMessage] = useState('');
   const [canSubmit, setCanSubmit] = useState(false);
   const {mutate: postLogin} = useRequestPostLogin();
@@ -38,11 +35,7 @@ const EventLoginPage = () => {
   };
 
   return (
-    <MainLayout>
-      <TopNav>
-        <Switch value={nav} values={paths} onChange={onChange} />
-        {/* <Back /> */}
-      </TopNav>
+    <>
       <Title
         title="행사 비밀번호 입력"
         description={`관리를 위해선 비밀번호가 필요해요. 행사 생성 시 설정한 ${PASSWORD_LENGTH} 자리의 숫자 비밀번호를 입력해 주세요.`}
@@ -61,7 +54,7 @@ const EventLoginPage = () => {
         ></LabelInput>
         <FixedButton disabled={!canSubmit}>관리 페이지로</FixedButton>
       </form>
-    </MainLayout>
+    </>
   );
 };
 
