6152fc5ffe74f79db8dab0ce8dc91b25c90241f6
Soyeon Choe <soy2302ten@gmail.com>
2024-08-21 11:23:37 +0900

    feat: BillItem이 존재하지 않을 때, 새로운 BillItem을 생성하여 지출 input 만들기

--- client/src/components/StepList/StepList.tsx
+++ client/src/components/StepList/StepList.tsx
@@ -1,5 +1,5 @@
 import {Flex} from 'haengdong-design';
-import {useMemo} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 
 import {BillStep, MemberStep} from 'types/serviceType';
 import useRequestGetStepList from '@hooks/queries/useRequestGetStepList';
@@ -13,14 +13,41 @@ interface StepListProps {
 
 const StepList = ({isAddEditableItem, setIsAddEditableItem}: StepListProps) => {
   const {data: stepListData} = useRequestGetStepList();
-  const stepList = stepListData ?? ([] as (MemberStep | BillStep)[]);
+  const [stepList, setStepList] = useState<(MemberStep | BillStep)[]>([]);
+  const existIndexInStepList = stepList.map((step, index) => ({...step, index}));
+  const [hasAddedItem, setHasAddedItem] = useState(false);
+
+  useEffect(() => {
+    if (stepListData) {
+      setStepList(stepListData);
+    }
+  }, [stepListData]);
 
   const lastBillItemIndex = useMemo(() => {
-    const billSteps = stepList.map((step, index) => ({...step, index})).filter(step => step.type === 'BILL');
+    const billSteps = existIndexInStepList.filter(step => step.type === 'BILL');
 
     // billSteps 배열이 비어 있지 않으면 마지막 항목의 index를 반환, 그렇지 않으면 -1을 반환
     return billSteps.length > 0 ? billSteps.slice(-1)[0].index : -1;
   }, [stepList]);
+
+  const lastItemIndex = useMemo(() => {
+    return existIndexInStepList.length > 0 ? existIndexInStepList.slice(-1)[0].index : -1;
+  }, [existIndexInStepList]);
+
+  useEffect(() => {
+    if (isAddEditableItem && lastBillItemIndex !== lastItemIndex && !hasAddedItem) {
+      setStepList(prev => [
+        ...prev,
+        {
+          type: 'BILL',
+          stepName: '',
+          members: [],
+          actions: [],
+        },
+      ]);
+      setHasAddedItem(true);
+    }
+  }, [isAddEditableItem, lastBillItemIndex, lastItemIndex, hasAddedItem]);
 
   return (
     <Flex flexDirection="column" gap="0.5rem" paddingInline="0.5rem">
@@ -29,6 +56,7 @@ const StepList = ({isAddEditableItem, setIsAddEditableItem}: StepListProps) => {
           index={index}
           step={step}
           lastBillItemIndex={lastBillItemIndex}
+          lastItemIndex={lastItemIndex}
           key={`${step.stepName}${index}`}
           isAddEditableItem={isAddEditableItem}
           setIsAddEditableItem={setIsAddEditableItem}
