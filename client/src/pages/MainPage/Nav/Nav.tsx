f91405c798963cfeae77df5d5e1b326be963800b
이태훈 <rhymint@gmail.com>
2024-08-23 02:28:14 +0900

    fix: 흔듯콘 logo에 사용

--- client/src/pages/MainPage/Nav/Nav.tsx
+++ client/src/pages/MainPage/Nav/Nav.tsx
@@ -1,17 +1,23 @@
-import {Button, Text} from 'haengdong-design';
+import {Button, Flex, Text} from 'haengdong-design';
 import {useNavigate} from 'react-router-dom';
 
 import {ROUTER_URLS} from '@constants/routerUrls';
 
 import {logoStyle, navStyle} from './Nav.style';
 
+import Heundeut from '@assets/image/heundeut.svg';
+import HeundeutLoto from '@components/Common/Logo/HeunDeutLogo';
+
 const Nav = () => {
   const navigate = useNavigate();
   return (
     <header css={navStyle}>
-      <div css={logoStyle}>
-        <Text size="subTitle">행동대장</Text>
-      </div>
+      <Flex gap="0.5rem">
+        <Heundeut />
+        <div css={logoStyle}>
+          <Text size="subTitle">행동대장</Text>
+        </div>
+      </Flex>
       <Button size="medium" variants="tertiary" onClick={() => navigate(ROUTER_URLS.eventCreateName)}>
         정산 시작하기
       </Button>
