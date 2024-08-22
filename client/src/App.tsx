aec4174d6b25c4409d36f5191494a4be84973983
pakxe <pigkill40@naver.com>
2024-08-22 18:57:21 +0900

    feat: App에 ErrorBoundary 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,22 +3,25 @@ import {HDesignProvider} from 'haengdong-design';
 import {Global} from '@emotion/react';
 
 import {ToastProvider} from '@hooks/useToast/ToastProvider';
-import AppErrorBoundary from '@components/AppErrorBoundary/AppErrorBoundary';
 import QueryClientBoundary from '@components/QueryClientBoundary/QueryClientBoundary';
+import ErrorCatcher from '@components/AppErrorBoundary/ErrorCatcher';
 
+import UnhandledErrorBoundary from './UnhandledErrorBoudnary';
 import {GlobalStyle} from './GlobalStyle';
 
 const App: React.FC = () => {
   return (
     <HDesignProvider>
-      <Global styles={GlobalStyle} />
-      <ToastProvider>
-        <AppErrorBoundary>
-          <QueryClientBoundary>
-            <Outlet />
-          </QueryClientBoundary>
-        </AppErrorBoundary>
-      </ToastProvider>
+      <UnhandledErrorBoundary>
+        <Global styles={GlobalStyle} />
+        <ToastProvider>
+          <ErrorCatcher>
+            <QueryClientBoundary>
+              <Outlet />
+            </QueryClientBoundary>
+          </ErrorCatcher>
+        </ToastProvider>
+      </UnhandledErrorBoundary>
     </HDesignProvider>
   );
 };
