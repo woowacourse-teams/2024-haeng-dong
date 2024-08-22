ebc2bc262282339b8828ab0ddadf6079e21ee1ad
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-08-23 00:07:09 +0900

    chore: React-Query devtools 환경 설정 (#463)
    * chore: devtools 설치
    
    * chore: stale time, gctime 셋팅 임시로 1분 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {Outlet} from 'react-router-dom';
 import {HDesignProvider} from 'haengdong-design';
 import {Global} from '@emotion/react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 import {ToastProvider} from '@hooks/useToast/ToastProvider';
 import {ErrorProvider} from '@hooks/useError/ErrorProvider';
@@ -9,12 +10,20 @@ import {ErrorProvider} from '@hooks/useError/ErrorProvider';
 import {GlobalStyle} from './GlobalStyle';
 import UnhandledErrorBoundary from './UnhandledErrorBoundary';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60, // 1 minute
+      gcTime: 1000 * 60, // 1 minute
+    },
+  },
+});
 
 const App: React.FC = () => {
   return (
     <HDesignProvider>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <UnhandledErrorBoundary>
           <Global styles={GlobalStyle} />
           <ErrorProvider>
