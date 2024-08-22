facd363db099d493e145b5beb8b5ee6465df0f2e
pakxe <pigkill40@naver.com>
2024-08-22 19:00:42 +0900

    feat: 에러 페이지에 메일 추가

--- client/src/pages/ErrorPage/ErrorPage.tsx
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,12 @@
 import {MainLayout, Title} from 'haengdong-design';
 
-// TODO: (@weadie) 임시 에러 페이지입니다.
 const ErrorPage = () => {
   return (
     <MainLayout>
-      <Title title="알 수 없는 오류입니다." description="오류가 난 상황에 대해 {메일}로 연락주시면 소정의 상품을..." />
+      <Title
+        title="알 수 없는 오류입니다."
+        description="오류가 난 상황에 대해 haengdongdj@gmail.com 로 연락주시면 소정의 상품을 드립니다."
+      />
     </MainLayout>
   );
 };
