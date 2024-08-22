926232647119b6df330a315dcb7ba591362c2d5f
pakxe <pigkill40@naver.com>
2024-08-22 20:47:23 +0900

    chore: 개행 추가

--- client/src/components/QueryClientBoundary/QueryClientBoundary.tsx
+++ client/src/components/QueryClientBoundary/QueryClientBoundary.tsx
@@ -4,6 +4,7 @@ import {useAppErrorStore} from '@store/appErrorStore';
 
 const QueryClientBoundary = ({children}: React.PropsWithChildren) => {
   const {updateAppError} = useAppErrorStore();
+
   const queryClient = new QueryClient({
     queryCache: new QueryCache({
       onError: (error: Error) => {
@@ -16,6 +17,7 @@ const QueryClientBoundary = ({children}: React.PropsWithChildren) => {
       },
     }),
   });
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
 
