28ea8578b212ea50f48e842a791fb8924fdc5670
이태훈 <rhymint@gmail.com>
2024-08-28 19:57:47 +0900

    chore: workflow 작성 및 script 수정

--- client/packages/haengdong-design/src/components/Button/Button.style.ts
+++ client/packages/haengdong-design/src/components/Button/Button.style.ts
@@ -75,6 +75,7 @@ const getButtonVariantsStyle = (variants: ButtonVariants, theme: Theme) => {
     primary: [
       css({
         backgroundColor: theme.colors.primary,
+        // backgroundColor: 'white',
         color: theme.colors.onPrimary,
       }),
       getHoverAndActiveBackground(theme.colors.primary),
