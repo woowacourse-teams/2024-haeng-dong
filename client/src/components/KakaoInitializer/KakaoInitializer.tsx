f773b5967da2c4676a5c4d60478c5d7dbca3c6fd
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-09-10 23:02:49 +0900

    fix: test 환경에선 돌아가지 않도록 부등호수정

--- client/src/components/KakaoInitializer/KakaoInitializer.tsx
+++ client/src/components/KakaoInitializer/KakaoInitializer.tsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react';
 
 const KakaoInitializer = ({children}: React.PropsWithChildren) => {
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'test') return;
+    if (process.env.NODE_ENV === 'test') return;
 
     if (!window.Kakao.isInitialized()) {
       window.Kakao.init(process.env.KAKAO_JAVASCRIPT_KEY);
