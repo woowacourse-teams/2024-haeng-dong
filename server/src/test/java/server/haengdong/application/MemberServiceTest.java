5a951c9de25cf31aeabd4183e9e185c5b64850a0
3juhwan <13selfesteem91@naver.com>
2024-09-15 01:15:36 +0900

    test: 현재 멤버 조회 기능 서비스 테스트

--- server/src/test/java/server/haengdong/application/MemberServiceTest.java
+++ server/src/test/java/server/haengdong/application/MemberServiceTest.java
@@ -3,25 +3,27 @@ package server.haengdong.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.tuple;
+import static server.haengdong.support.fixture.Fixture.BILL1;
+import static server.haengdong.support.fixture.Fixture.EVENT1;
 import static server.haengdong.support.fixture.Fixture.MEMBER1;
 import static server.haengdong.support.fixture.Fixture.MEMBER2;
 import static server.haengdong.support.fixture.Fixture.MEMBER3;
 
 import java.util.List;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import server.haengdong.application.response.MemberAppResponse;
 import server.haengdong.application.response.MemberDepositAppResponse;
 import server.haengdong.application.response.MembersDepositAppResponse;
 import server.haengdong.domain.action.Bill;
+import server.haengdong.domain.action.BillDetail;
 import server.haengdong.domain.action.BillRepository;
 import server.haengdong.domain.action.Member;
 import server.haengdong.domain.action.MemberRepository;
 import server.haengdong.domain.event.Event;
 import server.haengdong.domain.event.EventRepository;
 import server.haengdong.exception.HaengdongException;
-import server.haengdong.support.fixture.Fixture;
 
 class MemberServiceTest extends ServiceTestSupport {
 
@@ -40,14 +42,14 @@ class MemberServiceTest extends ServiceTestSupport {
     @DisplayName("행사에 참여한 전체 인원을 조회한다.")
     @Test
     void findAllMembersTest() {
-        Event event = Fixture.EVENT1;
-        Bill bill = Fixture.BILL1;
+        Event event = EVENT1;
+        Bill bill = BILL1;
         Member member1 = MEMBER1;
         Member member2 = MEMBER2;
         Member member3 = MEMBER3;
         eventRepository.save(event);
-        billRepository.save(bill);
         memberRepository.saveAll(List.of(member1, member2, member3));
+        billRepository.save(bill);
 
         MembersDepositAppResponse membersDepositAppResponse = memberService.findAllMembers(event.getToken());
 
@@ -60,14 +62,33 @@ class MemberServiceTest extends ServiceTestSupport {
                 );
     }
 
-    @Disabled
     @DisplayName("행사에 현재 참여 중인 인원을 조회한다.")
     @Test
     void getCurrentMembersTest() {
-        // 테스트를 위해 필요한 도메인을 어떻게 생성할 것인지 논의 후 작성
+        Event event = EVENT1;
+        Bill bill = BILL1;
+        Member member1 = MEMBER1;
+        Member member2 = MEMBER2;
+        Member member3 = MEMBER3;
+        bill.addDetail(new BillDetail(bill, member1, 10000L, false));
+        bill.addDetail(new BillDetail(bill, member2, 20000L, false));
+        bill.addDetail(new BillDetail(bill, member3, 30000L, false));
+        eventRepository.save(event);
+        memberRepository.saveAll(List.of(member1, member2, member3));
+        billRepository.save(bill);
+
+        List<MemberAppResponse> currentMembers = memberService.getCurrentMembers(event.getToken());
+
+        assertThat(currentMembers).hasSize(3)
+                .extracting(MemberAppResponse::id, MemberAppResponse::name)
+                .containsExactlyInAnyOrder(
+                        tuple(member1.getId(), member1.getName()),
+                        tuple(member2.getId(), member2.getName()),
+                        tuple(member3.getId(), member3.getName())
+                );
     }
 
-    @DisplayName("이벤트가 없으면 현재 참여 인원을 조회할 수 없다.")
+    @DisplayName("행사가 없으면 현재 참여 인원을 조회할 수 없다.")
     @Test
     void getCurrentMembersTest1() {
         assertThatThrownBy(() -> memberService.getCurrentMembers("token"))
