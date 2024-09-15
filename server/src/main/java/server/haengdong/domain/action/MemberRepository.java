3b6c30aed1c220197ab5ad44901e3b0c24e60106
juha <khabh@naver.com>
2024-09-15 16:38:41 +0900

    refactor: 테스트에서만 사용하는 메서드 삭제

--- server/src/main/java/server/haengdong/domain/action/MemberRepository.java
+++ server/src/main/java/server/haengdong/domain/action/MemberRepository.java
@@ -1,12 +1,9 @@
 package server.haengdong.domain.action;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.haengdong.domain.event.Event;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findAllByEvent(Event event);
-
-    Optional<Member> findByEventAndName(Event event, String name);
 }
