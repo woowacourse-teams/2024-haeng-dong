1d725eb593b36baf7aeba7704178c638283af10e
이태훈 <rhymint@gmail.com>
2024-09-17 22:14:52 +0900

    fix: Title component 수정

--- client/src/components/Design/components/Title/Title.tsx
+++ client/src/components/Design/components/Title/Title.tsx
@@ -1,31 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import Flex from '@HDcomponents/Flex/Flex';
 import Text from '@HDcomponents/Text/Text';
-import {priceContainerStyle, titleContainerStyle} from '@HDcomponents/Title/Title.style';
+import {amountContainerStyle, titleContainerStyle, titleStyle} from '@HDcomponents/Title/Title.style';
 import {TitleProps} from '@HDcomponents/Title/Title.type';
 import {useTheme} from '@theme/HDesignProvider';
+import Icon from '../Icon/Icon';
+import Amount from '../Amount/Amount';
 
-export const Title: React.FC<TitleProps> = ({title, description, price}: TitleProps) => {
+export const Title: React.FC<TitleProps> = ({title, amount}: TitleProps) => {
   const {theme} = useTheme();
   return (
-    <div css={titleContainerStyle(theme)}>
-      <Text size="subTitle">{title}</Text>
-      {description && (
-        <Text textColor="darkGray" size="body">
-          {description}
+    <div css={titleStyle(theme)}>
+      <div css={titleContainerStyle}>
+        <Text size="subTitle">{title}</Text>
+        <Icon iconType="meatballs" />
+      </div>
+      <div css={amountContainerStyle}>
+        <Text textColor="gray" size="caption">
+          전체 지출 금액
         </Text>
-      )}
-      {price !== undefined && (
-        <div css={priceContainerStyle}>
-          <Text textColor="gray" size="caption">
-            전체 지출 금액
-          </Text>
-          <Flex alignItems="center" gap="0.25rem">
-            <Text>{price.toLocaleString('ko-kr')}</Text>
-            <Text size="caption">원</Text>
-          </Flex>
-        </div>
-      )}
+        <Amount amount={amount ?? 0} />
+      </div>
     </div>
   );
 };
