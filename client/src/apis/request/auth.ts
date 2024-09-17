0ec52fc72eb100c36b65eef674ed5fe9f55b809d
이태훈 <rhymint@gmail.com>
2024-09-17 22:13:14 +0900

    fix: 잘못 적용된 endpoint 적용

--- client/src/apis/request/auth.ts
+++ client/src/apis/request/auth.ts
@@ -1,5 +1,5 @@
 import {BASE_URL} from '@apis/baseUrl';
-import {ADMIN_API_PREFIX} from '@apis/endpointPrefix';
+import {ADMIN_API_PREFIX, USER_API_PREFIX} from '@apis/endpointPrefix';
 import {requestPostWithoutResponse} from '@apis/fetcher';
 import {WithEventId} from '@apis/withEventId.type';
 
@@ -17,7 +17,7 @@ export const requestPostAuthentication = async ({eventId}: WithEventId) => {
 export const requestPostToken = async ({eventId, password}: WithEventId<RequestToken>) => {
   await requestPostWithoutResponse({
     baseUrl: BASE_URL.HD,
-    endpoint: `${ADMIN_API_PREFIX}/${eventId}/login`,
+    endpoint: `${USER_API_PREFIX}/${eventId}/login`,
     body: {
       password: password,
     },
