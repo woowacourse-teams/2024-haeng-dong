70d1d94509b2d4f9716938283e836f880d57f34a
Soyeon Choe <soy2302ten@gmail.com>
2024-09-19 09:33:10 +0900

    refactor: 스토리북에서 DepositToggle을 클릭시 focus가 변경되도록 수정

--- client/src/components/Design/components/DepositToggle/DepositToggle.stories.tsx
+++ client/src/components/Design/components/DepositToggle/DepositToggle.stories.tsx
@@ -1,4 +1,7 @@
+/** @jsxImportSource @emotion/react */
 import type {Meta, StoryObj} from '@storybook/react';
+
+import {useEffect, useState} from 'react';
 
 import {DepositToggle} from './DepositToggle';
 
@@ -20,14 +23,29 @@ const meta = {
       options: [undefined, () => alert('change toggle')],
     },
   },
-  args: {
-    isDeposit: false,
-    onToggle: () => alert('change toggle'),
-  },
 } satisfies Meta<typeof DepositToggle>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Playground: Story = {};
+export const Playground: Story = {
+  args: {
+    isDeposit: false,
+    onToggle: () => {},
+  },
+  render: ({isDeposit, onToggle, ...args}) => {
+    const [isDepositState, setIsDepositState] = useState(isDeposit);
+
+    useEffect(() => {
+      setIsDepositState(isDeposit);
+    }, [isDeposit]);
+
+    const handleToggle = () => {
+      setIsDepositState(!isDepositState);
+      onToggle();
+    };
+
+    return <DepositToggle {...args} isDeposit={isDepositState} onToggle={handleToggle} />;
+  },
+};
