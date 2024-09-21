102cbf64aca1eb7768a6943be51dcff5215e0ab5
김진호 <rlawlsgh1227@gmail.com>
2024-09-21 14:08:02 +0900

    style: 사용하지 않게 된 핸들러 제거

--- client/src/mocks/sharedState.ts
+++ client/src/mocks/sharedState.ts
@@ -1,7 +1,7 @@
 export let eventData = {
-  eventName: 'MSW 야유회',
-  bankName: '',
-  accountNumber: '',
+  eventName: '행동대장 야유회',
+  bankName: 'KB국민은행',
+  accountNumber: '000000-01-121212',
 };
 
 export let memberData = {
