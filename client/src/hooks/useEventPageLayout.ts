169ee742b3a331ca2fdb12865d30871c3558d9f7
김진호 <rlawlsgh1227@gmail.com>
2024-09-21 13:18:25 +0900

    style: get event return을 묶어서 return

--- client/src/hooks/useEventPageLayout.ts
+++ client/src/hooks/useEventPageLayout.ts
@@ -4,16 +4,22 @@ import getEventIdByUrl from '@utils/getEventIdByUrl';
 
 import {ROUTER_URLS} from '@constants/routerUrls';
 
-import useRequestGetEventOutline from './queries/useRequestGetEventOutline';
 import useNavSwitch from './useNavSwitch';
+import useRequestGetEvent from './queries/event/useRequestGetEvent';
 
 const useEventPageLayout = () => {
   const navProps = useNavSwitch();
   const eventId = getEventIdByUrl();
-  const eventOutline = useRequestGetEventOutline({eventId});
+  const {eventName, bankName, accountNumber} = useRequestGetEvent();
 
   const isAdmin = useMatch(ROUTER_URLS.eventManage) !== null;
   const isLoginPage = useMatch(ROUTER_URLS.eventLogin) !== null;
+
+  const eventOutline = {
+    eventName,
+    bankName,
+    accountNumber,
+  };
 
   return {
     eventId,
