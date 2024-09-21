f68654c45f2aca2083618af1532f2a737cd92f67
pakxe <pigkill40@naver.com>
2024-09-21 19:45:43 +0900

    feat: 토스트를 띄우기 위해 App에 ToastContainer 호출

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,9 @@ import {Outlet} from 'react-router-dom';
 import {Global} from '@emotion/react';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
-import {ToastProvider} from '@hooks/useToast/ToastProvider';
 import QueryClientBoundary from '@components/QueryClientBoundary/QueryClientBoundary';
 import ErrorCatcher from '@components/AppErrorBoundary/ErrorCatcher';
+import ToastContainer from '@components/Toast/ToastContainer';
 
 import {HDesignProvider} from '@HDesign/index';
 
@@ -16,14 +16,13 @@ const App: React.FC = () => {
     <HDesignProvider>
       <UnhandledErrorBoundary>
         <Global styles={GlobalStyle} />
-        <ToastProvider>
-          <ErrorCatcher>
-            <QueryClientBoundary>
-              <ReactQueryDevtools initialIsOpen={false} />
-              <Outlet />
-            </QueryClientBoundary>
-          </ErrorCatcher>
-        </ToastProvider>
+        <ToastContainer />
+        <ErrorCatcher>
+          <QueryClientBoundary>
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Outlet />
+          </QueryClientBoundary>
+        </ErrorCatcher>
       </UnhandledErrorBoundary>
     </HDesignProvider>
   );
