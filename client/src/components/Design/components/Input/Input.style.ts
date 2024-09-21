eba6146cdf546709b9ad56e6c9eb7703dabdf408
jinho_kim98 <rlawlsgh1227@gmail.com>
2024-09-21 23:53:20 +0900

    feat: inputType search일 때 검색 아이콘 보이도록 설정

--- client/src/components/Design/components/Input/Input.style.ts
+++ client/src/components/Design/components/Input/Input.style.ts
@@ -4,35 +4,17 @@ import {Theme} from '@theme/theme.type';
 
 import {InputType} from './Input.type';
 
-const getBackgroundColorStyle = (theme: Theme, inputType: InputType = 'input') => {
-  switch (inputType) {
-    case 'input':
-      return theme.colors.lightGrayContainer;
-
-    case 'search':
-      return theme.colors.white;
-
-    default:
-      return theme.colors.lightGrayContainer;
-  }
-};
-
 const getBorderStyle = (isFocus: boolean, theme: Theme, isError?: boolean) =>
   isError ? `0 0 0 1px ${theme.colors.error} inset` : isFocus ? `0 0 0 1px ${theme.colors.primary} inset` : 'none';
 
-export const inputBoxStyle = (
-  theme: Theme,
-  inputType: InputType = 'input',
-  isFocus: boolean,
-  isError: boolean | undefined,
-) =>
+export const inputBoxStyle = (theme: Theme, isFocus: boolean, isError: boolean | undefined) =>
   css({
     display: 'flex',
     justifyContent: 'space-between',
     gap: '1rem',
     padding: '0.75rem 1rem',
     borderRadius: '1rem',
-    backgroundColor: getBackgroundColorStyle(theme, inputType),
+    backgroundColor: theme.colors.lightGrayContainer,
 
     transition: '0.2s',
     transitionTimingFunction: 'cubic-bezier(0.7, 0.62, 0.62, 1.16)',
--- client/src/components/Design/components/Input/Input.tsx
+++ client/src/components/Design/components/Input/Input.tsx
@@ -25,7 +25,7 @@ export const Input: React.FC<InputProps> = forwardRef<HTMLInputElement, InputPro
   useImperativeHandle(ref, () => inputRef.current!);
 
   return (
-    <div css={inputBoxStyle(theme, inputType, hasFocus, isError)}>
+    <div css={inputBoxStyle(theme, hasFocus, isError)}>
       <input
         css={inputStyle(theme)}
         ref={inputRef}
@@ -38,9 +38,14 @@ export const Input: React.FC<InputProps> = forwardRef<HTMLInputElement, InputPro
         autoFocus={autoFocus}
         {...htmlProps}
       />
-      {value && hasFocus && (
+      {inputType === 'input' && value && hasFocus && (
         <IconButton tabIndex={-1} variants="none" onMouseDown={handleClickDelete}>
           <Icon iconType="inputDelete" />
+        </IconButton>
+      )}
+      {inputType === 'search' && (
+        <IconButton tabIndex={-1} variants="none">
+          <Icon iconType="search" />
         </IconButton>
       )}
     </div>
