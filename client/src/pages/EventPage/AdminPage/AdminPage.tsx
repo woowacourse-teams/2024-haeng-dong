48bf85dd55aecef62b767335009502a733a8a7b8
김진호 <rlawlsgh1227@gmail.com>
2024-09-23 17:05:38 +0900

    feat: AdminPage에 dropdown 컴포넌트 적용

--- client/src/pages/EventPage/AdminPage/AdminPage.tsx
+++ client/src/pages/EventPage/AdminPage/AdminPage.tsx
@@ -7,7 +7,7 @@ import useRequestGetSteps from '@hooks/queries/step/useRequestGetSteps';
 
 import {useTotalExpenseAmountStore} from '@store/totalExpenseAmountStore';
 
-import {Title, Button} from '@HDesign/index';
+import {Title, Button, Dropdown, DropdownButton} from '@HDesign/index';
 
 import getEventIdByUrl from '@utils/getEventIdByUrl';
 
@@ -31,7 +31,16 @@ const AdminPage = () => {
 
   return (
     <section css={receiptStyle}>
-      <Title title={eventName} amount={totalExpenseAmount} />
+      <Title
+        title={eventName}
+        amount={totalExpenseAmount}
+        dropdown={
+          <Dropdown>
+            <DropdownButton text="전체 참여자 관리" />
+            <DropdownButton text="계좌번호 입력하기" />
+          </Dropdown>
+        }
+      />
       <StepList data={steps ?? []} isAdmin={isAdmin} />
       <Button size="medium" onClick={() => navigate(`/event/${eventId}/addBill`)} style={{width: '100%'}}>
         지출내역 추가하기
