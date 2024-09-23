39b70ffbe4300e3c74cd1bbddc28549f38602565
pakxe <pigkill40@naver.com>
2024-09-23 21:19:10 +0900

    feat: 불필요해진 행사 생성 완료 path 제거

--- client/src/constants/routerUrls.ts
+++ client/src/constants/routerUrls.ts
@@ -1,8 +1,7 @@
 export const ROUTER_URLS = {
   main: '/',
-  createEvent: '/event/create/',
-  eventCreateComplete: '/event/create/complete',
-  event: '/event', // TODO: (@weadie) baseurl을 어떻게 관리할 것인가?
+  createEvent: '/event/create',
+  event: '/event',
   eventLogin: '/event/:eventId/login',
   eventManage: '/event/:eventId/admin',
   home: '/event/:eventId/home',
