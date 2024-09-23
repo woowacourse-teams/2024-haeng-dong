ea487e5cab7991d47729866a37e800b128d59a98
pakxe <pigkill40@naver.com>
2024-09-23 21:12:16 +0900

    test: 퍼널이 도입되어 행사 생성 과정에서 url이 변경되지 않기 때문에 url이 올바르게 변경되었는지 판단하는 라인을 제거

--- client/cypress/e2e/createEvent.cy.ts
+++ client/cypress/e2e/createEvent.cy.ts
@@ -8,7 +8,7 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
   it('랜딩페이지에서 "행사 생성하기" 버튼을 눌러 행사 이름 입력 페이지로 이동해야 한다.', () => {
     cy.visit('/');
     cy.get('header').find('button').click();
-    cy.url().should('include', '/event/create/name');
+    cy.url().should('include', '/event/create');
   });
 
   context('행사 이름 입력 페이지', () => {
@@ -19,7 +19,6 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
     it('행사 이름 입력 페이지에서 input이 포커싱 되어 있고, "다음" 버튼이 비활성화 되어 있어야 한다.', () => {
       cy.get('input').focused();
       cy.get('button').contains('다음').should('have.attr', 'disabled');
-      cy.url().should('include', '/event/create/name');
     });
 
     it('행사 이름이 1자 이상 입력된 경우 "다음" 버튼이 활성화 되고, 값이 없는 경우 "다음" 버튼이 비활성화 되어야 한다.', () => {
@@ -28,13 +27,11 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
       cy.get('input').clear();
       cy.get('input').should('have.value', '');
       cy.get('button').contains('다음').should('have.attr', 'disabled');
-      cy.url().should('include', '/event/create/name');
     });
 
     it('행사 이름을 입력한 후 "다음" 버튼을 누르면 행사 비밀번호 설정 화면으로 이동해야 한다.', () => {
       cy.get('input').type(CONSTANTS.eventName);
       cy.get('button').contains('다음').click();
-      cy.url().should('include', '/event/create/password');
     });
   });
 
@@ -46,7 +43,6 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
     it('행사 비밀번호 입력 페이지에서 input이 포커싱 되어 있고, "행동 개시!" 버튼이 비활성화 되어 있어야 한다.', () => {
       cy.get('input').focused();
       cy.get('button').contains('행동 개시!').should('have.attr', 'disabled');
-      cy.url().should('include', '/event/create/password');
     });
 
     it('행사 비밀번호에 숫자가 아닌 입력을 할 경우 값이 입력되지 않아야 한다.', () => {
@@ -65,7 +61,6 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
       cy.get('input').clear();
       cy.get('input').should('have.value', '');
       cy.get('button').contains('행동 개시!').should('have.attr', 'disabled');
-      cy.url().should('include', '/event/create/password');
     });
 
     it('행사 비밀번호을 입력한 후 "행동 개시!" 버튼을 누르면 행사 생성 완료 화면으로 이동해야 한다.', () => {
@@ -73,8 +68,6 @@ describe('Flow: 랜딩 페이지에서부터 이벤트를 생성 완료하는 fl
       cy.interceptAPI({type: 'getEventName', statusCode: 200});
       cy.get('input').type(CONSTANTS.eventPassword);
       cy.get('button').contains('행동 개시!').click();
-
-      cy.url().should('include', '/event/create/complete');
     });
   });
 });
