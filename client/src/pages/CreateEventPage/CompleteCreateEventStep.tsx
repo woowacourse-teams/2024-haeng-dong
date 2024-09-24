49d708ca4ce10f9bb91f5ef31c074a31bf9502c1
Soyeon Choe <soy2302ten@gmail.com>
2024-09-24 17:24:01 +0900

    design: MainLayout 중복으로 padding이 이중으로 생기는 문제 해결

--- client/src/pages/CreateEventPage/CompleteCreateEventStep.tsx
+++ client/src/pages/CreateEventPage/CompleteCreateEventStep.tsx
@@ -16,24 +16,21 @@ const CompleteCreateEventStep = ({eventToken}: CompleteCreateEventStepProps) => 
   const navigate = useNavigate();
 
   return (
-    <MainLayout backgroundColor="white">
-      <TopNav />
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-          gap: 3rem;
-          padding: 1rem;
-        `}
-      >
-        <Top>
-          <Top.Line text="행사가 생성되었어요!" emphasize={['행사가 생성되었어요!']} />
-          <Top.Line text="관리 페이지에서 정산을 시작하세요" emphasize={['정산을 시작하세요']} />
-        </Top>
-        <RunningDog />
-      </div>
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        gap: 3rem;
+        padding: 1rem;
+      `}
+    >
+      <Top>
+        <Top.Line text="행사가 생성되었어요!" emphasize={['행사가 생성되었어요!']} />
+        <Top.Line text="관리 페이지에서 정산을 시작하세요" emphasize={['정산을 시작하세요']} />
+      </Top>
+      <RunningDog />
       <FixedButton onClick={() => navigate(`${ROUTER_URLS.event}/${eventToken}/admin`)}>관리 페이지로 이동</FixedButton>
-    </MainLayout>
+    </div>
   );
 };
 
