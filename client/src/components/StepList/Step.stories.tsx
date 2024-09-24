040d580169daeb9b048570b3cd0725d8a3f8dc3d
이태훈 <rhymint@gmail.com>
2024-09-24 15:13:37 +0900

    test: Step.stories.tsx 수정

--- client/src/components/StepList/Step.stories.tsx
+++ client/src/components/StepList/Step.stories.tsx
@@ -2,6 +2,7 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
 import Step from './Step';
+import {MemoryRouter} from 'react-router-dom';
 
 const meta = {
   title: 'Components/Step',
@@ -52,9 +53,11 @@ type Story = StoryObj<typeof meta>;
 export const Playground: Story = {
   render: ({...args}) => {
     return (
-      <div style={{width: '400px'}}>
-        <Step {...args} />
-      </div>
+      <MemoryRouter>
+        <div style={{width: '400px'}}>
+          <Step {...args} />
+        </div>
+      </MemoryRouter>
     );
   },
 };
