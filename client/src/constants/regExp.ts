0294b81d32a9790bd28dce91c55dba5b9dee3a3c
김진호 <rlawlsgh1227@gmail.com>
2024-09-25 17:24:15 +0900

    feat: 계좌번호 입력 유효성 기능 추가

--- client/src/constants/regExp.ts
+++ client/src/constants/regExp.ts
@@ -3,6 +3,7 @@ const REGEXP = {
   eventUrl: /\/event\/([a-zA-Z0-9-]+)\//,
   billTitle: /^([ㄱ-ㅎ가-힣a-zA-Z0-9ㆍᆢ]\s?)*$/,
   memberName: /^([ㄱ-ㅎ가-힣a-zA-Zㆍᆢ]\s?)*$/,
+  accountNumber: /^[0-9\s\-]*$/,
 };
 
 export default REGEXP;
--- client/src/constants/rule.ts
+++ client/src/constants/rule.ts
@@ -3,6 +3,8 @@ const RULE = {
   maxEventPasswordLength: 4,
   maxMemberNameLength: 4,
   maxPrice: 10000000,
+  minAccountNumberLength: 8,
+  maxAccountNumberLength: 30,
 };
 
 export default RULE;
