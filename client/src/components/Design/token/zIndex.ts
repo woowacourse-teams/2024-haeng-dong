e8e37bb7cb04bcee0bde8c15712537ea0173b460
Soyeon Choe <soy2302ten@gmail.com>
2024-09-25 17:50:12 +0900

    feat: z-index 토큰 생성

--- client/src/components/Design/token/zIndex.ts
+++ client/src/components/Design/token/zIndex.ts
@@ -1,8 +1,36 @@
+// Utils
+const BASE = 0;
+const ABOVE = 1; // use this for all values above the base
+const BELOW = -1; // and this for all values below the base
+
+const NAV_BACKGROUND_COLOR = BASE + ABOVE;
+const TAB_INDICATOR = BASE;
+const TAB_TEXT = TAB_INDICATOR + ABOVE;
+const FIXED_BUTTON = BASE + ABOVE;
+const DEPOSIT_TOGGLE_INDICATOR_MOVING_ANIMATION = BASE + ABOVE;
+const NUMBER_KEYBOARD_BOTTOM_SHEET = FIXED_BUTTON + ABOVE;
+const BOTTOM_SHEET_DIMMED_LAYER = NUMBER_KEYBOARD_BOTTOM_SHEET + ABOVE;
+const BOTTOM_SHEET_CONTAINER = BOTTOM_SHEET_DIMMED_LAYER + ABOVE;
+
 export const ZINDEX = {
-  normal: 0,
-  hidden: -1,
-  visible: 1,
+  bottomSheetDimmedLayer: BOTTOM_SHEET_DIMMED_LAYER,
+  bottomSheetContainer: BOTTOM_SHEET_CONTAINER,
+  numberKeyboardBottomSheet: NUMBER_KEYBOARD_BOTTOM_SHEET,
+  depositToggleMovingAnimation: DEPOSIT_TOGGLE_INDICATOR_MOVING_ANIMATION,
+  fixedButton: FIXED_BUTTON,
+  navBackgroundColor: NAV_BACKGROUND_COLOR,
+  tabIndicator: TAB_INDICATOR,
+  tabText: TAB_TEXT,
 } as const;
 
-type ZIndexKeys = 'normal' | 'hidden' | 'visible';
+type ZIndexKeys =
+  | 'bottomSheetDimmedLayer'
+  | 'bottomSheetContainer'
+  | 'numberKeyboardBottomSheet'
+  | 'depositToggleMovingAnimation'
+  | 'fixedButton'
+  | 'navBackgroundColor'
+  | 'tabText'
+  | 'tabIndicator';
+
 export type ZIndexTokens = Record<ZIndexKeys, number>;
