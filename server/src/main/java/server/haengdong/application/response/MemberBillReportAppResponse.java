676ca57d3a3e7b6328564f9914e99df76094f364
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-09-26 19:30:30 +0900

    Revert "Be dev (#665)"
    This reverts commit d83281b2587554461d5a3bfa12777db1a9257ce1.

--- server/src/main/java/server/haengdong/application/response/MemberBillReportAppResponse.java
+++
@@ -1,9 +0,0 @@
-package server.haengdong.application.response;
-
-public record MemberBillReportAppResponse(
-        Long memberId,
-        String name,
-        boolean isDeposited,
-        Long price
-) {
-}
