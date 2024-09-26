e2fdf02324ba68c5aba43ef1ba59ce185ffa0092
Soyeon Choe <soy2302ten@gmail.com>
2024-09-26 13:37:45 +0900

    fix: input에 value가 변경될 때마다 컴포넌트가 리렌더링되는 문제 해결

--- client/src/hooks/useFunnel.tsx
+++ client/src/hooks/useFunnel.tsx
@@ -11,8 +11,24 @@ type StepProps = {
 };
 
 type FunnelProps = {
+  step: string;
   children: React.ReactElement<StepProps>[];
 };
+
+const Step = (stepProps: StepProps) => {
+  return <>{stepProps.children}</>;
+};
+
+const Funnel = ({children, step}: FunnelProps) => {
+  const targetStep = children.find(curStep => curStep.props.name === step);
+
+  if (!targetStep)
+    throw new Error(`현재 ${step} 단계에 보여줄 컴포넌트가 존재하지 않습니다. Step 컴포넌트를 호출해 사용해주세요.`);
+
+  return <>{targetStep}</>;
+};
+
+Funnel.Step = Step;
 
 const useFunnel = ({defaultStep, stepList}: UseFunnel) => {
   const [step, setStep] = useState(defaultStep);
@@ -31,19 +47,6 @@ const useFunnel = ({defaultStep, stepList}: UseFunnel) => {
     if (curStepIndex === 0) return;
 
     setStep(stepList[curStepIndex - 1]);
-  };
-
-  const Step = (stepProps: StepProps) => {
-    return <>{stepProps.children}</>;
-  };
-
-  const Funnel = ({children}: FunnelProps) => {
-    const targetStep = children.find(curStep => curStep.props.name === step);
-
-    if (!targetStep)
-      throw new Error(`현재 ${step} 단계에 보여줄 컴포넌트가 존재하지 않습니다. Step 컴포넌트를 호출해 사용해주세요.`);
-
-    return <>{targetStep}</>;
   };
 
   return {
--- client/src/pages/CreateEventPage/CreateEventFunnel.tsx
+++ client/src/pages/CreateEventPage/CreateEventFunnel.tsx
@@ -1,9 +1,13 @@
 import {useNavigate} from 'react-router-dom';
+import {useState} from 'react';
+
+import validateEventName from '@utils/validate/validateEventName';
 
 import useFunnel from '@hooks/useFunnel';
 import useCreateEventData from '@hooks/useCreateEventData';
+import useSetEventNameStep from '@hooks/useSetEventNameStep';
 
-import {Back, MainLayout, TopNav} from '@components/Design';
+import {Back, MainLayout, TopNav, LabelInput} from '@components/Design';
 
 import SetEventNameStep from './SetEventNameStep';
 import SetEventPasswordStep from './SetEventPasswordStep';
@@ -14,7 +18,7 @@ const STEP_SEQUENCE: CreateEventStep[] = ['eventName', 'eventPassword', 'complet
 
 const CreateEventFunnel = () => {
   const navigate = useNavigate();
-  const {moveToNextStep, moveToPrevStep, Step, Funnel, step} = useFunnel({
+  const {moveToNextStep, moveToPrevStep, Funnel, step} = useFunnel({
     defaultStep: 'eventName',
     stepList: STEP_SEQUENCE,
   });
@@ -32,22 +36,22 @@ const CreateEventFunnel = () => {
   return (
     <MainLayout backgroundColor="white">
       <TopNav>{step !== STEP_SEQUENCE[STEP_SEQUENCE.length - 1] && <Back onClick={handleBack} />}</TopNav>
-      <Funnel>
-        <Step name="eventName">
+      <Funnel step={step}>
+        <Funnel.Step name="eventName">
           <SetEventNameStep moveToNextStep={moveToNextStep} {...eventNameProps} />
-        </Step>
+        </Funnel.Step>
 
-        <Step name="eventPassword">
+        <Funnel.Step name="eventPassword">
           <SetEventPasswordStep
             moveToNextStep={moveToNextStep}
             eventName={eventNameProps.eventName}
             setEventToken={setEventToken}
           />
-        </Step>
+        </Funnel.Step>
 
-        <Step name="complete">
+        <Funnel.Step name="complete">
           <CompleteCreateEventStep eventToken={eventToken} />
-        </Step>
+        </Funnel.Step>
       </Funnel>
     </MainLayout>
   );
