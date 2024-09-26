4b31982e778ea3779ad987dec7e97fc943b00f20
김진호 <rlawlsgh1227@gmail.com>
2024-09-26 16:25:03 +0900

    feat: 안드로이드, ios 판별하는 기능 추가

--- client/src/utils/isMobileDevice.ts
+++
@@ -1,8 +0,0 @@
-const isMobileDevice = () => {
-  const userAgent = window.navigator.userAgent;
-  const mobileRegex = [/Android/i, /iPhone/i, /iPad/i, /iPod/i, /BlackBerry/i, /Windows Phone/i];
-
-  return mobileRegex.some(mobile => userAgent.match(mobile));
-};
-
-export default isMobileDevice;
