19b91a532d8da13b3e3cb07cfa79dea0a09a59d0
JinHo Kim <81083461+jinhokim98@users.noreply.github.com>
2024-09-26 15:22:03 +0900

    feat: 계좌번호 입력 제한 추가 (#634)
    * feat: 계좌번호 입력 유효성 기능 추가
    
    * feat: 계좌번호 유효성을 검증하고 input을 컨트롤하는 기능 추가
    
    * style: console.log 제거
    
    * feat: 계좌번호 정규표현식 수정
    
    ---------
    
    Co-authored-by: Soyeon Choe <77609591+soi-ha@users.noreply.github.com>

--- client/src/constants/regExp.ts
+++ client/src/constants/regExp.ts
@@ -3,6 +3,7 @@ const REGEXP = {
   eventUrl: /\/event\/([a-zA-Z0-9-]+)\//,
   billTitle: /^([ㄱ-ㅎ가-힣a-zA-Z0-9ㆍᆢ]\s?)*$/,
   memberName: /^([ㄱ-ㅎ가-힣a-zA-Zㆍᆢ]\s?)*$/,
+  accountNumber: /^\d+([\s\-]\d+)*[\s\-]?$/,
 };
 
 export default REGEXP;
