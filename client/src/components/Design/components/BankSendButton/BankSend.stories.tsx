10d0a6d166dc5066f4eb03f0a202e369b67b5090
김진호 <rlawlsgh1227@gmail.com>
2024-09-28 14:29:32 +0900

    test: prop 변경으로 스토리북 변경

--- client/src/components/Design/components/BankSendButton/BankSend.stories.tsx
+++ client/src/components/Design/components/BankSendButton/BankSend.stories.tsx
@@ -17,7 +17,6 @@ const meta = {
     },
   },
   args: {
-    clipboardText: '토스뱅크 010100-10-123123',
     onBankButtonClick: () => console.log('안녕'),
   },
 } satisfies Meta<typeof BankSendButton>;
