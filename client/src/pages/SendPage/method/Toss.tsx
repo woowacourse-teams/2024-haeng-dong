edb05ba1cf7b3e7f9f775848beaf54a2cefc75fd
김진호 <rlawlsgh1227@gmail.com>
2024-10-07 00:20:36 +0900

    feat: 아래 => 토스열기 명시적으로 변경

--- client/src/pages/SendPage/method/Toss.tsx
+++ client/src/pages/SendPage/method/Toss.tsx
@@ -10,12 +10,12 @@ const Toss = ({onTossClick, changeStep}: TossProps) => {
   return (
     <FunnelLayout>
       <Top>
-        <Top.Line text="아래 버튼을 눌러" />
+        <Top.Line text="토스열기 버튼을 눌러" />
         <Top.Line text="토스로 송금할 수 있어요." emphasize={['토스']} />
       </Top>
       <img src={`${process.env.IMAGE_URL}/standingDog.svg`} width={280} height={280} style={{margin: '0 auto'}} />
       <FixedButton onBackClick={() => changeStep('whole')} onClick={onTossClick}>
-        토스 열기
+        토스열기
       </FixedButton>
     </FunnelLayout>
   );
