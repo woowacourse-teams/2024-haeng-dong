0c063faf9dcdf4a019e1c07d6f84fcef15c8d366
pakxe <pigkill40@naver.com>
2024-10-07 16:29:02 +0900

    refactor: Flex컴포넌트의 스타일 추가 프롭을 otherStyle -> cssProp으로 이름 수정

--- client/src/components/Design/components/Flex/Flex.tsx
+++ client/src/components/Design/components/Flex/Flex.tsx
@@ -7,13 +7,9 @@ import {FlexProps} from './Flex.type';
 import {flexStyle} from './Flex.style';
 
 // TODO: (@weadie) 지정된 프롭 말고 다른 프롭도 가져올 수 있게 하자.
-function Flex({children, otherStyle, ...props}: StrictPropsWithChildren<FlexProps>) {
+function Flex({children, cssProp, ...props}: StrictPropsWithChildren<FlexProps>) {
   const {theme} = useTheme();
-  return (
-    <div css={flexStyle({theme, ...props})} style={otherStyle}>
-      {children}
-    </div>
-  );
+  return <div css={[flexStyle({theme, ...props}), cssProp]}>{children}</div>;
 }
 
 export default Flex;
