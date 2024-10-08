61c48cd61dddc2bb431d411b34155d56e3770cd1
김진호 <rlawlsgh1227@gmail.com>
2024-10-08 23:46:54 +0900

    refactor: ClickOutsideDetector 사용

--- client/src/components/Design/components/Dropdown/Dropdown.tsx
+++ client/src/components/Design/components/Dropdown/Dropdown.tsx
@@ -2,34 +2,39 @@
 import Icon from '../Icon/Icon';
 import IconButton from '../IconButton/IconButton';
 import Flex from '../Flex/Flex';
+import ClickOutsideDetector from '../ClickOutsideDetector';
 
 import useDropdown from './useDropdown';
 import {DropdownProps} from './Dropdown.type';
 import DropdownButton from './DropdownButton';
 import {dropdownStyle} from './Dropdown.style';
 
-const Dropdown = ({children}: DropdownProps) => {
-  const {isOpen, openDropdown, meetBallsRef, dropdownRef} = useDropdown();
+const Dropdown = ({base, children}: DropdownProps) => {
+  const {isOpen, setIsOpen, meetBallsRef, dropdownRef} = useDropdown();
   const isDropdownOpen = isOpen && meetBallsRef.current;
 
   return (
-    <IconButton
-      ref={meetBallsRef}
-      variants="none"
-      onClick={openDropdown}
-      style={{position: 'relative', WebkitTapHighlightColor: 'transparent'}}
-    >
-      <Icon iconType="meatballs" />
-      {isDropdownOpen && (
-        <section ref={dropdownRef}>
-          <Flex {...dropdownStyle}>
-            {children.map(button => (
-              <DropdownButton {...button.props} />
-            ))}
-          </Flex>
-        </section>
-      )}
-    </IconButton>
+    <ClickOutsideDetector targetRef={meetBallsRef} onClickOutside={() => setIsOpen(false)}>
+      <IconButton
+        ref={meetBallsRef}
+        variants="none"
+        onClick={() => setIsOpen(true)}
+        style={{position: 'relative', WebkitTapHighlightColor: 'transparent'}}
+      >
+        <Icon iconType="meatballs" />
+        {isDropdownOpen && (
+          <ClickOutsideDetector targetRef={dropdownRef} onClickOutside={() => setIsOpen(false)}>
+            <section ref={dropdownRef}>
+              <Flex {...dropdownStyle}>
+                {children.map(button => (
+                  <DropdownButton {...button.props} />
+                ))}
+              </Flex>
+            </section>
+          </ClickOutsideDetector>
+        )}
+      </IconButton>
+    </ClickOutsideDetector>
   );
 };
 
--- client/src/components/Design/components/Dropdown/useDropdown.ts
+++ client/src/components/Design/components/Dropdown/useDropdown.ts
@@ -1,36 +1,13 @@
-import {useEffect, useRef, useState} from 'react';
+import {useRef, useState} from 'react';
 
 const useDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
   const meetBallsRef = useRef<HTMLButtonElement>(null);
   const dropdownRef = useRef<HTMLElement>(null);
 
-  const openDropdown = () => {
-    setIsOpen(true);
-  };
-
-  useEffect(() => {
-    const clickOutSide = (event: MouseEvent) => {
-      const targetNode = event.target as Node;
-
-      if (
-        (dropdownRef.current && dropdownRef.current.contains(targetNode)) ||
-        (meetBallsRef.current && meetBallsRef.current.contains(targetNode))
-      ) {
-        return;
-      }
-
-      setIsOpen(false);
-    };
-    document.addEventListener('mousedown', clickOutSide);
-    return () => {
-      document.removeEventListener('mousedown', clickOutSide);
-    };
-  }, [dropdownRef]);
-
   return {
     isOpen,
-    openDropdown,
+    setIsOpen,
     meetBallsRef,
     dropdownRef,
   };
