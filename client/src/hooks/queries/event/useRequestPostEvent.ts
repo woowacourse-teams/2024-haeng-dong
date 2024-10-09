e888e0d2709d042a27bc726f1dc7b8868fd0f3e6
이태훈 <rhymint@gmail.com>
2024-10-10 03:59:42 +0900

    fix: 행사 생성 시 다른 query들 초기화하도록 변경

--- client/src/hooks/queries/event/useRequestPostEvent.ts
+++ client/src/hooks/queries/event/useRequestPostEvent.ts
@@ -1,10 +1,22 @@
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 import {RequestPostEvent, requestPostEvent} from '@apis/request/event';
+import QUERY_KEYS from '@constants/queryKeys';
 
 const useRequestPostEvent = () => {
+  const queryClient = useQueryClient();
+
   const {mutate, mutateAsync, ...rest} = useMutation({
     mutationFn: ({eventName, password}: RequestPostEvent) => requestPostEvent({eventName, password}),
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.steps]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.event]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.allMembers]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.currentMembers]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.reports]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.billDetails]});
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.images]});
+    },
   });
 
   // 실행 순서를 await으로 보장하기 위해 mutateAsync 사용
