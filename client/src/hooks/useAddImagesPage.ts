93656490384c2f6e82463d806f429fdebb791f38
이태훈 <rhymint@gmail.com>
2024-10-10 18:06:15 +0900

    fix: get 메소드가 오류날 때, 무한 렌더링이 일어나는 문제 수정

--- client/src/hooks/useAddImagesPage.ts
+++ client/src/hooks/useAddImagesPage.ts
@@ -16,7 +16,7 @@ const useAddImagesPage = () => {
   const [images, setImages] = useState<Array<LoadedImage | AddedImage>>([]);
   const [isPrevImageDeleted, setIsPrevImageDeleted] = useState(false);
   const addedImages = images.filter(image => image instanceof File);
-  const {images: prevImages} = useRequestGetImages();
+  const {images: prevImages, isSuccess} = useRequestGetImages();
   const urls = images.map(image => {
     if (image instanceof File) {
       return URL.createObjectURL(image);
@@ -33,11 +33,18 @@ const useAddImagesPage = () => {
   const {deleteImage} = useRequestDeleteImage();
 
   useEffect(() => {
-    if (!prevImages) return;
+    if (!isSuccess) return;
     setImages([...prevImages]);
-  }, [prevImages]);
+  }, [prevImages, isSuccess]);
+
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
 
   const handleChangeImages = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //TODO: (@Todari): 현재 A 이미지 추가 -> A 이미지 x 버튼 눌러 취소 -> 다시 A 이미지 추가 시 업로드 되지 않음
+    //                 event.target.files가 변경되지 않기 때문에 onChange에 넣은
+    //                 handleChangeImages가 실행되지 않아 일어나는 문제로 추정
     if (event.target.files) {
       const dataTransfer = new DataTransfer();
 
@@ -66,15 +73,16 @@ const useAddImagesPage = () => {
   const canSubmit = addedImages.length !== 0 || isPrevImageDeleted;
 
   const submitImages = async () => {
-    const formData = new FormData();
+    if (addedImages.length !== 0) {
+      const formData = new FormData();
 
-    if (!addedImages) return;
+      for (let i = 0; i < addedImages.length; i++) {
+        formData.append('images', addedImages[i], addedImages[i].name);
+      }
 
-    for (let i = 0; i < addedImages.length; i++) {
-      formData.append('images', addedImages[i], addedImages[i].name);
+      await postImages({formData});
     }
 
-    await postImages({formData});
     navigate(`/event/${eventId}/admin`);
   };
 
