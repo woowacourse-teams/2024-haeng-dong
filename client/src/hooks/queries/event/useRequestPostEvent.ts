0f22c768663ef467fcf9f1fbaed27482bf260bb1
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2024-10-10 16:11:07 +0900

    fix: react-query 캐싱 적용으로 인해 발생한 버그 해결 (#716)
    * fix: onSuccess에 currentMembers queryKey refetching 추가하기
    
    * fix: 행사를 생성할 때 마다, queryKey event refetching 추가하기
    
    * fix: 새로운 행사 생성시 기존(이전 행사)의 query 모두 제거

--- client/src/hooks/queries/event/useRequestPostEvent.ts
+++ client/src/hooks/queries/event/useRequestPostEvent.ts
@@ -1,10 +1,14 @@
-import {useMutation} from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 
 import {RequestPostEvent, requestPostEvent} from '@apis/request/event';
 
 const useRequestPostEvent = () => {
+  const queryClient = useQueryClient();
   const {mutate, mutateAsync, ...rest} = useMutation({
     mutationFn: ({eventName, password}: RequestPostEvent) => requestPostEvent({eventName, password}),
+    onSuccess: () => {
+      queryClient.removeQueries();
+    },
   });
 
   // 실행 순서를 await으로 보장하기 위해 mutateAsync 사용
