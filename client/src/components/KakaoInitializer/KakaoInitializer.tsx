1cb114bfcffff29b7fbfa210dd9835b2de18627d
김진호 <rlawlsgh1227@gmail.com>
2024-10-10 13:35:36 +0900

    chore: 불필요한 console.log제거

--- client/src/components/KakaoInitializer/KakaoInitializer.tsx
+++ client/src/components/KakaoInitializer/KakaoInitializer.tsx
@@ -6,7 +6,6 @@ const KakaoInitializer = ({children}: React.PropsWithChildren) => {
 
     if (!window.Kakao.isInitialized()) {
       window.Kakao.init(process.env.KAKAO_JAVASCRIPT_KEY);
-      console.log('Kakao initialized');
     }
   }, []);
 
