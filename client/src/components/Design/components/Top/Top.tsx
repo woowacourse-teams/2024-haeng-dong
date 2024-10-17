a29fcef87adb57c9286e912766f5717054d16729
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2024-10-17 15:27:45 +0900

    fix: 접근성 개선 : 지출내역 추가 Flow의 Top (#758)
    * fix: Top 컴포넌트 접근성 개선
    
    * fix: 불필요한 useState와 useEffect를 제거

--- client/src/components/Design/components/Top/Top.tsx
+++ client/src/components/Design/components/Top/Top.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import {css} from '@emotion/react';
+import React from 'react';
 
 import Line from './Line';
 import EditableLine from './EditableLine';
@@ -8,12 +9,21 @@ Top.Line = Line;
 Top.EditableLine = EditableLine;
 
 export default function Top({children}: React.PropsWithChildren) {
+  const childrenTexts: string[] = [];
+  React.Children.forEach(children, child => {
+    if (React.isValidElement(child) && child.type === Top.Line) {
+      childrenTexts.push(child.props.text);
+    }
+  });
+
   return (
     <div
       css={css`
         display: flex;
         flex-direction: column;
       `}
+      aria-label={childrenTexts.join(' ')}
+      tabIndex={0}
     >
       {children}
     </div>
