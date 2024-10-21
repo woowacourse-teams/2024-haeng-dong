f5acd6db851efe1fc75081407cd954c78905adc7
이태훈 <rhymint@gmail.com>
2024-10-21 21:06:14 +0900

    fix: 첫 스크롤이 이상하게 되던 오류 수정

--- client/src/hooks/useMainPageYScroll.ts
+++ client/src/hooks/useMainPageYScroll.ts
@@ -10,13 +10,13 @@ const useMainPageYScroll = () => {
         const featureSectionTop = featureSectionRef.current.offsetTop;
         const scrollY = window.scrollY;
 
-        if (scrollY >= featureSectionTop && translateX.current <= window.innerWidth * 4) {
+        if (scrollY >= featureSectionTop && translateX.current < window.innerWidth * 4) {
           window.scrollTo(0, featureSectionTop);
           translateX.current += scrollY - featureSectionTop;
           const newTransform = `translateX(calc(200vw - ${translateX.current > 0 ? translateX.current : 0}px))`;
           featureSectionRef.current.style.transform = newTransform;
         }
-        if (scrollY <= featureSectionTop && translateX.current >= 0) {
+        if (scrollY <= featureSectionTop && translateX.current > 0) {
           window.scrollTo(0, featureSectionTop);
           translateX.current += scrollY - featureSectionTop;
           const newTransform = `translateX(calc(200vw - ${translateX.current < window.innerWidth * 4 ? translateX.current : window.innerWidth * 4}px))`;
