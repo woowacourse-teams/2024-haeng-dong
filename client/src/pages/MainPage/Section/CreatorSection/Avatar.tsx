73d83fd5b465f2814b873e24a5d90f3fd3065817
pakxe <pigkill40@naver.com>
2024-10-22 00:29:51 +0900

    feat: Avatar 컴포넌트에서 이미지 경량화를 위한 Image 컴포넌트를 사용

--- client/src/pages/MainPage/Section/CreatorSection/Avatar.tsx
+++ client/src/pages/MainPage/Section/CreatorSection/Avatar.tsx
@@ -1,6 +1,10 @@
 import {css} from '@emotion/react';
 
+import Image from '@components/Design/components/Image/Image';
+
 import {Text} from '@components/Design';
+
+import getImageUrl from '@utils/getImageUrl';
 
 interface Props {
   imagePath: string;
@@ -22,8 +26,9 @@ const Avatar = ({imagePath, name, onClick}: Props) => {
         },
       })}
     >
-      <img
-        src={`${process.env.IMAGE_URL}/${imagePath}.png`}
+      <Image
+        src={getImageUrl(imagePath, 'webp')}
+        fallbackSrc={getImageUrl(imagePath, 'png')}
         css={css({
           width: '100%',
           height: '100%',
