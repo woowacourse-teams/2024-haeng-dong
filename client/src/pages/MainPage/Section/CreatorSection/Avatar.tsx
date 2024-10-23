0eecb95c58238c027a65e509ca82c8bea2f79a9e
이태훈 <rhymint@gmail.com>
2024-10-23 15:02:24 +0900

    refactor: avatar style 분ㄹ

--- client/src/pages/MainPage/Section/CreatorSection/Avatar.tsx
+++ client/src/pages/MainPage/Section/CreatorSection/Avatar.tsx
@@ -1,6 +1,7 @@
 import {css} from '@emotion/react';
 
 import {Text} from '@components/Design';
+import {avatarImageStyle, avatarStyle} from './Avatar.style';
 
 interface Props {
   imagePath: string;
@@ -10,26 +11,8 @@ interface Props {
 
 const Avatar = ({imagePath, name, onClick}: Props) => {
   return (
-    <button
-      onClick={onClick}
-      css={css({
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '0.5rem',
-        '@media (min-width: 1200px)': {
-          gap: '1rem',
-        },
-      })}
-    >
-      <img
-        src={`${process.env.IMAGE_URL}/${imagePath}.png`}
-        css={css({
-          width: '100%',
-          height: '100%',
-          borderRadius: '25%',
-        })}
-      />
+    <button onClick={onClick} css={avatarStyle}>
+      <img src={`${process.env.IMAGE_URL}/${imagePath}.png`} css={avatarImageStyle} />
       <Text size="bodyBold" textColor="white" responsive={true}>
         {name}
       </Text>
