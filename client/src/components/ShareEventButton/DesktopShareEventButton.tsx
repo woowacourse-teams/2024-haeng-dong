031d6330213e9cf612b7f8aedecadace82062a78
Soyeon Choe <77609591+soi-ha@users.noreply.github.com>
2024-10-23 17:04:02 +0900

    feat: QR코드로 초대하기 기능 추가 (#783)
    * fix: DropDown에 Tap 글씨가 위에 있는 에러 수정
    
    * feat: QR코드로 초대하기 페이지 디자인 구현 및 navigate 추가
    
    * feat: qrcode.react 라이브러리를 활용하여 행사 접속 QR코드 생성 구현
    
    * feat: 데스크탑 초대하기를 DropDown으로 변경하여 QR코드 초대 기능 추가하기

--- client/src/components/ShareEventButton/DesktopShareEventButton.tsx
+++ client/src/components/ShareEventButton/DesktopShareEventButton.tsx
@@ -1,12 +1,16 @@
+import {useNavigate} from 'react-router-dom';
+
 import toast from '@hooks/useToast/toast';
 
-import {Button} from '@components/Design';
+import {Dropdown, DropdownButton} from '@components/Design';
+
+import getEventIdByUrl from '@utils/getEventIdByUrl';
 
 type DesktopShareEventButtonProps = React.PropsWithChildren<React.HTMLAttributes<HTMLButtonElement>> & {
   onCopy: () => Promise<void>;
 };
 
-const DesktopShareEventButton = ({onCopy, children, ...buttonProps}: DesktopShareEventButtonProps) => {
+const DesktopShareEventButton = ({onCopy}: DesktopShareEventButtonProps) => {
   const copyAndToast = async () => {
     await onCopy();
     toast.confirm('링크가 복사되었어요 :) \n참여자들에게 링크를 공유해 주세요!', {
@@ -15,10 +19,20 @@ const DesktopShareEventButton = ({onCopy, children, ...buttonProps}: DesktopShar
     });
   };
 
+  const navigate = useNavigate();
+  const eventId = getEventIdByUrl();
+
+  const navigateQRPage = () => {
+    navigate(`/event/${eventId}/qrcode`);
+  };
+
   return (
-    <Button size="small" variants="tertiary" onClick={copyAndToast} style={{marginRight: '1rem'}} {...buttonProps}>
-      {children}
-    </Button>
+    <div style={{marginRight: '1rem'}}>
+      <Dropdown base="button" baseButtonText="정산 초대하기">
+        <DropdownButton text="링크 복사하기" onClick={copyAndToast} />
+        <DropdownButton text="QR코드로 초대하기" onClick={navigateQRPage} />
+      </Dropdown>
+    </div>
   );
 };
 
---
+++ client/src/pages/QRCodePage/QRCodePage.style.ts
@@ -0,0 +1,8 @@
+import {css} from '@emotion/react';
+
+export const QRCodeStyle = () => css`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
