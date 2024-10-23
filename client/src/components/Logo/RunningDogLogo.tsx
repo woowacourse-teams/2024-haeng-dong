7555433b54e8c22057423ea41cb38e441dfe96fc
pakxe <pigkill40@naver.com>
2024-10-23 15:03:47 +0900

    design: 행동개시 행댕이의 크기가 너무 커지지 않도록 maxWidth 속성 추가

--- client/src/components/Logo/RunningDogLogo.tsx
+++ client/src/components/Logo/RunningDogLogo.tsx
@@ -2,12 +2,16 @@ import Image from '@components/Design/components/Image/Image';
 
 import getImageUrl from '@utils/getImageUrl';
 
-import {logoStyle} from './Logo.style';
+import {logoImageStyle, logoStyle} from './Logo.style';
 
 const RunningDogLogo = () => {
   return (
     <div css={logoStyle}>
-      <Image width="100%" src={getImageUrl('runningDog', 'webp')} fallbackSrc={getImageUrl('runningDog', 'png')} />
+      <Image
+        css={logoImageStyle}
+        src={getImageUrl('runningDog', 'webp')}
+        fallbackSrc={getImageUrl('runningDog', 'png')}
+      />
     </div>
   );
 };
