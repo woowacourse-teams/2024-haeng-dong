b7935f2aa78c0683953ee6435c1fd5e1c9823938
TaehunLee <85233397+Todari@users.noreply.github.com>
2024-10-24 21:20:34 +0900

    fix: 768 이상의 해상도에서 랜딩페이지 좌우 패딩 오류 (#808)
    * fix: mainPage 768px 이상 해상도에서 제대로 작동하지 않던 오류 해결
    
    * style: lint 적용

--- client/src/pages/MainPage/MainPage.style.ts
+++ client/src/pages/MainPage/MainPage.style.ts
@@ -5,6 +5,24 @@ export const mainContainer = css({
   flexDirection: 'column',
   justifyContent: 'start',
   alignItems: 'center',
+  width: '100vw',
   height: '850vh',
   overflowX: 'hidden',
 });
+
+export const backgroundStyle = css({
+  position: 'fixed',
+  height: '100vh',
+  width: '100vw',
+  top: 0,
+  zIndex: -1,
+  backgroundColor: '#000000',
+});
+
+export const backgroundImageStyle = (isVisible: boolean) =>
+  css({
+    objectFit: 'cover',
+    height: '100vh',
+    width: '100vw',
+    opacity: isVisible ? 1 : 0,
+  });
--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,15 +1,20 @@
+import Image from '@components/Design/components/Image/Image';
+
 import useAmplitude from '@hooks/useAmplitude';
+import usePageBackground from '@hooks/usePageBackground';
+
+import getImageUrl from '@utils/getImageUrl';
 
 import Nav from './Nav/Nav';
 import {MainSection} from './Section/MainSection';
 import {DescriptionSection} from './Section/DescriptionSection';
 import {FeatureSection} from './Section/FeatureSection';
-import {mainContainer} from './MainPage.style';
+import {backgroundImageStyle, backgroundStyle, mainContainer} from './MainPage.style';
 import CreatorSection from './Section/CreatorSection/CreatorSection';
 
 const MainPage = () => {
   const {trackStartCreateEvent} = useAmplitude();
-
+  const {isVisible} = usePageBackground();
   return (
     <div css={mainContainer}>
       <Nav trackStartCreateEvent={trackStartCreateEvent} />
@@ -17,6 +22,15 @@ const MainPage = () => {
       <DescriptionSection />
       <FeatureSection />
       <CreatorSection />
+
+      <div css={backgroundStyle}>
+        <Image
+          css={backgroundImageStyle(isVisible)}
+          src={getImageUrl('mainSectionBackground', 'webp')}
+          alt=""
+          fallbackSrc={getImageUrl('mainSectionBackground', 'png')}
+        />
+      </div>
     </div>
   );
 };
