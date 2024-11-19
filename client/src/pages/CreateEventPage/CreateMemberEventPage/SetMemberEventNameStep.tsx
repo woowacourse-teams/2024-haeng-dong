6016b66546c34cbdf0443835323de70fcc9b751c
pakxe <pigkill40@naver.com>
2024-11-19 18:47:31 +0900

    feat: 회원 이벤트 생성 시 행사명만 받으면 바로 행사를 생성하는 플로우로 스텝 페이지 구현

---
+++ client/src/pages/CreateEventPage/CreateMemberEventPage/SetMemberEventNameStep.tsx
@@ -0,0 +1,65 @@
+import {css} from '@emotion/react';
+
+import Top from '@components/Design/components/Top/Top';
+import useSetEventNameStep from '@hooks/createEvent/useSetEventNameStep';
+import {requestPostMemberEvent} from '@apis/request/event';
+import useRequestPostMemberEvent from '@hooks/queries/event/useRequestPostMemberEvent';
+
+import useAmplitude from '@hooks/useAmplitude';
+
+import {FixedButton, Input} from '@HDesign/index';
+
+type SetEventNamePageProps = {
+  moveToNextStep: () => void;
+  setEventToken: (eventToken: string) => void;
+};
+
+const SetMemberEventNameStep = ({moveToNextStep, setEventToken}: SetEventNamePageProps) => {
+  const {eventName, errorMessage, canSubmit, handleEventNameChange} = useSetEventNameStep();
+  const {postEvent} = useRequestPostMemberEvent();
+  const {trackCompleteCreateMemberEvent} = useAmplitude();
+
+  const submit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await postEvent(eventName, {
+      onSuccess: ({eventId}) => {
+        trackCompleteCreateMemberEvent({eventName, eventToken: eventId});
+        setEventToken(eventId);
+      },
+    });
+
+    moveToNextStep();
+  };
+
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 1rem;
+      `}
+    >
+      <Top>
+        <Top.Line text="정산을 시작하려는" />
+        <Top.Line text="행사의 이름은 무엇인가요?" emphasize={['행사의 이름']} />
+      </Top>
+      <form onSubmit={submit}>
+        <Input
+          labelText="행사 이름"
+          errorText={errorMessage ?? ''}
+          value={eventName}
+          type="text"
+          placeholder="행동대장 야유회"
+          onChange={handleEventNameChange}
+          isError={!!errorMessage}
+          autoFocus
+        />
+        <FixedButton disabled={!canSubmit}>다음</FixedButton>
+      </form>
+    </div>
+  );
+};
+
+export default SetMemberEventNameStep;
