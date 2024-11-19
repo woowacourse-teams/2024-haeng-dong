13695a66bea949b953f75baa0fc02e25f1ef8439
pakxe <pigkill40@naver.com>
2024-11-19 18:41:16 +0900

    feat: 유저 이름이 올바르지 않을 때 보여줄 에러 메세지 수정

--- client/src/constants/errorMessage.ts
+++ client/src/constants/errorMessage.ts
@@ -52,7 +52,7 @@ export const SERVER_ERROR_MESSAGES: ErrorMessage = {
 export const ERROR_MESSAGE = {
   eventName: SERVER_ERROR_MESSAGES.EVENT_NAME_LENGTH_INVALID,
   eventPasswordType: SERVER_ERROR_MESSAGES.EVENT_PASSWORD_FORMAT_INVALID,
-  memberName: SERVER_ERROR_MESSAGES.MEMBER_NAME_LENGTH_INVALID,
+  memberName: `이름은 ${RULE.maxMemberNameLength}자까지 입력 가능해요.`,
   purchasePrice: `${RULE.maxPrice.toLocaleString('ko-kr')}원 이하의 숫자만 입력이 가능해요`,
   purchaseTitle: `지출 이름은 ${RULE.maxBillNameLength}자 이하의 한글, 영어, 숫자만 가능해요`,
   preventEmpty: '값은 비어있을 수 없어요',
