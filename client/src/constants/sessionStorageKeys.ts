94fdfa7330eb4be3ed9755a5b2d1d9cddf702a45
김진호 <rlawlsgh1227@gmail.com>
2024-11-21 15:19:59 +0900

    feat: 인증 api 요청할 때 세션스토리지에 createdByGuest 저장

--- client/src/constants/sessionStorageKeys.ts
+++ client/src/constants/sessionStorageKeys.ts
@@ -1,6 +1,8 @@
 const SESSION_STORAGE_KEYS = {
   closeAccountBannerByEventToken: (eventToken: string) => `closeAccountBanner-${eventToken}`,
   closeDepositStateBannerByEventToken: (eventToken: string) => `closeDepositStateBanner-${eventToken}`,
+  createdByGuest: 'createdByGuest',
+  previousUrlForLogin: 'previousUrlForLogin',
 } as const;
 
 export default SESSION_STORAGE_KEYS;
--- client/src/hooks/queries/auth/useRequestPostAuthentication.ts
+++ client/src/hooks/queries/auth/useRequestPostAuthentication.ts
@@ -5,14 +5,24 @@ import {requestPostAuthentication} from '@apis/request/auth';
 import {useAuthStore} from '@store/authStore';
 
 import getEventIdByUrl from '@utils/getEventIdByUrl';
+import SessionStorage from '@utils/SessionStorage';
+
+import SESSION_STORAGE_KEYS from '@constants/sessionStorageKeys';
+
+import useRequestGetEvent from '../event/useRequestGetEvent';
 
 const useRequestPostAuthentication = () => {
   const eventId = getEventIdByUrl();
   const {updateAuth} = useAuthStore();
 
+  const {createdByGuest} = useRequestGetEvent();
+
   const {mutate, ...rest} = useMutation({
     mutationFn: () => requestPostAuthentication({eventId}),
     onSuccess: () => updateAuth(true),
+    onMutate: () => {
+      SessionStorage.set<boolean>(SESSION_STORAGE_KEYS.createdByGuest, createdByGuest);
+    },
   });
 
   return {
