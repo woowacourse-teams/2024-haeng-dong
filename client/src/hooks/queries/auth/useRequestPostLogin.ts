e8e5e714ffeacd679d0919019c9f97ae9196b8a2
김진호 <rlawlsgh1227@gmail.com>
2024-11-21 15:40:00 +0900

    feat: 로그인에 성공했을 때 이전의 주소로 이동하는 기능 구현

--- client/src/hooks/queries/auth/useRequestPostLogin.ts
+++ client/src/hooks/queries/auth/useRequestPostLogin.ts
@@ -1,18 +1,30 @@
 import {useMutation} from '@tanstack/react-query';
+import {useNavigate} from 'react-router-dom';
 
 import {RequestPostToken, requestPostToken} from '@apis/request/auth';
 
 import {useAuthStore} from '@store/authStore';
 
 import getEventIdByUrl from '@utils/getEventIdByUrl';
+import SessionStorage from '@utils/SessionStorage';
+
+import SESSION_STORAGE_KEYS from '@constants/sessionStorageKeys';
 
 const useRequestPostLogin = () => {
   const eventId = getEventIdByUrl();
   const {updateAuth} = useAuthStore();
+  const navigate = useNavigate();
 
   const {mutate, ...rest} = useMutation({
     mutationFn: ({password}: RequestPostToken) => requestPostToken({eventId, password}),
-    onSuccess: () => updateAuth(true),
+    onSuccess: () => {
+      const previousUrlForLogin = SessionStorage.get<string>(SESSION_STORAGE_KEYS.previousUrlForLogin);
+      if (previousUrlForLogin) {
+        SessionStorage.remove(SESSION_STORAGE_KEYS.previousUrlForLogin);
+        navigate(previousUrlForLogin, {replace: true});
+      }
+      updateAuth(true);
+    },
   });
 
   return {postLogin: mutate, ...rest};
