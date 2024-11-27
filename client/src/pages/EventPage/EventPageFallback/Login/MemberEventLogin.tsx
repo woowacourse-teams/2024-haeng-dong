15f57d67d4833bb0dc5699fea5768aa5073959ef
김진호 <rlawlsgh1227@gmail.com>
2024-11-27 10:01:59 +0900

    refactor: 카카오 계정 로그인 강조 길이 변경

--- client/src/pages/EventPage/EventPageFallback/Login/MemberEventLogin.tsx
+++ client/src/pages/EventPage/EventPageFallback/Login/MemberEventLogin.tsx
@@ -14,7 +14,7 @@ const MemberEventLogin = () => {
     <FunnelLayout>
       <Top>
         <Top.Line text={`행사 관리 접근을 위해`} />
-        <Top.Line text="카카오 계정 로그인을 해주세요." emphasize={['카카오 계정 로그인을 해주세요.']} />
+        <Top.Line text="카카오 계정 로그인을 해주세요." emphasize={['카카오 계정 로그인']} />
       </Top>
       <Button variants="kakao" size="large" onClick={() => goKakaoLogin(previousUrl)}>
         <Flex alignItems="center" gap="0.625rem">
