eb1b63984deb55bbfd9e25b2deacf56039d8e663
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-12-10 19:31:43 +0900

    feat: 멀티스레드 종료 시 자원 반납 대기 시간 추가

--- server/src/main/java/haengdong/event/config/S3Config.java
+++ server/src/main/java/haengdong/event/config/S3Config.java
@@ -3,6 +3,7 @@ package haengdong.event.config;
 import jakarta.annotation.PreDestroy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -12,6 +13,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 public class S3Config {
 
     private static final int THREAD_POOL_SIZE = 10;
+    private static final int LIFE_CYCLE_SHUTDOWN_TIME_SEC = 10;
 
     private final ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
 
@@ -30,5 +32,13 @@ public class S3Config {
     @PreDestroy
     public void shutdown() {
         executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(LIFE_CYCLE_SHUTDOWN_TIME_SEC, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
     }
 }
