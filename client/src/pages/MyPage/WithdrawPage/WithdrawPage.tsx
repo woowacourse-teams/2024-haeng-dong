2d1866b26233d2dc412c00a09ed8ef3388ac5bbd
Soyeon Choe <soy2302ten@gmail.com>
2024-12-17 12:26:58 +0900

    feat: 회원탈퇴/ 서비스를 사용하지 않는 이유 Step 구현

--- client/src/pages/MyPage/WithdrawPage/WithdrawPage.tsx
+++ client/src/pages/MyPage/WithdrawPage/WithdrawPage.tsx
@@ -3,6 +3,7 @@ import useWithdrawFunnel from '@hooks/useWithdrawFunnel';
 import {MainLayout, TopNav} from '@components/Design';
 
 import ReasonStep from './steps/ReasonStep';
+import NotUseServiceStep from './steps/NotUseServiceStep';
 
 const WithdrawPage = () => {
   const {step, handleMoveStep} = useWithdrawFunnel();
@@ -13,6 +14,7 @@ const WithdrawPage = () => {
         <TopNav.Item displayName="뒤로가기" noEmphasis routePath="-1" />
       </TopNav>
       {step === 'withdrawReason' && <ReasonStep handleMoveStep={handleMoveStep} />}
+      {step === 'notUseService' && <NotUseServiceStep handleMoveStep={handleMoveStep} />}
     </MainLayout>
   );
 };
