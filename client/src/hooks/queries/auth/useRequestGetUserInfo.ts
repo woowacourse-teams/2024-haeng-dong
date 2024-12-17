e629535af826b6c2a822251c544ba6acf28b5f6f
pakxe <pigkill40@naver.com>
2024-12-17 19:41:26 +0900

    fix: 에러 바운더리를 키지 않도록 하며 초기 데이터를 설정해 isGuest인데도 로그인 화면이 안뜨는 오류를 막음

--- client/src/hooks/queries/auth/useRequestGetUserInfo.ts
+++ client/src/hooks/queries/auth/useRequestGetUserInfo.ts
@@ -1,4 +1,5 @@
 import {useQuery} from '@tanstack/react-query';
+import {useEffect} from 'react';
 
 import {requestGetUserInfo} from '@apis/request/auth';
 
@@ -10,6 +11,15 @@ const useRequestGetUserInfo = () => {
   const {data, ...rest} = useQuery({
     queryKey: [QUERY_KEYS.userInfo],
     queryFn: () => requestGetUserInfo(),
+    // quernFn은 ErrorCatcher 구독을 하지 않으며 오류가 났을 경우 로그인 화면을 띄워야하므로 initialData를 설정했습니다.
+    throwOnError: false,
+    initialData: {
+      isGuest: true,
+      nickname: '',
+      profileImage: '',
+      accountNumber: '',
+      bankName: '',
+    },
   });
 
   return {
