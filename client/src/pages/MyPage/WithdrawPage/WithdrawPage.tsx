38e4a0b18e81db4f4a1279457bc6fabdb1bae1e9
Soyeon Choe <soy2302ten@gmail.com>
2024-12-17 14:02:49 +0900

    feat: 회원 탈퇴 완료 페이지 구현

--- client/src/pages/MyPage/WithdrawPage/WithdrawPage.tsx
+++ client/src/pages/MyPage/WithdrawPage/WithdrawPage.tsx
@@ -6,6 +6,7 @@ import ReasonStep from './steps/ReasonStep';
 import NotUseServiceStep from './steps/NotUseServiceStep';
 import EtcStep from './steps/EtcStep';
 import CheckBeforeWithdrawingStep from './steps/CheckBeforeWithdrawingStep';
+import WithdrawalCompleted from './steps/WithdrawalCompleted';
 
 const WithdrawPage = () => {
   const {step, handleMoveStep} = useWithdrawFunnel();
@@ -19,6 +20,7 @@ const WithdrawPage = () => {
       {step === 'notUseService' && <NotUseServiceStep handleMoveStep={handleMoveStep} />}
       {step === 'etc' && <EtcStep handleMoveStep={handleMoveStep} />}
       {step === 'checkBeforeWithdrawing' && <CheckBeforeWithdrawingStep handleMoveStep={handleMoveStep} />}
+      {step === 'withdrawalCompleted' && <WithdrawalCompleted />}
     </MainLayout>
   );
 };
