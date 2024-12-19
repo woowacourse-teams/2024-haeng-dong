69ed50cfa8c3a41e5da10aca117c0ce187dff137
김진호 <rlawlsgh1227@gmail.com>
2024-12-19 15:53:44 +0900

    feat: 비회원일 때 마이페이지 접근 시 에러 화면 보여주기, 로딩 화면 보여주는 기능 추가

--- client/src/pages/mypage/MyPage.tsx
+++ client/src/pages/mypage/MyPage.tsx
@@ -1,55 +1,55 @@
 import {useNavigate} from 'react-router-dom';
 
-import Container from '@pages/mypage/Container';
 import {Profile} from '@components/Design/components/Profile/Profile';
+import useRequestSuspenseGetUserInfo from '@hooks/queries/auth/useRequestSuspenseGetUserInfo';
 
-import useMyPage from '@hooks/useMyPage';
-
-import {Flex, FunnelLayout, MainLayout, Text, TextButton, TopNav, useTheme} from '@components/Design';
+import {Flex, Text, TextButton} from '@components/Design';
 
 import getImageUrl from '@utils/getImageUrl';
 
 import {ROUTER_URLS} from '@constants/routerUrls';
 
-const MyPage = () => {
-  const navigate = useNavigate();
-  const {profileImage, nickname} = useMyPage();
+import MyPageLayout from './MyPageLayout';
+
+const UserInfoSection = () => {
+  const {userInfo} = useRequestSuspenseGetUserInfo();
+  const {profileImage, nickname} = userInfo;
 
   return (
-    <MainLayout backgroundColor="gray">
-      <TopNav>
-        <TopNav.Item displayName="뒤로가기" noEmphasis routePath="-1" />
-      </TopNav>
-      <FunnelLayout>
-        <Container>
-          <Flex justifyContent="spaceBetween" alignItems="center" margin="0 1rem">
-            <Flex gap="1rem" alignItems="center">
-              <Profile src={profileImage ?? getImageUrl('runningDog', 'png')} size="medium" />
-              <Text size="bodyBold" textColor="onTertiary">
-                {nickname}
-              </Text>
-            </Flex>
-          </Flex>
-        </Container>
-        <Container>
-          <Flex flexDirection="column" margin="0 1.5rem" gap="1rem">
-            <TextButton textColor="onTertiary" textSize="body">
-              닉네임 설정하기
-            </TextButton>
-            <TextButton textColor="onTertiary" textSize="body">
-              기본 계좌 번호 설정하기
-            </TextButton>
-            <TextButton textColor="onTertiary" textSize="body" onClick={() => navigate(ROUTER_URLS.createdEvents)}>
-              내가 만든 행사 목록 보기
-            </TextButton>
-            <TextButton textColor="onTertiary" textSize="body" onClick={() => navigate(ROUTER_URLS.withdraw)}>
-              탈퇴하기
-            </TextButton>
-          </Flex>
-        </Container>
-      </FunnelLayout>
-    </MainLayout>
+    <Flex justifyContent="spaceBetween" alignItems="center" margin="0 1rem">
+      <Flex gap="1rem" alignItems="center">
+        <Profile src={profileImage ?? getImageUrl('runningDog', 'png')} size="large" />
+        <Text size="bodyBold" textColor="onTertiary">
+          {nickname}
+        </Text>
+      </Flex>
+    </Flex>
   );
+};
+
+const SettingSection = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Flex flexDirection="column" margin="0 1.5rem" gap="1rem">
+      <TextButton textColor="onTertiary" textSize="body">
+        닉네임 설정하기
+      </TextButton>
+      <TextButton textColor="onTertiary" textSize="body">
+        기본 계좌 번호 설정하기
+      </TextButton>
+      <TextButton textColor="onTertiary" textSize="body" onClick={() => navigate(ROUTER_URLS.createdEvents)}>
+        내가 만든 행사 목록 보기
+      </TextButton>
+      <TextButton textColor="onTertiary" textSize="body" onClick={() => navigate(ROUTER_URLS.withdraw)}>
+        탈퇴하기
+      </TextButton>
+    </Flex>
+  );
+};
+
+const MyPage = () => {
+  return <MyPageLayout userInfoSection={<UserInfoSection />} settingSection={<SettingSection />} />;
 };
 
 export default MyPage;
