747ced1513ac9e413a2d51eaeaf3bd0283433bc1
TaehunLee <85233397+Todari@users.noreply.github.com>
2025-01-01 05:22:23 +0900

    feat: IconX component 구현

---
+++ client/src/components/Design/components/Icon/IconX.tsx
@@ -0,0 +1,24 @@
+/** @jsxImportSource @emotion/react */
+import {useTheme} from '@components/Design/theme/HDesignProvider';
+import {SvgProps} from '../Icon.type';
+import Svg from '../Svg';
+
+export const IconXCircle = ({color = 'gray', size, width, height}: Omit<SvgProps, 'children'>) => {
+  const {theme} = useTheme();
+  const w = width ?? size ?? 12;
+  const h = height ?? size ?? 12;
+  const x = w / 12;
+  const y = h / 12;
+
+  return (
+    <Svg size={size} width={w} height={h}>
+      <path
+        d={`M${9 * x} ${3 * y}L${3 * x} ${9 * y}M${3 * x} ${3 * y}L${9 * x} ${9 * y}`}
+        stroke={theme.colors[color]}
+        stroke-width={w * 0.133}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </Svg>
+  );
+};
