49ad133e0d8befc950bb4fc2660447fb74e46e7d
김진호 <rlawlsgh1227@gmail.com>
2025-01-03 16:57:36 +0900

    refactor: long press 애니메이션 커스텀 훅으로 분리

---
+++ client/src/hooks/useLongPressAnimation.tsx
@@ -0,0 +1,99 @@
+import {css, keyframes} from '@emotion/react';
+import {useMemo, useRef, useState} from 'react';
+
+import {WithTheme} from '@components/Design/type/withTheme';
+import {ColorKeys} from '@components/Design/token/colors';
+
+import {useTheme} from '@components/Design';
+
+export type AnimationCoordinate = {
+  x: number;
+  y: number;
+  size: number;
+};
+
+const animationFrame = keyframes`
+  from {
+    transform: scale(0);
+  }
+  to {
+    transform: scale(1);
+    opacity: 0;
+  }
+`;
+
+const animationStyle = ({
+  theme,
+  x,
+  y,
+  size,
+  animationColor,
+}: WithTheme<AnimationCoordinate & {animationColor: ColorKeys}>) => {
+  const remSize = size / 16;
+  const remX = x / 16;
+  const remY = y / 16;
+
+  return css({
+    position: 'absolute',
+    top: `${remY - remSize / 2}rem`,
+    left: `${remX - remSize / 2}rem`,
+    width: `${remSize}rem`,
+    height: `${remSize}rem`,
+    background: animationColor,
+    borderRadius: `50%`,
+    transform: 'scale(1)',
+    animation: `${animationFrame} 0.5s ease-out forwards`,
+  });
+};
+
+type UseLongPressAnimationOptions = {
+  longProgressTime?: number;
+  animationColor: ColorKeys;
+};
+
+const useLongPressAnimation = (onLongPress: () => void, options?: UseLongPressAnimationOptions) => {
+  const {theme} = useTheme();
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+  const [coordinate, setCoordinate] = useState<AnimationCoordinate | null>(null);
+
+  const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    const rect = event.currentTarget.getBoundingClientRect();
+    const touch = event.touches[0];
+    const x = touch.clientX - rect.left;
+    const y = touch.clientY - rect.top;
+
+    setCoordinate({x, y, size: 0});
+
+    timeoutRef.current = setTimeout(() => {
+      onLongPress();
+    }, options?.longProgressTime ?? 500);
+  };
+
+  const handleTouchMove = () => {
+    clearTimeout(timeoutRef.current!);
+    setCoordinate(null);
+  };
+
+  const handleTouchEnd = () => {
+    clearTimeout(timeoutRef.current!);
+    timeoutRef.current = null;
+  };
+
+  const LongPressAnimation = useMemo(() => {
+    return coordinate ? (
+      <div
+        css={animationStyle({theme, animationColor: options?.animationColor ?? 'grayContainer', ...coordinate})}
+        onAnimationStart={() => setCoordinate(prev => prev && {...prev, size: 800})}
+      />
+    ) : null;
+  }, [coordinate]);
+
+  return {
+    handleTouchStart,
+    handleTouchMove,
+    handleTouchEnd,
+    LongPressAnimation,
+  };
+};
+
+export default useLongPressAnimation;
