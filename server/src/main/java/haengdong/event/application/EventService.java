7a101880af46d69d6392859076f8776cb69192c6
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-01-06 18:03:42 +0900

    feat: 트랜잭션 전파 옵션 변경

--- server/src/main/java/haengdong/event/application/EventService.java
+++ server/src/main/java/haengdong/event/application/EventService.java
@@ -33,6 +33,7 @@ import java.util.Map.Entry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -230,7 +231,7 @@ public class EventService {
     }
 
     @Async
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleUserDelete(UserDeleteEvent userDeleteEvent) {
         Long userId = userDeleteEvent.id();
