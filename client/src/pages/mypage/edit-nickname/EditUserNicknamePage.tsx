a6fb3123d7239b200d0fdd5144b772d233f1f0b4
Soyeon Choe <soy2302ten@gmail.com>
2025-01-06 17:21:20 +0900

    feat: 회원 이름 변경 기능 구현 (기존 관리자 이름 hook을 활용하여 유저 이름을 변경하고 반영하기)

---
+++ client/src/pages/mypage/edit-nickname/EditUserNicknamePage.tsx
@@ -0,0 +1,50 @@
+import {useNavigate} from 'react-router-dom';
+
+import useUserInfoContext from '@hooks/useUserInfoContext';
+import useMemberName from '@hooks/useMemberName';
+
+import {FixedButton, FunnelLayout, Input, MainLayout, Top, TopNav} from '@components/Design';
+
+import {ROUTER_URLS} from '@constants/routerUrls';
+
+const EditUserNicknamePage = () => {
+  const {nickname} = useUserInfoContext();
+
+  const {errorMessage, canSubmit, name, handleNameChange} = useMemberName(nickname);
+
+  const navigate = useNavigate();
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    navigate(ROUTER_URLS.myPage);
+  };
+  return (
+    <MainLayout backgroundColor="white">
+      <TopNav>
+        <TopNav.Item displayName="뒤로가기" noEmphasis routePath="-1" />
+      </TopNav>
+      <FunnelLayout>
+        <Top>
+          <Top.Line text="행사에서 사용할" />
+          <Top.Line text="본인의 이름을 입력해주세요" emphasize={['본인의 이름']} />
+        </Top>
+        <form onSubmit={event => onSubmit(event)}>
+          <Input
+            labelText="행사 이름"
+            errorText={errorMessage ?? ''}
+            value={name}
+            type="text"
+            placeholder="박행댕"
+            onChange={handleNameChange}
+            isError={!!errorMessage}
+            autoFocus
+          />
+          <FixedButton disabled={!canSubmit}>변경완료</FixedButton>
+        </form>
+      </FunnelLayout>
+    </MainLayout>
+  );
+};
+
+export default EditUserNicknamePage;
