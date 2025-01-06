8d856d02779ada223af1f4226710ec6fdfa1fe65
Arachne <66822642+Arachneee@users.noreply.github.com>
2025-01-06 15:32:13 +0900

    fix: 계좌 빈값 버그 수정

--- server/src/main/java/haengdong/user/presentation/response/UserResponse.java
+++ server/src/main/java/haengdong/user/presentation/response/UserResponse.java
@@ -1,6 +1,8 @@
 package haengdong.user.presentation.response;
 
 import haengdong.event.application.response.UserAppResponse;
+import haengdong.user.domain.AccountNumber;
+import haengdong.user.domain.Bank;
 
 public record UserResponse(
         String nickname,
@@ -11,9 +13,15 @@ public record UserResponse(
 ) {
 
     public static UserResponse of(UserAppResponse response) {
+        Bank bank = response.bankName();
+        AccountNumber accountedNumber = response.accountNumber();
+
         return new UserResponse(
-                response.nickname().getValue(), response.bankName().getName(), response.accountNumber().getValue(), response.isGuest(),
-                response.profileImage()
+                response.nickname().getValue(),
+                bank == null ? "" : bank.getName(),
+                accountedNumber == null ? "" : accountedNumber.getValue(),
+                response.isGuest(),
+                response.profileImage() == null ? "" : response.profileImage()
         );
     }
 }
