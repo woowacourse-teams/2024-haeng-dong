2e9b2342498c29b0c9fb2890ff14d018cb340bd1
김진호 <rlawlsgh1227@gmail.com>
2025-01-08 10:50:27 +0900

    feat: 시간보다 행사 상태를 기준으로 먼저 정렬하는 기능 추가

--- client/src/hooks/queries/event/useRequestGetCreatedEvents.ts
+++ client/src/hooks/queries/event/useRequestGetCreatedEvents.ts
@@ -1,16 +1,25 @@
 import {useQuery} from '@tanstack/react-query';
 
 import {requestGetCreatedEvents} from '@apis/request/event';
+import {CreatedEvent} from 'types/serviceType';
 
 import QUERY_KEYS from '@constants/queryKeys';
 
 const useRequestGetCreatedEvents = () => {
+  const sortByCreatedAndFinishedStatus = (a: CreatedEvent, b: CreatedEvent) => {
+    if (a.isFinished !== b.isFinished) {
+      return a.isFinished ? 1 : -1;
+    }
+
+    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  };
+
   const {data, ...rest} = useQuery({
     queryKey: [QUERY_KEYS.createdEvents],
     queryFn: () => requestGetCreatedEvents(),
     select: data => ({
       ...data,
-      events: data.events.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()),
+      events: data.events.sort(sortByCreatedAndFinishedStatus),
     }),
   });
 
