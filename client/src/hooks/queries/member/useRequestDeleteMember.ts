bccd4d764317c2d45e905fffd7c43b397126f89b
김진호 <rlawlsgh1227@gmail.com>
2025-01-12 18:31:53 +0900

    refactor: 불필요한 invalidate 코드 제거, 멤버 변경 시 createdEvent invalidate 실행

--- client/src/hooks/queries/member/useRequestDeleteMember.ts
+++ client/src/hooks/queries/member/useRequestDeleteMember.ts
@@ -13,12 +13,17 @@ const useRequestDeleteMember = () => {
   const {mutateAsync, ...rest} = useMutation({
     mutationFn: ({memberId}: RequestDeleteMember) => requestDeleteMember({eventId, memberId}),
     onSuccess: () => {
+      // putAsyncMember 전에만 실행되는 hook이라 데이터를 무효화하지 않아도 putAsyncMember가 무효화한다.
+      // queryClient.invalidateQueries({queryKey: [QUERY_KEYS.reports, eventId]});
+
+      // 현재 페이지에서 active하지 않음
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.steps, eventId]});
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.allMembers, eventId]});
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.currentMembers, eventId]});
-      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.reports, eventId]});
-
       queryClient.removeQueries({queryKey: [QUERY_KEYS.billDetails, eventId]});
+
+      // 멤버 변경으로 인해 행사 종료가 될 수 있으므로 invalidate 실행
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.createdEvents]});
     },
   });
 
--- client/src/hooks/queries/member/useRequestPostMembers.ts
+++ client/src/hooks/queries/member/useRequestPostMembers.ts
@@ -17,6 +17,10 @@ const useRequestPostMembers = () => {
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.steps, eventId]});
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.reports, eventId]});
       queryClient.invalidateQueries({queryKey: [QUERY_KEYS.currentMembers, eventId]});
+
+      // merge 할 때 조심 #921 브랜치에서 위 invalidate 조작했다는 것을
+      // 멤버 추가로 인해 행사 종료가 될 수 있으므로 invalidate 실행
+      queryClient.invalidateQueries({queryKey: [QUERY_KEYS.createdEvents]});
       return responseData;
     },
   });
