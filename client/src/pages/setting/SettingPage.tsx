8d75b7e984d5699ae3e68409ab0bf6de5a31acc1
Soyeon Choe <soy2302ten@gmail.com>
2025-01-20 16:26:00 +0900

    feat: SettingSection 구현 및 Category 컴포넌트 구현

--- client/src/pages/setting/SettingPage.tsx
+++ client/src/pages/setting/SettingPage.tsx
@@ -1,10 +1,50 @@
 import {ErrorBoundary} from '@sentry/react';
 import {Suspense} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 import MyPageError from '@pages/fallback/MyPageError';
 import MyPageLoading from '@pages/fallback/MyPageLoading';
+import VStack from '@components/Design/components/Stack/VStack';
 
 import {FunnelLayout, MainLayout, TopNav} from '@components/Design';
+import {useTheme, Text, TextButton} from '@components/Design';
+
+import {CategoryProps, TabList} from './SettingPage.type';
+
+const accountCategory: TabList[] = [
+  {name: '약관', url: 'd'},
+  {name: '로그아웃', url: 'd'},
+];
+
+const Category = ({categoryTitle, tabList}: CategoryProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <VStack gap="16">
+      <Text textColor="onTertiary" size="bodyBold">
+        {categoryTitle}
+      </Text>
+      {tabList.map(tab => {
+        return (
+          <TextButton textColor="onTertiary" textSize="body" onClick={() => navigate(tab.url)}>
+            {tab.name}
+          </TextButton>
+        );
+      })}
+    </VStack>
+  );
+};
+
+const SettingSection = () => {
+  const {theme} = useTheme();
+
+  return (
+    <VStack p="24" bg={`${theme.colors.white}`} br="12">
+      <div>test</div>
+      <Category categoryTitle="계정" tabList={accountCategory} />
+    </VStack>
+  );
+};
 
 const SettingPage = () => {
   return (
@@ -14,7 +54,9 @@ const SettingPage = () => {
       </TopNav>
       <FunnelLayout>
         <ErrorBoundary fallback={<MyPageError />}>
-          <Suspense fallback={<MyPageLoading />}>{/* section이 들어올 것 */}</Suspense>
+          <Suspense fallback={<MyPageLoading />}>
+            <SettingSection />
+          </Suspense>
         </ErrorBoundary>
       </FunnelLayout>
     </MainLayout>
---
+++ client/src/pages/setting/SettingPage.type.ts
@@ -0,0 +1,6 @@
+export type TabList = {name: string; url: string};
+
+export interface CategoryProps {
+  categoryTitle: string;
+  tabList: TabList[];
+}
