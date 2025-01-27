edf6f4d73a48fca9c65ed0a310d6caeb3908a573
김진호 <rlawlsgh1227@gmail.com>
2025-01-27 21:43:26 +0900

    fix: 회원 행사 생성 완료 시 trackCompleteCreateEvent이벤트로 변경 (상위에서 이미 카카오 유저 판단하고 있음)

--- client/src/pages/event/create/user/SetUserEventNameStep.tsx
+++ client/src/pages/event/create/user/SetUserEventNameStep.tsx
@@ -16,14 +16,14 @@ type SetEventNamePageProps = {
 const SetUserEventNameStep = ({moveToNextStep, setEventToken}: SetEventNamePageProps) => {
   const {eventName, errorMessage, canSubmit, handleEventNameChange} = useSetEventNameStep();
   const {postEvent, isPostEventPending} = useRequestPostUserEvent();
-  const {trackCompleteCreateUserEvent} = useAmplitude();
+  const {trackCompleteCreateEvent} = useAmplitude();
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     await postEvent(eventName, {
       onSuccess: ({eventId}) => {
-        trackCompleteCreateUserEvent({eventName, eventToken: eventId});
+        trackCompleteCreateEvent({eventName, eventToken: eventId});
         setEventToken(eventId);
       },
     });
