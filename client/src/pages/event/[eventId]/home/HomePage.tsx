b1f4cb5c163d23f9958c25a0abd67a1ea6030f86
김진호 <rlawlsgh1227@gmail.com>
2025-01-30 15:40:54 +0900

    refactor: 불필요한 prop 제거

--- client/src/pages/event/[eventId]/home/HomePage.tsx
+++ client/src/pages/event/[eventId]/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import {useLocation, useMatch, useNavigate} from 'react-router-dom';
+import {useMatch, useNavigate} from 'react-router-dom';
 
 import StepList from '@components/StepList/Steps';
 import Reports from '@components/Reports/Reports';
@@ -18,7 +18,6 @@ import {ROUTER_URLS} from '@constants/routerUrls';
 import {receiptStyle} from './HomePage.style';
 
 const HomePage = () => {
-  const location = useLocation();
   const {isAdmin, eventName, steps} = useEventDataContext();
   const isInHomePage = useMatch(ROUTER_URLS.home) !== null;
 
@@ -40,7 +39,7 @@ const HomePage = () => {
           )
         }
       />
-      <Tabs active={location.state?.tab === 'step' ? 1 : 0}>
+      <Tabs>
         <Tab label="참여자 별 정산" content={<Reports />} />
         <Tab label="전체 지출 내역" content={<StepList data={steps ?? []} isAdmin={isAdmin && !isInHomePage} />} />
       </Tabs>
