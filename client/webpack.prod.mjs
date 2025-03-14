70d3597362bc5b11950d69cb72fa922a3c3399f8
pakxe <pigkill40@naver.com>
2025-03-14 21:52:07 +0900

    chore: prod 환경에 터서 최적화 장착

--- client/webpack.prod.mjs
+++ client/webpack.prod.mjs
@@ -3,7 +3,8 @@ import {merge} from 'webpack-merge';
 import Dotenv from 'dotenv-webpack';
 import common from './webpack.common.mjs';
 import {fileURLToPath} from 'url';
-import {sentryWebpackPlugin} from '@sentry/webpack-plugin';
+// import {sentryWebpackPlugin} from '@sentry/webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,13 +23,32 @@ export default merge(common, {
     new Dotenv({
       path: '.env.prod',
     }),
-    sentryWebpackPlugin({
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: 'wtc-o6',
-      project: 'javascript-react',
-      sourcemaps: {
-        filesToDeleteAfterUpload: ['**/*.js.map', '**/*.css.map', '**/*.LICENSE.txt'],
-      },
-    }),
+    // sentryWebpackPlugin({
+    //   authToken: process.env.SENTRY_AUTH_TOKEN,
+    //   org: 'wtc-o6',
+    //   project: 'javascript-react',
+    //   sourcemaps: {
+    //     filesToDeleteAfterUpload: ['**/*.js.map', '**/*.css.map', '**/*.LICENSE.txt'],
+    //   },
+    // }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            // 가능한 모든 코드를 압축
+            drop_console: true, // 콘솔 로그를 제거하여 파일 크기 감소
+          },
+          mangle: true, // 변수 및 함수 이름을 짧게 변경
+          output: {
+            comments: false, // 주석을 제거
+          },
+        },
+        extractComments: false, // 별도의 파일로 주석을 추출하지 않음
+        parallel: true, // 멀티 프로세스를 사용하여 빌드 속도 향상
+      }),
+    ],
+  },
 });
