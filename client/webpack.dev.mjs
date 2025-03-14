724272256dfa54e67953c5bac8afa7c44058fd8e
pakxe <pigkill40@naver.com>
2025-03-14 21:50:35 +0900

    chore: dev 환경에선 불필요한 터서 최적화 제거

--- client/webpack.dev.mjs
+++ client/webpack.dev.mjs
@@ -3,7 +3,7 @@ import {merge} from 'webpack-merge';
 import Dotenv from 'dotenv-webpack';
 import common from './webpack.common.mjs';
 import {fileURLToPath} from 'url';
-import TerserPlugin from 'terser-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,7 +15,6 @@ export default merge(common, {
     chunkFilename: '[id].chunk.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
   devtool: 'eval-source-map',
   devServer: {
@@ -30,26 +29,7 @@ export default merge(common, {
     new Dotenv({
       path: '.env.dev',
     }),
+    new ForkTsCheckerWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            // 가능한 모든 코드를 압축
-            drop_console: true, // 콘솔 로그를 제거하여 파일 크기 감소
-            drop_debugger: true, // 디버거 코드 제거
-            pure_funcs: ['console.info'], // 특정 함수 호출 제거
-          },
-          mangle: true, // 변수 및 함수 이름을 짧게 변경
-          output: {
-            comments: false, // 주석을 제거
-          },
-        },
-        extractComments: false, // 별도의 파일로 주석을 추출하지 않음
-        parallel: true, // 멀티 프로세스를 사용하여 빌드 속도 향상
-      }),
-    ],
-  },
+  
 });
