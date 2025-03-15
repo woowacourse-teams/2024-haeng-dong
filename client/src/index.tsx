5c87b695c548adb2e7f000e3bd76ae9b4d7b8aff
Pakxe <64801796+pakxe@users.noreply.github.com>
2025-03-15 14:30:22 +0900

    feat: webpack build, buld-dev 속도 개선 (#1002)
    * chore: 센트리 비활성화
    
    * chore:  ts-loader가 타입 검사를 중복으로 수행하지 않도록 함
    
    * chore: 사용되지 않는 코드 주석처리
    
    * chore: emotion을 위해 jsx pragma를 쓰지 않아도 되도록 쑤정
    
    * chore: dev 환경에선 불필요한 터서 최적화 제거
    
    * chore: deprecated된 [has]를 [contenthash]로 변경
    
    * chore: prod 환경에 터서 최적화 장착
    
    * chore: 원본 코드 없이 라인 트래킹만 가능한 소스맵으로 변경
    
    * chore: prod환경에서는 파일시스템 캐싱 사용
    
    * chore: 린트를 위해 devdep 업데이트
    
    * chore: 불필요한 공백 제거
    
    * chore: line 커멘드에 --fix 옵션 추가
    
    * chore: node_modules와 webpack cache를 워크플로우에 추가
    
    * fix: hotfix 시에는 타입 체크 필요하므로 prod build에 타입 검사 추가
    
    * fix: 현재 위치에서 요청하지 않도록 publicPath 작성
    
    * fix: 노드 모듈스 캐싱을 뒤쪽에 해서 의미가 없던걸 앞으로 땡김
    
    * chore: CI 과정에도 노드모듈스와 웹팩 캐시
    
    * chore: prod, dev에서 중복으로 사용되는 플러그인을 common에 추가
    
    * chore: import 수정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from 'react-router-dom';
-import * as Sentry from '@sentry/react';
+// import * as Sentry from '@sentry/react';
 
 import router from './router';
 
-Sentry.init({
-  dsn: 'https://81685591a3234c689be8c48959b04c88@o4507739935997952.ingest.us.sentry.io/4507739943272448',
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ['localhost', /^https:\/\/api\.haengdong\.pro/],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+// Sentry.init({
+//   dsn: 'https://81685591a3234c689be8c48959b04c88@o4507739935997952.ingest.us.sentry.io/4507739943272448',
+//   integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+//   // Performance Monitoring
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: ['localhost', /^https:\/\/api\.haengdong\.pro/],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
 
 // MSW 모킹을 사용하려면 아래 주석을 해제하고 save해주세요.
 // async function enableMocking() {
--- client/webpack.dev.mjs
+++ client/webpack.dev.mjs
@@ -3,7 +3,6 @@ import {merge} from 'webpack-merge';
 import Dotenv from 'dotenv-webpack';
 import common from './webpack.common.mjs';
 import {fileURLToPath} from 'url';
-import TerserPlugin from 'terser-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,25 +30,4 @@ export default merge(common, {
       path: '.env.dev',
     }),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          compress: {
-            // 가능한 모든 코드를 압축
-            drop_console: true, // 콘솔 로그를 제거하여 파일 크기 감소
-            drop_debugger: true, // 디버거 코드 제거
-            pure_funcs: ['console.info'], // 특정 함수 호출 제거
-          },
-          mangle: true, // 변수 및 함수 이름을 짧게 변경
-          output: {
-            comments: false, // 주석을 제거
-          },
-        },
-        extractComments: false, // 별도의 파일로 주석을 추출하지 않음
-        parallel: true, // 멀티 프로세스를 사용하여 빌드 속도 향상
-      }),
-    ],
-  },
 });
